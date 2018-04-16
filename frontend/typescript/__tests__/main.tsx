1aaeb9f770593cf7971a897352bf7e42caad40cf
Alex Ware <alex.ware@guardian.co.uk>
2018-04-16 14:47:20 +0100

    remove duplicate import

--- frontend/typescript/__tests__/main.tsx
+++ frontend/typescript/__tests__/main.tsx
@@ -1,5 +1,4 @@
 import 'dom-testing-library/extend-expect'
-import 'dom-testing-library/extend-expect'
 import React from 'react'
 import {render, Simulate, wait} from 'react-testing-library'
 // this add custom expect matchers from dom-testing-library
