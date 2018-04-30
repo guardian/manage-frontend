deabe55d3ba8cdd2ace99273babe1db009b38ef2
Alex Ware <alex.ware@guardian.co.uk>
2018-04-30 11:45:18 +0100

    fix watch script

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,16 +1,23 @@
+import cookieParser from "cookie-parser"
 import { renderStylesToString } from "emotion-server";
 import express from "express";
+import helmet from "helmet"
 import path from "path"
 import React from "react";
 import { renderToString } from "react-dom/server";
 import User from "../client/components/user";
 import html from "./html";
+
+
 const port = 9233;
 
 const server = express();
+server.use(helmet())
+server.use(cookieParser())
+
 server.use("/static", express.static("dist/static"));
 
-server.get("/", (req, res) => {
+server.get("/", (req: express.Request, res: express.Response) => {
   /**
    * renderToString() will take our React app and turn it into a string
    * to be inserted into our Html template function.
@@ -18,6 +25,8 @@ server.get("/", (req, res) => {
   const body = renderStylesToString(renderToString(User));
   const title = "Server side Rendering with Styled Components";
   const src= "static/user.js"
+  
+  console.log(req.cookies)
 
   res.send(
     html({
