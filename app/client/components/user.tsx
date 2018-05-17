c7f38f97c8d92876826ff83873737c90c31df258
Tom Richards <tom.richards@guardian.co.uk>
2018-05-17 11:55:09 +0100

    now handles {} responses from members-data-api - with some cool type magic

--- app/client/components/user.tsx
+++ app/client/components/user.tsx
@@ -4,34 +4,47 @@ import fonts from "../styles/fonts";
 import global from "../styles/global";
 import AsyncLoader from "./asyncLoader";
 import { Main } from "./main";
-import { Table } from "./table";
 
-interface Details {
-  id: string;
+interface MembershipData {
+  regNumber: string;
   tier: string;
-  [key: string]: string;
 }
 
-class Membership extends AsyncLoader<Details> {}
+type MembersDataApiResponse = MembershipData | {};
 
-const loadMembershipData: () => Promise<Details> = async () => {
-  const resp = await fetch("api/membership", { credentials: "include" });
-  const data = await resp.json();
+function hasMembership(data: MembersDataApiResponse): data is MembershipData {
+  return data.hasOwnProperty("tier");
+}
 
-  return {
-    tier: data.tier,
-    id: data.subscription.subscriberId
-  };
+class Membership extends AsyncLoader<MembersDataApiResponse> {}
+
+const loadMembershipData: () => Promise<MembersDataApiResponse> = async () => {
+  return (await fetch("api/membership", { credentials: "include" })).json();
+};
+
+const renderMembershipData = (data: MembersDataApiResponse) => {
+  if (hasMembership(data)) {
+    return (
+      <table>
+        <tr>
+          <th>Membership number</th>
+          <td>{data.regNumber}</td>
+        </tr>
+        <tr>
+          <th>Membership tier</th>
+          <td>{data.tier}</td>
+        </tr>
+      </table>
+    );
+  }
+  return <h1>No Membership</h1>;
 };
 
 const User = (
   <Main>
     {injectGlobal`${global}`}
     {injectGlobal`${fonts}`}
-    <Membership
-      fetch={loadMembershipData}
-      render={data => <Table data={data} />}
-    />
+    <Membership fetch={loadMembershipData} render={renderMembershipData} />
   </Main>
 );
 
