0db91633f17e0f505d9889a8a461c6b30a64465e
Alex Ware <alex.ware@guardian.co.uk>
2018-05-18 13:43:57 +0100

    Add raven and refactor conf.

--- app/client/user.ts
+++ app/client/user.ts
@@ -1,6 +1,8 @@
+import Raven from "raven-js";
 import ReactDOM from "react-dom";
-
 import User from "./components/user";
+declare var CLIENT_DSN: string;
+Raven.config(CLIENT_DSN).install();
 
 const element = document.getElementById("app");
 ReactDOM.hydrate(User, element);
