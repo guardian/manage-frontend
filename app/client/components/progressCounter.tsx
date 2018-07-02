555923793c93e36feec91c7a7652419b542fd31a
Tom Richards <tom.richards@guardian.co.uk>
2018-07-02 12:33:32 +0100

    progressCounter tweaks

--- app/client/components/progressCounter.tsx
+++ app/client/components/progressCounter.tsx
@@ -1,8 +1,7 @@
-import { cx } from "emotion";
 import React from "react";
 import palette from "../colours";
 
-const RADIUS = 42;
+const RADIUS = 40;
 const FONT_SIZE = 16;
 export const ProgressCounter = ({
   current,
@@ -23,14 +22,15 @@ export const ProgressCounter = ({
   );
   const a = Array(total)
     .fill(0)
-    .map((_, n) => (
+    .map((_, i) => i + 1)
+    .map(n => (
       <div
         css={{
           transitionProperty: "background",
           transitionDuration: "0.2s",
           fontFamily: "'Guardian Text Sans Web'",
-          color: palette.neutral["1"],
-          border: `1px solid ${palette.neutral["3"]}`,
+          color: n === current ? palette.neutral["5"] : palette.neutral["1"],
+          border: `1px solid ${palette.neutral["2"]}`,
           minHeight: `${RADIUS}px`,
           minWidth: `${RADIUS}px`,
           marginLeft: `${RADIUS}px`,
@@ -44,13 +44,16 @@ export const ProgressCounter = ({
             paddingTop: `${FONT_SIZE / 8}px`, // magic number to visually center the number because numbers dont have descenders
             margin: 0
           },
-          ...(n !== current && {
+          ...(n < current && {
             background: palette.neutral["5"]
+          }),
+          ...(n === current && {
+            background: palette.neutral["2"]
           })
         }}
         key={n}
       >
-        <span>{n + 1}</span>
+        <span>{n}</span>
       </div>
     ));
   const xs = [...spacers].map((x, n) => (n % 2 ? x : a[n / 2]));
