07c255c8c8a89f48b0d8cf096286091c0872f297
Tom Richards <tom.richards@guardian.co.uk>
2018-07-03 12:22:42 +0100

    removed accidental test code

--- app/client/components/cancel/notFound.tsx
+++ app/client/components/cancel/notFound.tsx
@@ -1,11 +1,3 @@
 import React from "react";
-import { ProgressCounter } from "../progressCounter";
 
-export const NotFound = (props: { default: boolean }) => (
-  <div>
-    <div css={{ paddingBottom: "40px" }}>
-      <ProgressCounter current={2} total={3} />
-    </div>
-    <h1>Not Found</h1>
-  </div>
-);
+export const NotFound = (props: { default: boolean }) => <h1>Not Found</h1>;
