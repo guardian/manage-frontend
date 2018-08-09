40781989f3176ddf353b069bddbf1ea52b84bb11
Tom Richards <tom.richards@guardian.co.uk>
2018-08-09 14:12:20 +0100

    corrected 'contact us' placement and little refactoring for better re-use

--- app/client/components/callCentreNumbers.tsx
+++ app/client/components/callCentreNumbers.tsx
@@ -1,31 +1,50 @@
 import React from "react";
 import { Accordion } from "./accordion";
+import { css } from "../styles/emotion";
 
-export const CallCentreNumbers = () => (
-  <Accordion>
-    <div title="United Kingdom, Europe and rest of world">
-      <div>
-        <b>+ 44 (0) 330 333 6790</b>
+const contactUsStyles = css({
+  margin: "0 0 10px",
+  paddingRight: "5px"
+});
+
+const callCenterStyles = css({
+  marginBottom: "50px",
+  display: "flex",
+  flexWrap: "wrap"
+});
+
+export interface CallCentreNumbersProps {
+  prefixText: string;
+}
+
+export const CallCentreNumbers = (props: CallCentreNumbersProps) => (
+  <div className={callCenterStyles}>
+    <p className={contactUsStyles}>{props.prefixText}</p>
+    <Accordion>
+      <div title="United Kingdom, Europe and rest of world">
+        <div>
+          <b>+ 44 (0) 330 333 6790</b>
+        </div>
+        <div>8am - 8pm on weekdays, 8am-6pm at weekends (GMT/BST)</div>
       </div>
-      <div>8am - 8pm on weekdays, 8am-6pm at weekends (GMT/BST)</div>
-    </div>
-    <div title="Australia, New Zealand, and Asia Pacific">
-      <div>
-        <b>1800 773 766</b> (within Australia)
+      <div title="Australia, New Zealand, and Asia Pacific">
+        <div>
+          <b>1800 773 766</b> (within Australia)
+        </div>
+        <div>
+          <b>+61 2​ 8​076 8599</b> (outside Australia)
+        </div>
+        <div>9am - 5pm Monday - Friday (AEDT)</div>
       </div>
-      <div>
-        <b>+61 2​ 8​076 8599</b> (outside Australia)
+      <div title="Canada and USA">
+        <div>
+          <b>1-844-632-2010</b> (toll free USA)
+        </div>
+        <div>
+          <b>+1 917-900-4663</b> (outside USA)
+        </div>
+        <div>9:15am - 6pm Monday - Friday (EST)</div>
       </div>
-      <div>9am - 5pm Monday - Friday (AEDT)</div>
-    </div>
-    <div title="Canada and USA">
-      <div>
-        <b>1-844-632-2010</b> (toll free USA)
-      </div>
-      <div>
-        <b>+1 917-900-4663</b> (outside USA)
-      </div>
-      <div>9:15am - 6pm Monday - Friday (EST)</div>
-    </div>
-  </Accordion>
+    </Accordion>
+  </div>
 );
--- app/client/components/genericErrorScreen.tsx
+++ app/client/components/genericErrorScreen.tsx
@@ -1,26 +1,6 @@
 import React from "react";
-import { minWidth } from "../styles/breakpoints";
-import { css } from "../styles/emotion";
 import { CallCentreNumbers } from "./callCentreNumbers";
 import { PageContainer } from "./page";
-
-const contactUsStyles = css({
-  margin: "0 0 10px",
-
-  [minWidth.phablet]: {
-    paddingRight: "5px",
-    margin: "0",
-    flexBasis: "40%"
-  }
-});
-
-const callCenterStyles = css({
-  marginBottom: "50px",
-
-  [minWidth.phablet]: {
-    display: "flex"
-  }
-});
 
 export const GenericErrorScreen = () => (
   <PageContainer>
@@ -31,9 +11,6 @@ export const GenericErrorScreen = () => (
       Please try again in 5 minutes. Alternatively, please call to speak to one
       of our customer service specialists.
     </p>
-    <div className={callCenterStyles}>
-      <p className={contactUsStyles}>To contact us</p>
-      <CallCentreNumbers />
-    </div>
+    <CallCentreNumbers prefixText="To contact us" />
   </PageContainer>
 );
