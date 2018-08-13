16fe1ff13a5f52313e2d0d67f8322fb2fd02cd6f
shaundillon <shaun.dillon@guardian.co.uk>
2018-08-13 17:44:06 +0100

    set hard coded font styles

--- app/client/styles/global.ts
+++ app/client/styles/global.ts
@@ -1,7 +1,7 @@
 const global = `
 html {
   box-sizing: border-box;
-  font-family: "Guardian Text Sans Web","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;
+  font-family: "GuardianTextSans", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
 }
