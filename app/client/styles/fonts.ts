e07cf84c6e4b255b5c2a652d90e42a1bbe871173
shaundillon <shaun.dillon@guardian.co.uk>
2018-08-14 10:50:33 +0100

    absolute path for fonts

--- app/client/styles/fonts.ts
+++ app/client/styles/fonts.ts
@@ -1,180 +1,180 @@
 export const fonts = `
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Light.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Light.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Light.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Light.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Light.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Light.ttf") format("truetype");
 	font-weight: 300;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-LightItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-LightItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-LightItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-LightItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-LightItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-LightItalic.ttf") format("truetype");
 	font-weight: 300;
 	font-style: italic;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Regular.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Regular.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Regular.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Regular.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Regular.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Regular.ttf") format("truetype");
 	font-weight: 400;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-RegularItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-RegularItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-RegularItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-RegularItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-RegularItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-RegularItalic.ttf") format("truetype");
 	font-weight: 400;
 	font-style: italic;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Medium.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Medium.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Medium.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Medium.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Medium.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Medium.ttf") format("truetype");
 	font-weight: 500;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-MediumItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-MediumItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-MediumItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-MediumItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-MediumItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-MediumItalic.ttf") format("truetype");
 	font-weight: 500;
 	font-style: italic;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Semibold.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Semibold.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Semibold.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Semibold.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Semibold.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Semibold.ttf") format("truetype");
 	font-weight: 600;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-SemiboldItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-SemiboldItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-SemiboldItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-SemiboldItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-SemiboldItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-SemiboldItalic.ttf") format("truetype");
 	font-weight: 600;
 	font-style: italic;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Bold.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Bold.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Bold.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Bold.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Bold.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Bold.ttf") format("truetype");
 	font-weight: 700;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-BoldItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-BoldItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-BoldItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-BoldItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-BoldItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-BoldItalic.ttf") format("truetype");
 	font-weight: 700;
 	font-style: italic;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-Black.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-Black.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-Black.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-Black.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-Black.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-Black.ttf") format("truetype");
 	font-weight: 900;
 	font-style: normal;
 	}
 
 @font-face {
 	font-family: "GH Guardian Headline";
-	src: url("./static/fonts/GHGuardianHeadline-BlackItalic.woff2") format("woff2"),
-		     url("./static/fonts/GHGuardianHeadline-BlackItalic.woff") format("woff"),
-		     url("./static/fonts/GHGuardianHeadline-BlackItalic.ttf") format("truetype");
+	src: url("/static/fonts/GHGuardianHeadline-BlackItalic.woff2") format("woff2"),
+		     url("/static/fonts/GHGuardianHeadline-BlackItalic.woff") format("woff"),
+		     url("/static/fonts/GHGuardianHeadline-BlackItalic.ttf") format("truetype");
 	font-weight: 900;
 	font-style: italic;
     }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-Regular.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-Regular.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-Regular.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-Regular.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-Regular.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-Regular.ttf") format("truetype");
         font-weight: 400;
         font-style: normal;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-RegularItalic.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-RegularItalic.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-RegularItalic.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-RegularItalic.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-RegularItalic.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-RegularItalic.ttf") format("truetype");
         font-weight: 400;
         font-style: italic;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-Medium.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-Medium.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-Medium.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-Medium.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-Medium.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-Medium.ttf") format("truetype");
         font-weight: 500;
         font-style: normal;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-MediumItalic.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-MediumItalic.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-MediumItalic.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-MediumItalic.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-MediumItalic.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-MediumItalic.ttf") format("truetype");
         font-weight: 500;
         font-style: italic;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-Bold.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-Bold.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-Bold.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-Bold.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-Bold.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-Bold.ttf") format("truetype");
         font-weight: 700;
         font-style: normal;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-BoldItalic.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-BoldItalic.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-BoldItalic.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-BoldItalic.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-BoldItalic.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-BoldItalic.ttf") format("truetype");
         font-weight: 700;
         font-style: italic;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-Black.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-Black.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-Black.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-Black.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-Black.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-Black.ttf") format("truetype");
         font-weight: 900;
         font-style: normal;
         }
     
     @font-face {
         font-family: "GuardianTextSans";
-        src: url("./static/fonts/GuardianTextSans-BlackItalic.woff2") format("woff2"),
-                 url("./static/fonts/GuardianTextSans-BlackItalic.woff") format("woff"),
-                 url("./static/fonts/GuardianTextSans-BlackItalic.ttf") format("truetype");
+        src: url("/static/fonts/GuardianTextSans-BlackItalic.woff2") format("woff2"),
+                 url("/static/fonts/GuardianTextSans-BlackItalic.woff") format("woff"),
+                 url("/static/fonts/GuardianTextSans-BlackItalic.ttf") format("truetype");
         font-weight: 900;
         font-style: italic;
         }
