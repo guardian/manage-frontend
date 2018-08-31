8abe279750cfcaad28f0e20696172184e8bc08f6
Tom Richards <tom.richards@guardian.co.uk>
2018-08-31 17:01:45 +0100

    Card fields now only show once Stripe is ready to go

--- app/client/components/payment/update/flexCardElement.tsx
+++ app/client/components/payment/update/flexCardElement.tsx
@@ -5,6 +5,7 @@ import {
   CardNumberElement
 } from "react-stripe-elements";
 import { sans } from "../../../styles/fonts";
+import { Spinner } from "../../spinner";
 import { FieldWrapper } from "./fieldWrapper";
 
 export interface FlexCardElementProps {
@@ -18,29 +19,62 @@ const baseStyle = {
   }
 };
 
-export class FlexCardElement extends React.PureComponent<FlexCardElementProps> {
+export interface FlexCardElementState {
+  readyElements: string[];
+}
+
+export class FlexCardElement extends React.Component<
+  FlexCardElementProps,
+  FlexCardElementState
+> {
   /*TODO find some way to lock these based on this.props.disabled*/
   /*TODO add powered by Stripe badge - https://stripe.com/about/resources*/
 
+  public state = { readyElements: [] };
+
   public render(): React.ReactNode {
     return (
-      <div
-        css={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexWrap: "wrap"
-        }}
-      >
-        <FieldWrapper width="210px" label="Card Number" grow>
-          <CardNumberElement style={baseStyle} placeholder="Card Number" />
-        </FieldWrapper>
-        <FieldWrapper width="100px" label="Expiry Date">
-          <CardExpiryElement style={baseStyle} />
-        </FieldWrapper>
-        <FieldWrapper width="80px" label="CVC">
-          <CardCVCElement style={baseStyle} />
-        </FieldWrapper>
-      </div>
+      <>
+        <div
+          css={{
+            display: this.state.readyElements.length === 3 ? "flex" : "none",
+            justifyContent: "space-between",
+            flexWrap: "wrap"
+          }}
+        >
+          <FieldWrapper width="210px" label="Card Number" grow>
+            <CardNumberElement
+              style={baseStyle}
+              placeholder="Card Number"
+              onReady={this.markElementReady("CardNumberElement")}
+            />
+          </FieldWrapper>
+          <FieldWrapper width="100px" label="Expiry Date">
+            <CardExpiryElement
+              style={baseStyle}
+              onReady={this.markElementReady("CardExpiryElement")}
+            />
+          </FieldWrapper>
+          <FieldWrapper width="80px" label="CVC">
+            <CardCVCElement
+              style={baseStyle}
+              onReady={this.markElementReady("CardCVCElement")}
+            />
+          </FieldWrapper>
+        </div>
+        <div
+          css={{
+            display: this.state.readyElements.length === 3 ? "none" : "block"
+          }}
+        >
+          <Spinner loadingMessage="Preparing card details form..." />
+        </div>
+      </>
     );
   }
+
+  private markElementReady = (element: string) => () =>
+    this.setState(prevState => ({
+      readyElements: prevState.readyElements.concat(element)
+    }));
 }
