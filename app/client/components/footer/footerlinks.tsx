ebfd2bbfdd6031d6efddb2d305504b3015a8fb91
Tom Richards <tom.richards@guardian.co.uk>
2018-09-12 16:32:39 +0100

    all the dynamic footer links were missing the DOT before the domain

--- app/client/components/footer/footerlinks.tsx
+++ app/client/components/footer/footerlinks.tsx
@@ -16,63 +16,63 @@ export const footerLinks: FooterLink[][] = [
   [
     {
       title: "jobs",
-      link: `https://jobs${domain}/?INTCMP=NGW_FOOTER_UK_GU_JOBS`
+      link: `https://jobs.${domain}/?INTCMP=NGW_FOOTER_UK_GU_JOBS`
     },
     {
       title: "dating",
-      link: `https://soulmates${domain}/?INTCMP=NGW_FOOTER_UK_GU_SOULMATES`
+      link: `https://soulmates.${domain}/?INTCMP=NGW_FOOTER_UK_GU_SOULMATES`
     },
     {
       title: "make a contribution",
-      link: `https://support${domain}/contribute?INTCMP=footer_support_contribute&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_contribute%22%2C%22referrerPageviewId%22%3A%22jki3y55zbo7ri6mlon9r%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fprofile${domain}%2Fmembership%2Fedit%22%7D`
+      link: `https://support.${domain}/contribute?INTCMP=footer_support_contribute&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_contribute%22%2C%22referrerPageviewId%22%3A%22jki3y55zbo7ri6mlon9r%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fprofile.${domain}%2Fmembership%2Fedit%22%7D`
     },
     {
       title: "subscribe",
-      link: `https://support${domain}/subscribe?INTCMP=footer_support_subscribe&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_subscribe%22%2C%22referrerPageviewId%22%3A%22jki3y55zbo7ri6mlon9r%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fprofile${domain}%2Fmembership%2Fedit%22%7D`
+      link: `https://support.${domain}/subscribe?INTCMP=footer_support_subscribe&acquisitionData=%7B%22source%22%3A%22GUARDIAN_WEB%22%2C%22componentType%22%3A%22ACQUISITIONS_FOOTER%22%2C%22componentId%22%3A%22footer_support_subscribe%22%2C%22referrerPageviewId%22%3A%22jki3y55zbo7ri6mlon9r%22%2C%22referrerUrl%22%3A%22https%3A%2F%2Fprofile.${domain}%2Fmembership%2Fedit%22%7D`
     },
     {
       title: "guardian labs",
-      link: `https://www${domain}/guardian-labs`
+      link: `https://www.${domain}/guardian-labs`
     }
   ],
   [
     {
       title: "about us",
-      link: `https://www${domain}/about`
+      link: `https://www.${domain}/about`
     },
     {
       title: "work for us",
-      link: `https://workforus${domain}/locations/london`
+      link: `https://workforus.${domain}/locations/london`
     },
     {
       title: "advertise with us",
-      link: `https://advertising${domain}/`
+      link: `https://advertising.${domain}/`
     },
     {
       title: "contact us",
-      link: `https://www${domain}/help/contact-us`
+      link: `https://www.${domain}/help/contact-us`
     },
     {
       title: "help",
-      link: `https://www${domain}/help`
+      link: `https://www.${domain}/help`
     }
   ],
   [
     {
       title: "terms & conditions",
-      link: `https://www${domain}/help/terms-of-service`
+      link: `https://www.${domain}/help/terms-of-service`
     },
     {
       title: "privacy policy",
-      link: `https://www${domain}/info/privacy`
+      link: `https://www.${domain}/info/privacy`
     },
     {
       title: "cookie policy",
-      link: `https://www${domain}/info/cookies`
+      link: `https://www.${domain}/info/cookies`
     },
     {
       title: "securedrop",
-      link: `https://securedrop${domain}/`
+      link: `https://securedrop.${domain}/`
     },
     {
       title: "digital newspaper archive",
@@ -80,21 +80,21 @@ export const footerLinks: FooterLink[][] = [
     },
     {
       title: "complaints & corrections",
-      link: `https://www${domain}/info/complaints-and-corrections`
+      link: `https://www.${domain}/info/complaints-and-corrections`
     }
   ],
   [
     {
       title: "all topics",
-      link: `https://www${domain}/index/subjects/a`
+      link: `https://www.${domain}/index/subjects/a`
     },
     {
       title: "all contributors",
-      link: `https://www${domain}/index/contributors`
+      link: `https://www.${domain}/index/contributors`
     },
     {
       title: "modern slavery act",
-      link: `https://www${domain}/info/2016/jul/27/modern-slavery-and-our-supply-chains?INTCMP=NGW_FOOTER_UK_GU_MODERN_SLAVERY_ACT`
+      link: `https://www.${domain}/info/2016/jul/27/modern-slavery-and-our-supply-chains?INTCMP=NGW_FOOTER_UK_GU_MODERN_SLAVERY_ACT`
     },
     {
       title: "facebook",
