13795b4c49f551769c387ffa68b619a164c0636e
Tom Richards <tom.richards@guardian.co.uk>
2018-09-26 17:24:07 +0100

    tweak to copy and button alignment on 'No Membership' screen

--- app/client/components/cancel/membership/noMembership.tsx
+++ app/client/components/cancel/membership/noMembership.tsx
@@ -5,11 +5,9 @@ export const NoMembership = () => (
   <div>
     <h2>You do not currently have a membership.</h2>
     <p>
-      If you are interested in supporting our journalism in other ways, please
-      consider either a contribution or a subscription.
+      Please support our journalism by making either a contribution or a
+      subscription.
     </p>
-    <div css={{ textAlign: "right" }}>
-      <SupportTheGuardianButton supportReferer="no_membership_screen" />
-    </div>
+    <SupportTheGuardianButton supportReferer="no_membership_screen" />
   </div>
 );
