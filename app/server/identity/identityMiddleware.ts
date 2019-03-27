8afe05039cbf161aca0baf9c08f395a699ec43d3
Calum Campbell <campbell.cj@hotmail.co.uk>
2019-03-27 16:04:46 +0000

    add scope and update redirect response naming

--- app/server/identity/identityMiddleware.ts
+++ app/server/identity/identityMiddleware.ts
@@ -160,6 +160,7 @@ export const withIdentity: (statusCode?: number) => express.RequestHandler = (
             headers: {
               "X-GU-ID-Client-Access-Token":
                 "Bearer " + idapiConfig.accessToken,
+              "X-GU-ID-FORWARDED-SCOPE": "payment-flow",
               Cookie: getCookiesOrEmptyString(req)
             }
           }
@@ -180,7 +181,7 @@ export const withIdentity: (statusCode?: number) => express.RequestHandler = (
                 ),
                 statusCode
               );
-            } else if (redirectResponseBody.status === "ok") {
+            } else if (redirectResponseBody.status === "signedIn") {
               // If the request to manage contains sign-in token query parameters,
               // but they are not needed because the user is already signed in,
               // redirect them to the same url, but with the sign-in token query parameters removed.
