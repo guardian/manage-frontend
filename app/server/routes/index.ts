13133d0214e77e41449fe0b197bca628a2e71e93
Patrick Orrell <orrell@gmail.com>
2019-06-14 11:19:57 +0100

    Factor out product routes into a route provider

--- app/server/routes/index.ts
+++ app/server/routes/index.ts
@@ -2,3 +2,4 @@ export { default as core } from "./core";
 export { default as api } from "./api";
 export { default as profile } from "./profile";
 export { default as frontend } from "./frontend";
+export { default as productsProvider } from "./products";
--- app/server/server.ts
+++ app/server/server.ts
@@ -1,15 +1,13 @@
 import bodyParser from "body-parser";
-import express from "express";
+import {
+  default as express,
+  NextFunction,
+  Response
+} from "express";
 import helmet from "helmet";
 import Raven from "raven";
-import {
-  hasProductPageRedirect,
-  ProductType,
-  ProductTypes
-} from "../shared/productTypes";
 import { conf } from "./config";
 import { log } from "./log";
-import { membersDataApiHandler } from "./middleware/apiMiddleware";
 import * as routes from "./routes";
 
 const port = 9233;
@@ -35,7 +33,7 @@ server.use(helmet());
 server.use("/static", express.static(__dirname + "/static"));
 
 server.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  (_, res: Response, next: NextFunction) => {
     // this header is VERY IMPORTANT and prevents caching (on both CDN and in browsers)
     res.header(
       "Cache-Control",
@@ -51,37 +49,7 @@ server.use(bodyParser.raw({ type: "*/*" })); // parses all bodys to a raw 'Buffe
 server.use(routes.core);
 server.use("/api/", routes.api);
 server.use("/profile/", routes.profile);
-
-Object.values(ProductTypes).forEach((productType: ProductType) => {
-  server.use(
-    "/banner/" + productType.urlPart,
-    (req: express.Request, res: express.Response) => {
-      res.redirect("/payment/" + productType.urlPart + "?INTCMP=BANNER");
-    }
-  );
-
-  if (productType.updateAmountMdaEndpoint) {
-    server.post(
-      "/api/update/amount/" + productType.urlPart + "/:subscriptionName",
-      membersDataApiHandler(
-        "user-attributes/me/" +
-          productType.updateAmountMdaEndpoint +
-          "/:subscriptionName",
-        false,
-        "subscriptionName"
-      )
-    );
-  }
-  if (hasProductPageRedirect(productType)) {
-    server.get(
-      "/" + productType.urlPart,
-      (req: express.Request, res: express.Response) => {
-        res.redirect("/" + productType.productPage);
-      }
-    );
-  }
-});
-
+server.use(routes.productsProvider("/api/"));
 // ALL OTHER ENDPOINTS CAN BE HANDLED BY CLIENT SIDE REACT ROUTING
 server.use(routes.frontend);
 
