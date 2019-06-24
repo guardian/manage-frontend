c1e2956df9d90c1ebfe695e319144af12ff652c0
George Haberis <george.haberis@guardian.co.uk>
2019-06-24 14:56:06 +0100

    remove unused font styles

--- app/client/components/consent/App.tsx
+++ app/client/components/consent/App.tsx
@@ -1,6 +1,5 @@
 import { css, Global } from "@emotion/core";
 import React from "react";
-import { fonts } from "../../styles/fonts";
 // @ts-ignore
 import resetCSS from /* preval */ "../../styles/reset-css";
 import { ConsentManagementPortal } from "./ConsentMangementPortal";
@@ -8,7 +7,6 @@ import { ConsentManagementPortal } from "./ConsentMangementPortal";
 export const App = () => (
   <>
     <Global styles={css(`${resetCSS}`)} />
-    <Global styles={css(`${fonts}`)} />
     <ConsentManagementPortal />
   </>
 );
