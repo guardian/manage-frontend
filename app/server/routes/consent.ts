1d3d9a71692743ec7997f2d131f6a30d225e25ae
George Haberis <george.haberis@guardian.co.uk>
2019-06-25 13:57:09 +0100

    type validation fix

--- app/server/routes/consent.ts
+++ app/server/routes/consent.ts
@@ -5,7 +5,7 @@ import html from "../consent/html";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response) => {
+router.get("/", (_, res: Response) => {
   try {
     const polyfillIO =
       "https://assets.guim.co.uk/polyfill.io/v3/polyfill.min.js?rum=0&features=es6,es7,es2017,default-3.6,HTMLPictureElement,IntersectionObserver,IntersectionObserverEntry&flags=gated&callback=guardianPolyfilled&unknown=polyfill";
