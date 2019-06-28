718e856217a4cccb0632c5f56c2250384688d84b
George Haberis <george.haberis@guardian.co.uk>
2019-06-28 17:26:08 +0100

    more styles added
    (cherry picked from commit f189c3f8d62e7e28a9333737fb0973a314b607c7)

--- app/client/components/consent/CollapsePurposeItemButton.tsx
+++ app/client/components/consent/CollapsePurposeItemButton.tsx
@@ -2,14 +2,6 @@ import React from "react";
 import { css, cx } from "@emotion/core";
 import palette from "../../colours";
 
-const getTransform = (collapsed: boolean): string => {
-  if (collapsed) {
-    return "rotate(45deg);";
-  }
-
-  return "rotate(-135deg)";
-};
-
 const collapsePurposeItemButtonStyles = (collapsed: boolean) => css`
   background-color: transparent;
   border: 0;
@@ -20,20 +12,19 @@ const collapsePurposeItemButtonStyles = (collapsed: boolean) => css`
   position: relative;
   width: 20px;
   height: 20px;
-  float: left;
   > * {
     pointer-events: none;
   }
   :before {
     position: absolute;
-    top: 5px;
+    top: ${collapsed ? "7px" : "5px"};
     left: 6px;
     border: 2px solid ${palette.neutral[4]};
     border-top: 0;
     border-left: 0;
     content: "";
     display: inline-block;
-    transform: ${getTransform(collapsed)};
+    transform: ${collapsed ? "rotate(-135deg)" : "rotate(45deg)"};
     height: 6px;
     width: 6px;
   }
