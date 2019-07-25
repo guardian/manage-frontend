d59ce6210b2f30f9dac614c68215369ed0240500
Patrick Orrell <orrell@gmail.com>
2019-07-25 11:55:40 +0100

    Fix up DropMenu tests

--- app/client/__tests__/components/identity/DropMenu.tsx
+++ app/client/__tests__/components/identity/DropMenu.tsx
@@ -1,38 +1,58 @@
+import {
+  cleanup,
+  fireEvent,
+  render,
+  waitForElement,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
 import serializer from "jest-emotion";
 import React from "react";
-import { act, create } from "react-test-renderer";
+import { create } from "react-test-renderer";
 import { DropMenu } from "../../../components/identity/DropMenu";
 
 expect.addSnapshotSerializer(serializer);
 
+const ELEMENT_TEXT = {
+  TITLE: "TEST DROPMENU",
+  CONTENT: "TEST_CONTENT"
+};
+const element = (
+  <DropMenu color="blue" title={ELEMENT_TEXT.TITLE}>
+    <p>{ELEMENT_TEXT.CONTENT}</p>
+  </DropMenu>
+);
+
+afterEach(cleanup);
+
 describe("DropMenu", () => {
   it("initalises in the unopened state and displays the title", () => {
-    const rendered = create(
-      <DropMenu color={"red"} title="test component">
-        <h1>SHOULD NOT BE VISIBLE</h1>
-      </DropMenu>
-    );
-    expect(rendered.toJSON()).toMatchSnapshot();
+    const { container } = render(element);
+    expect(container).toMatchSnapshot();
   });
-  it.skip("@TODO: Make testing-library test: open on click", () => {
-    const rendered = create(
-      <DropMenu color={"red"} title="test component">
-        <h1>SHOULD BE VISIBLE</h1>
-      </DropMenu>
-    );
-    act(() => rendered.root.findByType("div").props.onClick());
-    expect(rendered.toJSON()).toMatchSnapshot();
+  it("if closed, opens and shows children on click", async () => {
+    const { container, getByText } = render(element);
+    fireEvent.click(getByText(ELEMENT_TEXT.TITLE));
+    await waitForElement(() => getByText(ELEMENT_TEXT.CONTENT));
+    expect(container).toMatchSnapshot();
   });
-  it.skip("@TODO: Make testing-library test: should close on click if already open", () => {
-    const rendered = create(
-      <DropMenu color={"red"} title="test component">
-        <h1>SHOULD NOT BE VISIBLE</h1>
-      </DropMenu>
-    );
-    act(() => {
-      rendered.root.findByType("div").props.onClick();
-      rendered.root.findByType("div").props.onClick();
+  it("if open, closes and hides children on click", async () => {
+    const { container, getByText } = render(element);
+    fireEvent.click(getByText(ELEMENT_TEXT.TITLE));
+    await waitForElement(() => getByText(ELEMENT_TEXT.CONTENT));
+    const promise = waitForElementToBeRemoved(() =>
+      getByText(ELEMENT_TEXT.CONTENT)
+    ).then(() => {
+      expect(container).toMatchSnapshot();
     });
-    expect(rendered.toJSON()).toMatchSnapshot();
+    fireEvent.click(getByText(ELEMENT_TEXT.TITLE));
+    return promise;
+  });
+  it("displays the text in the supplied color", () => {
+    const container = create(
+      <DropMenu color="green" title={ELEMENT_TEXT.TITLE}>
+        {ELEMENT_TEXT.CONTENT}
+      </DropMenu>
+    );
+    expect(container).toMatchSnapshot();
   });
 });
