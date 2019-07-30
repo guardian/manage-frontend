9d4f055fe5ebe4252ff4e31a6f971b6e24ce67d3
Patrick Orrell <orrell@gmail.com>
2019-07-30 14:57:01 +0100

    Tidy up main fetching function

--- app/client/components/identity/idapi/fetch.ts
+++ app/client/components/identity/idapi/fetch.ts
@@ -1,26 +1,32 @@
+const handleResponseFailure = async (response: Response) => {
+  let err;
+  try {
+    err = await response.json();
+  } catch (e) {
+    err = await response.text();
+  }
+  throw new Error(`Response error: ${err}`);
+};
+
+const handleResponseSuccess = async (response: Response) => {
+  try {
+    return await response.json();
+  } catch (e) {
+    throw new Error(`Error decoding JSON response: ${e}`);
+  }
+};
+
 export const APIFetch = (baseUrl: string) => async (
   url: string,
   options?: RequestInit
 ): Promise<any> => {
   const response = await fetch(baseUrl + url, options);
   if (!response.ok) {
-    let err;
-    try {
-      err = await response.json();
-    } catch (e) {
-      err = await response.text();
-    }
-    throw new Error(`Response error: ${err}`);
+    await handleResponseFailure(response);
   } else if (response.status === 204) {
     return null;
   } else {
-    let data;
-    try {
-      data = await response.json();
-    } catch (e) {
-      throw new Error(`Error decoding JSON response: ${e}`);
-    }
-    return data;
+    return await handleResponseSuccess(response);
   }
 };
 
