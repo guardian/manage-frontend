7bf3d2ab2346110699ca6a8eca4e55dc73e4875d
Patrick Orrell <orrell@gmail.com>
2019-08-06 11:59:17 +0100

    Add initial field and submit button (with a noop handler)

--- app/client/components/identity/PublicProfile/index.tsx
+++ app/client/components/identity/PublicProfile/index.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { headline } from "../../../styles/fonts";
+import { Button } from "../../buttons";
 import { navLinks } from "../../nav";
 import { PageContainer, PageHeaderContainer } from "../../page";
+import { Spinner } from "../../spinner";
+import { Users } from "../identity";
 import { IdentityLocations } from "../IdentityLocations";
 import { Lines } from "../Lines";
+import { PageSection } from "../PageSection";
 
 export const PublicProfile = (props: { path?: string }) => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    Users.getCurrentUser().then(setUser);
+  }, []);
+
+  const loader = (
+    <PageContainer>
+      <Spinner loadingMessage="Loading your profile ..." />
+    </PageContainer>
+  );
+
+  const content = () => (
+    <>
+      <PageContainer>
+        These details will be publicly visible to everyone who sees your profile
+        in the <a href={IdentityLocations.COMMUNITY_FAQS}>commenting</a>{" "}
+        section.
+      </PageContainer>
+      <PageContainer>
+        <Lines n={1} />
+      </PageContainer>
+      <PageContainer>
+        <PageSection title="Profile">
+          <label>
+            Location
+            <input type="text" value={user.location} />
+          </label>
+          <Button text="Save changes" onClick={() => void 0} />
+        </PageSection>
+      </PageContainer>
+    </>
+  );
+
   return (
     <>
       <PageHeaderContainer selectedNavItem={navLinks.publicProfile}>
@@ -21,14 +58,7 @@ export const PublicProfile = (props: { path?: string }) => {
           Edit your profile
         </h1>
       </PageHeaderContainer>
-      <PageContainer>
-        These details will be publicly visible to everyone who sees your profile
-        in the <a href={IdentityLocations.COMMUNITY_FAQS}>commenting</a>{" "}
-        section.
-      </PageContainer>
-      <PageContainer>
-        <Lines n={1} />
-      </PageContainer>
+      {user ? content() : loader}
     </>
   );
 };
