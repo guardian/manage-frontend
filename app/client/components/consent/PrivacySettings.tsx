44ee1d144888b6a0906a7acec25c4de009f5e300
Ricardo Costa <ricardo.costa@guardian.co.uk>
2019-08-23 12:16:04 +0100

    Relocated types

--- app/client/components/consent/PrivacySettings.tsx
+++ app/client/components/consent/PrivacySettings.tsx
@@ -83,50 +83,7 @@ const buttonStyles = css`
 `;
 
 interface State {
-  iabPurposes: { [key: number]: boolean | null };
-}
-
-interface IabPurpose {
-  id: number;
-  name: string;
-  description: string;
-}
-
-interface IabFeature {
-  id: number;
-  name: string;
-  description: string;
-}
-
-interface IabVendor {
-  id: number;
-  name: string;
-  policyUrl: string;
-  purposeIds: number[];
-  legIntPurposeIds: number[];
-  featureIds: number[];
-}
-
-interface ParsedIabVendor {
-  id: number;
-  name: string;
-  policyUrl: string;
-  purposeIds: number[];
-  legIntPurposeIds: number[];
-  featureIds: number[];
-  description: React.ReactNode;
-}
-
-interface IabVendorList {
-  vendorListVersion: number;
-  lastUpdated: string;
-  purposes: IabPurpose[];
-  features: IabFeature[];
-  vendors: IabVendor[];
-}
-
-interface ParsedIabVendorList extends IabVendorList {
-  vendors: ParsedIabVendor[];
+  iabPurposes: IabPurposeState;
 }
 
 export class PrivacySettings extends Component<{}, State> {
