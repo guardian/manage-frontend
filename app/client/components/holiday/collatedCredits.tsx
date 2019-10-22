5d13745d59c868bcd59dc6f1dca93216b779d6e1
Tom Richards <tom.richards@guardian.co.uk>
2019-10-22 15:22:15 +0100

    improved the credit collation algorithm

--- app/client/components/holiday/collatedCredits.tsx
+++ app/client/components/holiday/collatedCredits.tsx
@@ -5,26 +5,22 @@ interface CollatedCreditByInvoiceDate {
   [invoiceDateString: string]: number;
 }
 
-const collateCreditsByInvoiceDate = (
-  publicationsImpacted: HolidayStopDetail[]
-) =>
-  publicationsImpacted.reduce(
-    (accumulator, currentValue) => {
-      const credit = currentValue.actualPrice || currentValue.estimatedPrice;
-      if (currentValue.invoiceDate && credit) {
-        const invoiceDateAsString = currentValue.invoiceDate.format(
-          friendlyLongDateFormat
-        );
-        return {
-          ...accumulator,
-          [invoiceDateAsString]:
-            credit + (accumulator[invoiceDateAsString] || 0)
-        };
-      }
-      return {}; // if we don't have credits and dates for every entry we shouldn't show anything
-    },
-    {} as CollatedCreditByInvoiceDate
-  );
+const reduceCreditCallback = (
+  accumulator: CollatedCreditByInvoiceDate | null,
+  currentValue: HolidayStopDetail
+) => {
+  const credit = currentValue.actualPrice || currentValue.estimatedPrice;
+  if (accumulator && currentValue.invoiceDate && credit) {
+    const invoiceDateAsString = currentValue.invoiceDate.format(
+      friendlyLongDateFormat
+    );
+    return {
+      ...accumulator,
+      [invoiceDateAsString]: credit + (accumulator[invoiceDateAsString] || 0)
+    };
+  }
+  return null; // if we don't have credits and dates for EVERY entry we shouldn't show anything
+};
 
 export interface CollatedCreditsProps {
   publicationsImpacted: HolidayStopDetail[];
@@ -33,27 +29,29 @@ export interface CollatedCreditsProps {
 }
 
 export const CollatedCredits = (props: CollatedCreditsProps) => {
-  const collatedCreditsByInvoiceDate = collateCreditsByInvoiceDate(
-    props.publicationsImpacted
+  const collatedCreditsByInvoiceDate: CollatedCreditByInvoiceDate | null = props.publicationsImpacted.reduce(
+    reduceCreditCallback,
+    {} as CollatedCreditByInvoiceDate
   );
-
-  const invoiceDateStrings = Object.keys(collatedCreditsByInvoiceDate);
 
   return (
     <div>
-      {invoiceDateStrings.length === 0 && "Unavailable at this time."}
-      {invoiceDateStrings.map((invoiceDateString, index) => (
-        <div key={`cc-${index}`}>
-          {props.withBullet && "- "}
-          <strong>
-            {props.currency}
-            {Math.abs(collatedCreditsByInvoiceDate[invoiceDateString]).toFixed(
-              2
-            )}
-          </strong>{" "}
-          off your {invoiceDateString} payment
-        </div>
-      ))}
+      {collatedCreditsByInvoiceDate
+        ? Object.keys(collatedCreditsByInvoiceDate).map(
+            (invoiceDateString, index) => (
+              <div key={`cc-${index}`}>
+                {props.withBullet && "- "}
+                <strong>
+                  {props.currency}
+                  {Math.abs(
+                    collatedCreditsByInvoiceDate[invoiceDateString]
+                  ).toFixed(2)}
+                </strong>{" "}
+                off your {invoiceDateString} payment
+              </div>
+            )
+          )
+        : "Unavailable at this time."}
     </div>
   );
 };
