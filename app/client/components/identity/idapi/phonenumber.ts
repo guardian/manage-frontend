89d729ba31da184a1ee7cd94173efdcf3be0d0b5
Patrick Orrell <orrell@gmail.com>
2019-10-25 14:46:34 +0100

    Add missing credentials for telephone endpoint cors

--- app/client/components/identity/idapi/phonenumber.ts
+++ app/client/components/identity/idapi/phonenumber.ts
@@ -1,9 +1,9 @@
-import { identityFetch } from "./fetch";
+import { APIUseCredentials, identityFetch } from "./fetch";
 
 export const remove = async () => {
   const url = "/user/me/telephoneNumber";
   const options = {
     method: "DELETE"
   };
-  return await identityFetch(url, options);
+  return await identityFetch(url, APIUseCredentials(options));
 };
