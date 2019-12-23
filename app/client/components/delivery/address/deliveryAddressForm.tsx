43e565ff486748367daffa2f4b0c1c96605241fb
Richard Bangay <richard.bangay@gmail.com>
2019-12-23 11:50:14 +0000

    [RB] - replace flat method with flatMap ... babel seems to handle this better for browsers (safari, ie/edge) that don't support the flat method ... tmp fix until we mae babel play nice

--- app/client/components/delivery/address/deliveryAddressForm.tsx
+++ app/client/components/delivery/address/deliveryAddressForm.tsx
@@ -54,6 +54,8 @@ function hasContactId(
   return !!productDetail.subscription.contactId;
 }
 
+const babelFlatMapFunction = (x: any) => x;
+
 export const getValidDeliveryAddressChangeEffectiveDates = (
   allProductDetail: ProductDetail[]
 ) =>
@@ -134,7 +136,7 @@ const FormContainer = (props: FormContainerProps) => {
   const subscriptionsNames = Object.values(
     props.contactIdToArrayOfProductDetail
   )
-    .flat()
+    .flatMap(babelFlatMapFunction)
     .map(productDetail => {
       const friendlyProductName = ProductTypes.contentSubscriptions.mapGroupedToSpecific?.(
         productDetail
@@ -221,8 +223,9 @@ const FormContainer = (props: FormContainerProps) => {
               {Object.keys(props.contactIdToArrayOfProductDetail).length ===
                 1 && (
                 <div>
-                  {Object.values(props.contactIdToArrayOfProductDetail).flat()
-                    .length > 1 && (
+                  {Object.values(props.contactIdToArrayOfProductDetail).flatMap(
+                    babelFlatMapFunction
+                  ).length > 1 && (
                     <p
                       css={css`
                         border-top: 1px solid ${palette.neutral["86"]};
@@ -316,7 +319,7 @@ export const SubscriptionsAffectedList = (
         `}
       >
         {Object.values(props.contactIdDictOfProductDetails)
-          .flat()
+          .flatMap(babelFlatMapFunction)
           .map(productDetail => {
             const friendlyProductName = upperFirst(
               ProductTypes.contentSubscriptions.mapGroupedToSpecific?.(
