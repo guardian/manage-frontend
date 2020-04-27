ec22249e61786d8c826f19eafcab2d1fbbdc758a
Richard Bangay <richard.bangay@gmail.com>
2020-04-27 17:15:49 +0100

    [RB] - fix css error and moment format error

--- app/client/components/flowStartMultipleProductDetailHandler.tsx
+++ app/client/components/flowStartMultipleProductDetailHandler.tsx
@@ -300,13 +300,15 @@ export class FlowStartMultipleProductDetailHandler extends React.Component<
     return (
       <div
         css={
-          !this.props.hasLeftNav && {
-            padding: "0 0.625rem",
+          !this.props.hasLeftNav
+            ? {
+                padding: "0 0.625rem",
 
-            [minWidth.tablet]: {
-              padding: "0 1.25rem"
-            }
-          }
+                [minWidth.tablet]: {
+                  padding: "0 1.25rem"
+                }
+              }
+            : undefined
         }
       >
         {!this.props.hideHeading && (
