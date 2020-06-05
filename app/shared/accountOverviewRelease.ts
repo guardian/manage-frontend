4137918084a69eb2851caca4c003fe94e0f300a0
Tom Richards <tom.richards@guardian.co.uk>
2020-06-05 10:06:01 +0100

    force every user into the AccountOverview test (without yet removing any of the tracking or old code)

--- app/shared/accountOverviewRelease.ts
+++ app/shared/accountOverviewRelease.ts
@@ -1,8 +1,3 @@
-const TEST_IDENTITY_ID_SUFFIXES = ["0", "3", "5", "7", "9"];
-
 export const isIdentityInAccountOverviewTest = (
   identityID: string | undefined
-) =>
-  TEST_IDENTITY_ID_SUFFIXES.filter(
-    suffix => identityID && identityID.endsWith(suffix)
-  ).length > 0;
+) => true;
