f8c942172f7d77708c91d14b0745c16bd95a77e9
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-10-22 11:34:19 +0100

    Use originalUrl instead of path

--- app/shared/requiresSignin.ts
+++ app/shared/requiresSignin.ts
@@ -1,7 +1,7 @@
 import * as pathLib from "path";
 
 // To avoid security vulnerabilities do no add public paths that do not end in a slash
-const publicPaths = ["/contact-us/", "/contact-us-config/"];
+const publicPaths = ["/contact-us/", "/api/contact-us-config/"];
 
 export const requiresSignin = (path: string) => {
   const normalizedPath = pathLib.normalize(path + "/");
