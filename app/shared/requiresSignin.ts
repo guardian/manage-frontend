f717a63ff96c1d3303096a2e5e493d7b1b1c34fd
Ricardo Costa <ricardo.costa@guardian.co.uk>
2020-10-23 12:21:41 +0100

    Add Contact Us form endpoint + validation

--- app/shared/requiresSignin.ts
+++ app/shared/requiresSignin.ts
@@ -1,7 +1,11 @@
 import * as pathLib from "path";
 
 // To avoid security vulnerabilities do no add public paths that do not end in a slash
-const publicPaths = ["/contact-us/", "/api/contact-us-config/"];
+const publicPaths = [
+  "/contact-us/",
+  "/api/contact-us-config/",
+  "/api/contact-us/"
+];
 
 export const requiresSignin = (path: string) => {
   const normalizedPath = pathLib.normalize(path + "/");
