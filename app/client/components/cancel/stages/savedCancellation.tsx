7221d2fd9ae0f039b2e038fdc5f3377e20c65439
Tom Pretty <tompretty2@gmail.com>
2020-12-22 15:02:11 +0000

    Add return to account link on saved step

--- app/client/components/cancel/stages/savedCancellation.tsx
+++ app/client/components/cancel/stages/savedCancellation.tsx
@@ -1,5 +1,8 @@
 import { css } from "@emotion/core";
+import { LinkButton } from "@guardian/src-button";
 import { space } from "@guardian/src-foundations";
+import { SvgArrowLeftStraight } from "@guardian/src-icons";
+import { navigate } from "@reach/router";
 import * as Sentry from "@sentry/browser";
 import React from "react";
 import { GenericErrorMessage } from "../../identity/GenericErrorMessage";
@@ -8,23 +11,48 @@ import { ProgressIndicator } from "../../progressIndicator";
 import { RouteableStepProps, WizardStep } from "../../wizardRouterAdapter";
 import { CancellationReason } from "../cancellationReason";
 
-const innerContent = (reason: CancellationReason, updatedAmount: number) => (
-  <>
-    <ProgressIndicator
-      steps={[
-        { title: "Reason" },
-        { title: "Review" },
-        { title: "Confirmation", isCurrentStep: true }
-      ]}
-      additionalCSS={css`
-        margin: ${space[5]}px 0 ${space[12]}px;
-      `}
-    />
-    <WithStandardTopMargin>
-      {reason.savedBody && <reason.savedBody amount={updatedAmount} />}
-    </WithStandardTopMargin>
-  </>
-);
+const innerContent = (reason: CancellationReason, updatedAmount: number) => {
+  const onReturnClicked = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <ProgressIndicator
+        steps={[
+          { title: "Reason" },
+          { title: "Review" },
+          { title: "Confirmation", isCurrentStep: true }
+        ]}
+        additionalCSS={css`
+          margin: ${space[5]}px 0 ${space[12]}px;
+        `}
+      />
+      <WithStandardTopMargin>
+        {reason.savedBody && <reason.savedBody amount={updatedAmount} />}
+
+        <div
+          css={css`
+            margin-top: ${space[24]}px;
+          `}
+        >
+          <LinkButton
+            href="/"
+            onClick={onReturnClicked}
+            priority="tertiary"
+            icon={<SvgArrowLeftStraight />}
+            iconSide="left"
+          >
+            Return to your account
+          </LinkButton>
+        </div>
+      </WithStandardTopMargin>
+    </>
+  );
+};
 
 interface SavedCancellationStepProps extends RouteableStepProps {
   reason: CancellationReason;
