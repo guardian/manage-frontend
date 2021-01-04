0eeb605fe6bd6dfb76bfe401395ee928118bdabd
Richard Bangay <richard.bangay@gmail.com>
2021-01-04 11:45:29 +0000

    [RB] - remove the xmas and new years opening hours

--- app/client/components/callCenterEmailAndNumbers.tsx
+++ app/client/components/callCenterEmailAndNumbers.tsx
@@ -34,8 +34,6 @@ const PHONE_DATA: PhoneRegion[] = [
     key: "UK & ROW",
     title: "United Kingdom, Europe and rest of world",
     openingHours: "9am - 6pm Monday - Sunday (GMT/BST)",
-    additionalOpeningHoursInfo:
-      "Closed Christmas Day, Boxing Day and New Years Day",
     phoneNumbers: [
       {
         phoneNumber: "+44 (0) 330 333 6790"
@@ -46,7 +44,6 @@ const PHONE_DATA: PhoneRegion[] = [
     key: "AUS",
     title: "Australia, New Zealand, and Asia Pacific",
     openingHours: "9am - 5pm Monday - Friday (AEDT)",
-    additionalOpeningHoursInfo: "Closed Christmas Day",
     phoneNumbers: [
       {
         phoneNumber: "1800 773 766",
@@ -62,7 +59,6 @@ const PHONE_DATA: PhoneRegion[] = [
     key: "US",
     title: "Canada and USA",
     openingHours: "9am - 5pm on weekdays (EST/EDT)",
-    additionalOpeningHoursInfo: "Closed Christmas Eve and Christmas Day",
     phoneNumbers: [
       {
         phoneNumber: "1-844-632-2010",
