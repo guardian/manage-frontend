334173aa6d9adbe02298b62a5d07a6345a75ea3d
Maria <m.olanipekun@hotmail.com>
2021-03-09 12:58:12 +0000

    update the section header and page container components

--- app/client/components/sectionHeader.tsx
+++ app/client/components/sectionHeader.tsx
@@ -13,6 +13,7 @@ import { gridBase, gridItemPlacement } from "../styles/grid";
 
 interface SectionHeaderProps {
   title: string | JSX.Element;
+  isArticlePage?: boolean;
 }
 
 const chevronCss = css`
@@ -30,89 +31,100 @@ const isLandingPage =
   window.location.pathname === "/help-centre" ||
   window.location.pathname === "/help-centre/";
 
-export const SectionHeader = (props: SectionHeaderProps) => (
-  <header
-    css={{
-      backgroundColor: Color(palette.brand[800])
-        .alpha(0.3)
-        .string()
-    }}
-  >
-    <div
-      css={{
-        ...gridBase,
-        maxWidth: `${breakpoints.wide}px`,
-        margin: "0 auto",
-        borderLeft: `1px solid ${palette.neutral[86]}`,
-        borderRight: `1px solid ${palette.neutral[86]}`,
+export const SectionHeader = (props: SectionHeaderProps) => {
+  const gridPlacementDesktop = props.isArticlePage
+    ? gridItemPlacement(5, 9)
+    : gridItemPlacement(3, 10);
 
-        [minWidth.desktop]: {
-          ...(gridBase[minWidth.desktop] as object)
-        },
-        [minWidth.wide]: {
-          ...(gridBase[minWidth.wide] as object)
-        }
+  const gridPlacementWide = props.isArticlePage
+    ? gridItemPlacement(5, 16)
+    : gridItemPlacement(3, 14);
+
+  return (
+    <header
+      css={{
+        backgroundColor: Color(palette.brand[800])
+          .alpha(0.3)
+          .string()
       }}
     >
       <div
         css={{
-          marginTop: `${space[3]}px`,
-          ...gridItemPlacement(1, 12)
-        }}
-      >
-        <a
-          href="/help-centre"
-          css={css`
-            ${textSans.medium()};
-            color: ${palette.neutral[0]};
-            position: relative;
-          `}
-        >
-          {isLandingPage ? (
-            <span
-              css={css`
-                ${textSans.medium({ fontWeight: "bold" })};
-                color: ${palette.neutral[0]};
-              `}
-            >
-              Help Centre
-            </span>
-          ) : (
-            <span css={{ marginLeft: "1rem" }}>
-              <span css={chevronCss} />
-              Back to Help centre
-            </span>
-          )}
-        </a>
-      </div>
-      <h1
-        css={{
-          font: titlepiece.small(),
-          fontSize: "2rem",
-          ...gridItemPlacement(1, 4),
-          margin: `${space[9]}px 0 0 0`,
-          padding: `${space[3]}px 0`,
-
-          [minWidth.tablet]: {
-            ...gridItemPlacement(1, 12)
-          },
+          ...gridBase,
+          maxWidth: `${breakpoints.wide}px`,
+          margin: "0 auto",
+          borderLeft: `1px solid ${palette.neutral[86]}`,
+          borderRight: `1px solid ${palette.neutral[86]}`,
 
           [minWidth.desktop]: {
-            ...gridItemPlacement(3, 10),
-            fontSize: "2.625rem",
-            paddingLeft: `${space[5]}px`,
-            marginTop: `${space[24]}px`,
-            marginLeft: `-${space[5]}px`,
-            borderLeft: `1px solid ${palette.neutral[86]}`,
-            borderTop: `1px solid ${palette.neutral[86]}`
+            ...(gridBase[minWidth.desktop] as object)
           },
           [minWidth.wide]: {
-            ...gridItemPlacement(3, 14)
+            ...(gridBase[minWidth.wide] as object)
           }
         }}
       >
-        {props.title}
-      </h1>
-    </div>
-  </header>
-);
+        <div
+          css={{
+            marginTop: `${space[3]}px`,
+            ...gridItemPlacement(1, 12)
+          }}
+        >
+          <a
+            href="/help-centre"
+            css={css`
+              ${textSans.medium()};
+              color: ${palette.neutral[0]};
+              position: relative;
+            `}
+          >
+            {isLandingPage ? (
+              <span
+                css={css`
+                  ${textSans.medium({ fontWeight: "bold" })};
+                  color: ${palette.neutral[0]};
+                `}
+              >
+                Help Centre
+              </span>
+            ) : (
+              <span css={{ marginLeft: "1rem" }}>
+                <span css={chevronCss} />
+                Back to Help Centre
+              </span>
+            )}
+          </a>
+        </div>
+        <h1
+          css={{
+            font: titlepiece.small(),
+            fontSize: "2rem",
+            ...gridItemPlacement(1, 12),
+            margin: `${space[9]}px 0 0 0`,
+            padding: `${space[3]}px 0`,
+
+            [minWidth.tablet]: {
+              ...gridItemPlacement(1, 12)
+            },
+
+            [minWidth.desktop]: {
+              ...gridPlacementDesktop,
+              fontSize: "2.625rem",
+              paddingLeft: `${space[5]}px`,
+              marginTop: `${space[24]}px`,
+              marginLeft: `-${space[5]}px`,
+              borderLeft: `1px solid ${palette.neutral[86]}`,
+              borderTop: `1px solid ${palette.neutral[86]}`
+            },
+            [minWidth.wide]: {
+              ...gridPlacementWide,
+              marginLeft: `${props.isArticlePage ? "0px" : `-${space[5]}px`}`
+            }
+          }}
+        >
+          {props.title}
+        </h1>
+      </div>
+    </header>
+  );
+};
--- app/client/components/sectionPageContainer.tsx
+++ app/client/components/sectionPageContainer.tsx
@@ -1,56 +1,83 @@
 import { breakpoints, palette, space } from "@guardian/src-foundations";
 import React, { ReactNode } from "react";
-import { minWidth } from "../styles/breakpoints";
+import { maxWidth, minWidth } from "../styles/breakpoints";
 import { gridBase, gridItemPlacement } from "../styles/grid";
+import { HelpCentreNavConfig } from "./helpCentre/helpCentreConfig";
+import HelpCentreNav from "./helpCentre/helpCentreNav";
 
 interface SectionPageContainerProps {
   children: ReactNode;
+  hasNav?: boolean;
+  selectedTopicObject?: HelpCentreNavConfig;
 }
 
-export const SectionPageContainer = (props: SectionPageContainerProps) => (
-  <div
-    css={{
-      maxWidth: `${breakpoints.wide}px`,
-      margin: "0 auto",
-      paddingTop: `${space[12]}px`,
-      borderLeft: `1px solid ${palette.neutral[86]}`,
-      borderRight: `1px solid ${palette.neutral[86]}`,
-      height: "100%"
-    }}
-  >
+const sectionCss = (hasNav: boolean, isNavSection: boolean) => ({
+  ...gridItemPlacement(1, 4),
+  ...(isNavSection && { marginRight: "-13px", marginLeft: "-13px" }),
+
+  [minWidth.tablet]: {
+    ...gridItemPlacement(1, 12),
+    ...(isNavSection && { marginRight: "-21px", marginLeft: "-21px" })
+  },
+
+  [minWidth.desktop]: {
+    ...(isNavSection
+      ? gridItemPlacement(1, 3)
+      : hasNav
+      ? gridItemPlacement(5, 11)
+      : gridItemPlacement(3, 9)),
+    ...(isNavSection && { marginRight: "-21px", marginLeft: "-21px" })
+  },
+
+  [minWidth.wide]: {
+    ...(isNavSection
+      ? gridItemPlacement(1, 3)
+      : hasNav
+      ? gridItemPlacement(5, 9)
+      : gridItemPlacement(3, 12))
+  }
+});
+
+export const SectionPageContainer = (props: SectionPageContainerProps) => {
+  return (
     <div
       css={{
-        ...gridBase,
-        paddingBottom: "1rem",
-        [minWidth.desktop]: {
-          ...(gridBase[minWidth.desktop] as object),
-          paddingBottom: 0,
-          borderTop: `1px solid ${palette.neutral[86]}`
-        },
-        [minWidth.wide]: {
-          ...(gridBase[minWidth.wide] as object)
+        maxWidth: `${breakpoints.wide}px`,
+        margin: "0 auto",
+        paddingTop: `${space[12]}px`,
+        borderLeft: `1px solid ${palette.neutral[86]}`,
+        borderRight: `1px solid ${palette.neutral[86]}`,
+        height: "100%",
+        [maxWidth.desktop]: {
+          paddingTop: 0
         }
       }}
     >
-      <section
+      <div
         css={{
-          ...gridItemPlacement(1, 4),
-
-          [minWidth.tablet]: {
-            ...gridItemPlacement(1, 12)
-          },
-
+          ...gridBase,
+          paddingBottom: "1rem",
           [minWidth.desktop]: {
-            ...gridItemPlacement(3, 9)
+            ...(gridBase[minWidth.desktop] as object),
+            paddingBottom: 0,
+            borderTop: `${
+              props.hasNav ? "none" : `1px solid ${palette.neutral[86]}`
+            }`
           },
-
           [minWidth.wide]: {
-            ...gridItemPlacement(3, 12)
+            ...(gridBase[minWidth.wide] as object)
           }
         }}
       >
-        {props.children}
-      </section>
+        {props.hasNav && (
+          <section css={sectionCss(props.hasNav || false, true)}>
+            <HelpCentreNav selectedTopicObject={props.selectedTopicObject} />
+          </section>
+        )}
+        <section css={sectionCss(props.hasNav || false, false)}>
+          {props.children}
+        </section>
+      </div>
     </div>
-  </div>
-);
+  );
+};
