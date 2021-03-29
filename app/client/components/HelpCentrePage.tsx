14073eeda4a0203fd4ca7489d9b77323a5293f94
Ricardo Costa <ricardo.costa@guardian.co.uk>
2021-03-29 10:39:34 +0100

    Comment out Article and Page path

--- app/client/components/HelpCentrePage.tsx
+++ app/client/components/HelpCentrePage.tsx
@@ -17,17 +17,17 @@ const HelpCentre = lazy(() =>
   import(/* webpackChunkName: "HelpCentre" */ "./helpCentre/helpCentre")
 );
 
-const HelpCentreArticle = lazy(() =>
-  import(
-    /* webpackChunkName: "HelpCentreArticle" */ "./helpCentre/helpCentreArticle"
-  )
-);
+// const HelpCentreArticle = lazy(() =>
+//   import(
+//     /* webpackChunkName: "HelpCentreArticle" */ "./helpCentre/helpCentreArticle"
+//   )
+// );
 
-const HelpCentreTopic = lazy(() =>
-  import(
-    /* webpackChunkName: "HelpCentreTopic" */ "./helpCentre/helpCentreTopic"
-  )
-);
+// const HelpCentreTopic = lazy(() =>
+//   import(
+//     /* webpackChunkName: "HelpCentreTopic" */ "./helpCentre/helpCentreTopic"
+//   )
+// );
 
 const ContactUs = lazy(() =>
   import(/* webpackChunkName: "ContactUs" */ "./contactUs/contactUs")
@@ -42,8 +42,8 @@ const HelpCentreRouter = () => {
         <Router primary={true} css={{ height: "100%" }}>
           <HelpCentre path="/help-centre" />
 
-          <HelpCentreArticle path="/help-centre/article/:articleCode" />
-          <HelpCentreTopic path="/help-centre/topic/:topicCode" />
+          {/* <HelpCentreArticle path="/help-centre/article/:articleCode" />
+          <HelpCentreTopic path="/help-centre/topic/:topicCode" /> */}
 
           <ContactUs path="/help-centre/contact-us" />
           <ContactUs path="/help-centre/contact-us/:urlTopicId" />
