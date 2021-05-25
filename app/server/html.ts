8cf0f49aa253572e9f606433114e4d2dd2614941
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-05-25 14:09:05 +0100

    Add lang attribute to docs

--- app/server/html.ts
+++ app/server/html.ts
@@ -22,7 +22,7 @@ const html: (_: {
   readonly globals: Globals;
 }) => string = ({ title, src, globals }) => `
   <!DOCTYPE html>
-  <html>
+  <html lang="en">
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
