5b0702acf293d9fd8c03287668b2dcf5681fdffa
Kelvin Chappell <kelvin.chappell@theguardian.com>
2021-06-03 14:32:08 +0100

    Add robots.txt to control indexing

--- app/server/routes/core.ts
+++ app/server/routes/core.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { authKeysAreFetchableMemoisedHealthcheck } from "../apiGatewayDiscovery";
+import { Environments } from "../config";
 import { withIdentity } from "../middleware/identityMiddleware";
 
 const router = Router();
@@ -19,6 +20,25 @@ router.get(
 
 router.get("/_prout", (_, res: Response) => {
   res.send(`<pre>${GIT_COMMIT_HASH}</pre>`);
+});
+
+router.get("/robots.txt", (_, res: Response) => {
+  const disallowAll = "UserAgent: *\n" + "Disallow: /\n\n";
+  const allowHelpCentre =
+    "UserAgent: *\n" +
+    "Allow: /help-centre\n" +
+    "Allow: /help-centre/\n" +
+    "Disallow: /\n\n";
+  const disallowGoogleAdsBots =
+    "User-agent: AdsBot-Google\n" +
+    "User-agent: AdsBot-Google-Mobile\n" +
+    "Disallow: /\n\n";
+  const prodAccessList = allowHelpCentre + disallowGoogleAdsBots;
+  const preProdAccessList = disallowAll;
+  const accessList = Environments.PRODUCTION
+    ? prodAccessList
+    : preProdAccessList;
+  res.contentType("text/plain").send(accessList);
 });
 
 export default router;
