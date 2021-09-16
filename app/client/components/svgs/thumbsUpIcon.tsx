096e6d62c09ec0ce232e2e0e3f8c9f6d6e736c6c
Richard Bangay <richard.bangay@gmail.com>
2021-09-16 09:08:16 +0100

    [RB] - fix svg transform rotate bug in safari

--- app/client/components/svgs/thumbsUpIcon.tsx
+++ app/client/components/svgs/thumbsUpIcon.tsx
@@ -7,16 +7,12 @@ interface ThumbsUpIconProps {
 }
 
 export const ThumbsUpIcon = (props: ThumbsUpIconProps) => (
-  <svg
-    width="20"
-    height="24"
-    viewBox="0 0 20 24"
-    fill="none"
-    {...(props.invertIcon && { transform: "rotate(175)" })}
-  >
-    <path
-      d="M19.2261 13.3474L16.3955 22.7067L15.3096 23.4317L0 21.1823V8.70523L5.22193 7.68963L12.9156 0H14.0028L15.959 1.95889L12.9142 7.76327H12.9859L20 8.64029"
-      fill={props.overrideFillColor || neutral[100]}
-    />
+  <svg width="20" height="24" viewBox="0 0 20 24" fill="none">
+    <g {...(props.invertIcon && { transform: "rotate(175 10 12)" })}>
+      <path
+        d="M19.2261 13.3474L16.3955 22.7067L15.3096 23.4317L0 21.1823V8.70523L5.22193 7.68963L12.9156 0H14.0028L15.959 1.95889L12.9142 7.76327H12.9859L20 8.64029"
+        fill={props.overrideFillColor || neutral[100]}
+      />
+    </g>
   </svg>
 );
