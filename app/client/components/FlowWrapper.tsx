558bec7dc79efcc1c24b1f271b1b8e7f646e6bd5
Jonathan Flynn <37303488+01jonathanf@users.noreply.github.com>
2021-10-27 11:39:31 +0100

    formatting and spacing :)

--- app/client/components/FlowWrapper.tsx
+++ app/client/components/FlowWrapper.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { PageContainer, PageContainerProps } from "./page";
 import {
-    ProductDetailProvider,
-    ProductDetailProviderProps
+  ProductDetailProvider,
+  ProductDetailProviderProps
 } from "./productDetailProvider";
 
 type FlowWrapperProps = PageContainerProps & ProductDetailProviderProps;
 
 export const FlowWrapper = (props: FlowWrapperProps) => (
-    <PageContainer {...props}>
-        <ProductDetailProvider {...props}>
-            {productDetail => props.children(productDetail)}
-        </ProductDetailProvider>
-    </PageContainer>
+  <PageContainer {...props}>
+    <ProductDetailProvider {...props}>
+      {productDetail => props.children(productDetail)}
+    </ProductDetailProvider>
+  </PageContainer>
 );
