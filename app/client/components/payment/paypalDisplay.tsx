e926429811f26b199583e628ab5252b01f9b2a5d
James Mockett <james.mockett@guardian.co.uk>
2021-12-06 11:26:54 +0000

    Fix obfuscation code so short IDs don't throw an error
    Currently the PayPal ID masking code assumes IDs are at least 3
    characters long. Shorter IDs result in a JS error, preventing the page
    from loading. Whilst this is an edge case we have at least one supporter
    with a single character email address who is unable to manage their
    account due to this.

--- app/client/components/payment/paypalDisplay.tsx
+++ app/client/components/payment/paypalDisplay.tsx
@@ -7,15 +7,13 @@ interface PayPalProps {
 }
 
 export const getObfuscatedPayPalId = (rawId: string) => {
-  const indexOfAtSymbol = rawId.indexOf("@");
-  if (indexOfAtSymbol > -1) {
-    return `${rawId.charAt(0)}${"*".repeat(indexOfAtSymbol - 2)}${rawId.charAt(
-      indexOfAtSymbol - 1
-    )}${rawId.substring(indexOfAtSymbol)}`;
-  }
-  return `${rawId.charAt(0)}${"*".repeat(rawId.length - 2)}${rawId.charAt(
-    rawId.length - 1
-  )}`;
+  return rawId.replace(
+    /^(.)(.*?)(.?|.?@.+)$/,
+    (_, firstChar, remainingChars, lastChar) => {
+      const maskedRemainingChars = remainingChars.replace(/./g, "*");
+      return firstChar + maskedRemainingChars + lastChar;
+    }
+  );
 };
 
 export const PayPalDisplay = (props: PayPalProps) => (
