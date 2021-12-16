9045a549d3dd50b785cd6e2f9bbb269c366ab4a8
James Mockett <james.mockett@guardian.co.uk>
2021-12-16 16:40:44 +0000

    Remove Source link component

--- app/client/components/helpCentre/BackToHelpCentreLink.tsx
+++ app/client/components/helpCentre/BackToHelpCentreLink.tsx
@@ -1,24 +1,43 @@
 import { css } from "@emotion/core";
-import { space } from "@guardian/src-foundations";
+import { palette, space } from "@guardian/src-foundations";
 import { neutral } from "@guardian/src-foundations/palette";
+import { textSans } from "@guardian/src-foundations/typography";
 import { SvgChevronLeftSingle } from "@guardian/src-icons";
-import { Link } from "@guardian/src-link";
+import { Link } from "@reach/router";
 import React from "react";
 
-const linkDividerCss = css`
+const dividerCss = css`
   margin-top: ${space[12]}px;
   padding-top: ${space[9]}px;
   border-top: 1px solid ${neutral["86"]};
 `;
 
+const linkCss = css`
+  display: flex;
+  align-items: center;
+  ${textSans.medium()};
+  color: ${palette.brand[500]};
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
+
+const linkIconCss = css`
+  width: 18px;
+  height: 18px;
+  margin: 0 4px 2px -4px;
+  & > svg {
+    stroke: currentColor;
+  }
+`;
+
 export const BackToHelpCentreLink = () => (
-  <div css={linkDividerCss}>
-    <Link
-      icon={<SvgChevronLeftSingle />}
-      iconSide="left"
-      subdued
-      href="/help-centre"
-    >
+  <div css={dividerCss}>
+    <Link to="/help-centre" css={linkCss}>
+      <span css={linkIconCss}>
+        <SvgChevronLeftSingle />
+      </span>
       Back to Help Centre
     </Link>
   </div>
