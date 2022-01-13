786decfed05483a097c10e47bc1bb6e3a4f4d611
James Mockett <james.mockett@guardian.co.uk>
2022-01-13 16:45:39 +0000

    Remove superfluous control matchers

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -17,10 +17,4 @@ export const decorators = [
 export const parameters = {
   viewport,
   actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
 };
