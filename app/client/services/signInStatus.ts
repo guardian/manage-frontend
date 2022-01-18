d9658ae996bf86952d82a0ca66e6ca1ffd0b1921
James Mockett <james.mockett@guardian.co.uk>
2022-01-18 18:08:51 +0000

    Extract sign in status logic and raise state to page components

---
+++ app/client/services/signInStatus.ts
@@ -0,0 +1,10 @@
+import { requiresSignin } from "../../shared/requiresSignin";
+
+export type SignInStatus = "init" | "signedIn" | "signedOut";
+
+export const isSignedIn = () =>
+  typeof window !== "undefined" &&
+  window.guardian?.identityDetails?.signInStatus === "signedInRecently";
+
+export const pageRequiresSignIn = () =>
+  requiresSignin(typeof window !== "undefined" ? window.location.pathname : "");
