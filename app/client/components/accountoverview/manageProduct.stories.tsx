7883b159e89d56c931c0f1e87a8bb0ade906df4b
James Mockett <james.mockett@guardian.co.uk>
2022-01-28 17:09:35 +0000

    Add stories for ManageProduct

---
+++ app/client/components/accountoverview/manageProduct.stories.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import ManageProduct from "./manageProduct";
+import { GROUPED_PRODUCT_TYPES } from "../../../shared/productTypes";
+
+import {
+  guardianWeeklyCard,
+  digitalDD,
+  newspaperVoucherPaypal,
+} from "../../fixtures/productDetail";
+
+export default {
+  title: "Pages/ManageProduct",
+  component: ManageProduct,
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof ManageProduct>;
+
+export const GuardianWeekly: ComponentStory<typeof ManageProduct> = () => (
+  <ManageProduct
+    path="/"
+    groupedProductType={GROUPED_PRODUCT_TYPES.subscriptions}
+    location={{ state: { productDetail: guardianWeeklyCard } }}
+  />
+);
+
+export const NewspaperSubscriptionCard: ComponentStory<
+  typeof ManageProduct
+> = () => (
+  <ManageProduct
+    path="/"
+    groupedProductType={GROUPED_PRODUCT_TYPES.subscriptions}
+    location={{ state: { productDetail: digitalDD } }}
+  />
+);
+
+export const DigitalSubscription: ComponentStory<typeof ManageProduct> = () => (
+  <ManageProduct
+    path="/"
+    groupedProductType={GROUPED_PRODUCT_TYPES.subscriptions}
+    location={{ state: { productDetail: newspaperVoucherPaypal } }}
+  />
+);
