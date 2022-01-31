1c58093aec877f66d17af11da1ff36236a7f419d
James Mockett <james.mockett@guardian.co.uk>
2022-01-31 18:33:01 +0000

    Add story for BasicProductInfoTable

---
+++ app/client/components/BasicProductInfoTable.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { BasicProductInfoTable } from "./basicProductInfoTable";
+import { newspaperVoucherPaypal } from "../fixtures/productDetail";
+import { GROUPED_PRODUCT_TYPES } from "../../shared/productTypes";
+
+export default {
+  title: "Components/BasicProductInfoTable",
+  component: BasicProductInfoTable,
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+} as ComponentMeta<typeof BasicProductInfoTable>;
+
+export const Default: ComponentStory<typeof BasicProductInfoTable> = () => (
+  <BasicProductInfoTable
+    groupedProductType={GROUPED_PRODUCT_TYPES.subscriptions}
+    productDetail={newspaperVoucherPaypal}
+  />
+);
