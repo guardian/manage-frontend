0659e19ef27517e7078c07f6260b5a928012b467
James Mockett <james.mockett@guardian.co.uk>
2022-02-15 16:41:09 +0000

    Add eslint-config-prettier to avoid conflicting rules

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:jest/recommended",
     "plugin:@typescript-eslint/recommended",
+	"prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
