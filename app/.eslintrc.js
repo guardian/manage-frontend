5eadc94ad9a2c6b4c92f4c5867dd370c64d2c1db
vanessa <32312712+vlbee@users.noreply.github.com>
2022-02-15 09:59:34 +0000

    Update eslintrc

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -34,7 +34,6 @@ module.exports = {
     "@typescript-eslint/ban-types": ["off"],
     "@typescript-eslint/explicit-module-boundary-types": ["off"],
     "@typescript-eslint/no-inferrable-types": ["off"],
-    "@typescript-eslint/no-explicit-any": ["off"],
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "_" }],
     "@typescript-eslint/ban-ts-comment": [
       "error",
