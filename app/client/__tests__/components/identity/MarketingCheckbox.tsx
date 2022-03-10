258d748dd429f48bb1e1d062514bf3da485fc053
vanessa <32312712+vlbee@users.noreply.github.com>
2022-03-10 11:59:03 +0000

    Split MarketingPreference into Toggle | Checkbox

---
+++ app/client/__tests__/components/identity/MarketingCheckbox.tsx
@@ -0,0 +1,52 @@
+import { cleanup, render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MarketingCheckbox } from '../../../components/identity/MarketingCheckbox';
+
+afterEach(cleanup);
+
+describe('MarketingCheckbox component', () => {
+	const input = {
+		id: '13',
+		description: 'Test description',
+		title: 'Test title',
+		clickHandler: jest.fn(),
+	};
+
+	const marketingCheckbox = (selected: boolean) => (
+		<MarketingCheckbox
+			id={input.id}
+			description={input.description}
+			title={input.title}
+			selected={selected}
+			onClick={input.clickHandler}
+		/>
+	);
+
+	it('renders correctly and displays marketing information', () => {
+		render(marketingCheckbox(false));
+
+		const checkboxRoles = screen.getAllByRole('checkbox');
+		expect(checkboxRoles).toHaveLength(2);
+		checkboxRoles.map((role) => expect(role).not.toBeChecked());
+
+		expect(screen.getByText('Test title')).toBeVisible();
+		expect(screen.getByText('Test description')).toBeVisible();
+	});
+
+	it('will select the checkbox when the selected prop is passed', () => {
+		render(marketingCheckbox(true));
+
+		const checkboxRoles = screen.getAllByRole('checkbox');
+		checkboxRoles.map((role) => expect(role).toBeChecked());
+	});
+
+	it('will call the click handler when it is clicked', () => {
+		render(marketingCheckbox(false));
+
+		const checkboxRoles = screen.getAllByRole('checkbox');
+		fireEvent.click(checkboxRoles[0]);
+
+		expect(input.clickHandler).toHaveBeenCalledTimes(1);
+		expect(input.clickHandler).toHaveBeenLastCalledWith(input.id);
+	});
+});
---
+++ app/client/components/identity/MarketingToggle.tsx
@@ -0,0 +1,71 @@
+import { FC } from 'react';
+import { sans } from '../../styles/fonts';
+import { css } from '@emotion/core';
+import { ToggleSwitch } from './ToggleSwitch';
+
+interface MarketingToggleProps {
+	id: string;
+	description?: string;
+	title?: string;
+	selected?: boolean;
+	onClick: (id: string) => {};
+}
+
+const standardText = {
+	fontFamily: sans,
+	fontSize: '14px',
+	lineHeight: '1.333',
+};
+
+const getDescription = (description: MarketingToggleProps['description']) => (
+	<p
+		css={[
+			standardText,
+			{
+				margin: '0',
+				padding: '2.88px 90px 0 0',
+			},
+		]}
+	>
+		{description}
+	</p>
+);
+
+export const MarketingToggle: FC<MarketingToggleProps> = (props) => {
+	const { id, description, selected, title, onClick } = props;
+	return (
+		<div
+			css={[
+				standardText,
+				{
+					marginTop: '12px',
+					position: 'relative',
+				},
+			]}
+		>
+			<div css={{ left: 0 }}>
+				<ToggleSwitch
+					cssOverrides={css`
+						label {
+							${standardText};
+							padding-right: 20px;
+							font-weight: bold;
+						}
+						button {
+							align-self: flex-start;
+							cursor: pointer;
+						}
+					`}
+					label={title}
+					labelPosition="left"
+					labelId={id}
+					checked={!!selected}
+					onClick={(_) => {
+						onClick(id);
+					}}
+				/>
+			</div>
+			{description && getDescription(description)}
+		</div>
+	);
+};
