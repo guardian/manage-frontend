edb7a25a725dbb961954c91cf50971787919f964
vanessa <32312712+vlbee@users.noreply.github.com>
2022-03-10 12:05:28 +0000

    Introduce toggle switch to Emails & Marketing Page

--- app/client/components/identity/EmailAndMarketing/OptOutSection.tsx
+++ app/client/components/identity/EmailAndMarketing/OptOutSection.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import { Lines } from '../Lines';
-import { MarginWrapper } from '../MarginWrapper';
-import { MarketingPreference } from '../MarketingPreference';
-import { ConsentOption } from '../models';
+import { WithStandardTopMargin } from '../../WithStandardTopMargin';
+import { MarketingToggle } from '../MarketingToggle';
+import { ConsentOption, ConsentOptionType } from '../models';
 import { PageSection } from '../PageSection';
 
 type ClickHandler = (id: string) => {};
@@ -12,28 +12,83 @@ interface OptOutSectionProps {
 	clickHandler: ClickHandler;
 }
 
-const optOutFinder =
+/**
+ * NOTE:
+ * Only use this method for an OPT OUT consent, eg. "post_optout"
+ * The description of Opt Out consents have changed so for UX/UI purposes they are now opt INs
+ * The backend model remains an opt OUT, so we invert the consented/subscribed value here.
+ */
+const optOutFinderAndInverter =
 	(consents: ConsentOption[], clickHandler: ClickHandler) => (id: string) => {
 		const consent = consents.find((c) => c.id === id);
+
 		return (
 			consent && (
-				<MarketingPreference
+				<MarketingToggle
 					id={consent.id}
-					description={consent.description}
-					selected={consent.subscribed}
+					title={consent.name}
+					description={consent.description} // Not all consents from IDAPI have a description
+					selected={!consent.subscribed} // Opt Out consent value is inverted
 					onClick={clickHandler}
 				/>
 			)
 		);
 	};
 
-const standardTextSize = {
-	fontSize: '17px',
-};
+const YourDataDescription: FC = () => (
+	<>
+		<p
+			css={{
+				marginBottom: '6px',
+			}}
+		>
+			What we mean by your data:
+		</p>
+		<ul
+			css={{
+				paddingInlineStart: '20px',
+				marginBlockStart: '0',
+			}}
+		>
+			<li>Information you provide e.g. email address</li>
+			<li>Products or services you buy from us</li>
+			<li>
+				Pages you view on theguardian.com or other Guardian websites
+				when signed in
+			</li>
+		</ul>
+	</>
+);
 
 export const OptOutSection: FC<OptOutSectionProps> = (props) => {
 	const { consents, clickHandler } = props;
-	const getOptOut = optOutFinder(consents, clickHandler);
+
+	// TODO: Replace consentsHardcoded with props.consents once API Change released
+	const consentsHardcoded: ConsentOption[] = consents.map((consent) => {
+		const match = consentsFixture.find(
+			(hardConsent) => hardConsent.id === consent.id,
+		);
+		if (match) {
+			return {
+				id: consent.id,
+				isChannel: consent.isChannel,
+				isProduct: consent.isProduct,
+				type: consent.type,
+				subscribed: consent.subscribed,
+				name: match.name,
+				...('description' in match && {
+					description: match.description,
+				}),
+			};
+		}
+		return consent;
+	});
+
+	const addMarketingToggle = optOutFinderAndInverter(
+		consentsHardcoded,
+		clickHandler,
+	);
+
 	return (
 		<>
 			<PageSection
@@ -43,39 +98,59 @@ export const OptOutSection: FC<OptOutSectionProps> = (props) => {
         and services via telephone and post.
       `}
 			>
-				<p css={standardTextSize}>
-					Please tick the boxes below to let us know if you{' '}
-					<strong>do not wish to receive</strong> information via any
-					of these channels:
-				</p>
-				{getOptOut('post_optout')}
-				{getOptOut('phone_optout')}
-				<h2 css={[standardTextSize, { fontWeight: 'bold' }]}>
-					Market Research
-				</h2>
-				<p css={standardTextSize}>
-					From time to time we may contact you for market research
-					purposes inviting you to complete a survey, or take part in
-					a group discussion. Normally, this invitation would be sent
-					via email, but we may also contact you by phone.
-				</p>
-				{getOptOut('market_research_optout')}
+				{addMarketingToggle('post_optout')}
+				{addMarketingToggle('phone_optout')}
+				{addMarketingToggle('market_research_optout')}
 			</PageSection>
-			<MarginWrapper>
+			<WithStandardTopMargin>
 				<Lines n={1} />
-			</MarginWrapper>
-			<PageSection title="Using your data for marketing analysis">
-				<p css={standardTextSize}>
-					From time to time we may use your personal data for
-					marketing analysis. That includes looking at what products
-					or services you have bought from us and what pages you have
-					been viewing on theguardian.com and other Guardian websites
-					(e.g. Guardian Jobs or Guardian Holidays). We do this to
-					understand your interests and preferences so that we can
-					make our marketing communication more relevant to you.
-				</p>
-				{getOptOut('profiling_optout')}
-			</PageSection>
+			</WithStandardTopMargin>
+			<WithStandardTopMargin>
+				<PageSection
+					title="Your data"
+					description={<YourDataDescription />}
+				>
+					{addMarketingToggle('profiling_optout')}
+				</PageSection>
+			</WithStandardTopMargin>
 		</>
 	);
 };
+
+// TODO Replace with IDAPI model values once released
+const consentsFixture: ConsentOption[] = [
+	{
+		id: 'market_research_optout',
+		description:
+			'From time to time we may contact you for market research purposes inviting you to complete a survey, or take part in a group discussion. Normally, this invitation would be sent via email, but we may also contact you by phone.',
+		name: 'Allow the Guardian to contact me for market research purposes',
+		isProduct: false,
+		isChannel: false,
+		type: ConsentOptionType.OPT_OUT,
+		subscribed: false,
+	},
+	{
+		id: 'post_optout',
+		name: 'Allow the Guardian to send communications by post',
+		isProduct: false,
+		isChannel: false,
+		type: ConsentOptionType.OPT_OUT,
+		subscribed: true,
+	},
+	{
+		id: 'profiling_optout',
+		name: 'Allow the Guardian to analyse this data to improve marketing content',
+		isProduct: false,
+		isChannel: false,
+		type: ConsentOptionType.OPT_OUT,
+		subscribed: true,
+	},
+	{
+		id: 'phone_optout',
+		name: 'Allow the Guardian to send communications by telephone',
+		isProduct: false,
+		isChannel: true,
+		type: ConsentOptionType.OPT_OUT,
+		subscribed: false,
+	},
+];
