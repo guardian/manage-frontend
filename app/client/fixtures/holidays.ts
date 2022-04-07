6b58a8e9e8ea98e2cb5b39ce3cca9db365c2292e
James Mockett <james.mockett@guardian.co.uk>
2022-04-07 12:42:40 +0100

    Add tests for amending and creating holidays stops with no deliveries

---
+++ app/client/fixtures/holidays.ts
@@ -0,0 +1,46 @@
+export const potentialDeliveries = {
+	nextInvoiceDateAfterToday: '2023-02-01',
+	potentials: [
+		{
+			publicationDate: '2022-02-11',
+			credit: -8.13,
+			invoiceDate: '2023-02-01',
+		},
+	],
+};
+
+export const noPotentialDeliveries = {
+	nextInvoiceDateAfterToday: '2023-02-01',
+	potentials: [],
+};
+
+export const existingHolidays = {
+	existing: [
+		{
+			id: 'a2k9E000005NnbrQAC',
+			startDate: '2022-03-11',
+			endDate: '2022-03-12',
+			subscriptionName: 'A-S00293857',
+			publicationsImpacted: [
+				{
+					publicationDate: '2022-03-11',
+					estimatedPrice: -8.13,
+					invoiceDate: '2022-03-24',
+					isActioned: false,
+				},
+			],
+			mutabilityFlags: {
+				isFullyMutable: true,
+				isEndDateEditable: true,
+			},
+		},
+	],
+	issueSpecifics: [
+		{
+			firstAvailableDate: '2022-02-05',
+			issueDayOfWeek: 5,
+		},
+	],
+	annualIssueLimit: 6,
+	firstAvailableDate: '2022-02-05',
+};
--- app/cypress/integration/parallel-3/holidayStops.spec.ts
+++ app/cypress/integration/parallel-3/holidayStops.spec.ts
@@ -1,5 +1,10 @@
 import { setLocalBaseUrl } from '../../lib/setLocalBaseUrl';
 import { guardianWeeklyCurrentSubscription } from '../../../client/fixtures/productDetail';
+import {
+	potentialDeliveries,
+	noPotentialDeliveries,
+	existingHolidays,
+} from '../../../client/fixtures/holidays';
 
 // 'No IAB consent management framework' exception is thrown from here: https://github.com/guardian/consent-management-platform/blob/405a4fee4c54c2bdabea3df0fd1bf187ae6d7927/src/onConsentChange.ts#L34
 Cypress.on('uncaught:exception', () => {
@@ -9,7 +14,7 @@ Cypress.on('uncaught:exception', () => {
 const iframeMessage = `[id^="sp_message_iframe_"]`;
 const acceptCookiesButtonText = 'Yes, I’m happy';
 
-describe('E2E Page rendering', function () {
+describe('Holiday stops', function () {
 	beforeEach(function () {
 		cy.session('auth', () => {
 			setLocalBaseUrl();
@@ -42,7 +47,7 @@ describe('E2E Page rendering', function () {
 		});
 	});
 
-	it('Completes adding holiday stop', function () {
+	it.only('can add a holiday stop', function () {
 		cy.intercept('GET', '/api/me/mma', {
 			statusCode: 200,
 			body: [guardianWeeklyCurrentSubscription],
@@ -55,50 +60,12 @@ describe('E2E Page rendering', function () {
 
 		cy.intercept('GET', '/api/holidays/A-S00293857/potential?*', {
 			statusCode: 200,
-			body: {
-				nextInvoiceDateAfterToday: '2022-02-24',
-				potentials: [
-					{
-						publicationDate: '2022-03-25',
-						credit: -8.13,
-						invoiceDate: '2022-04-24',
-					},
-				],
-			},
+			body: potentialDeliveries,
 		}).as('fetch_potential_holidays');
 
 		cy.intercept('GET', '/api/holidays/A-S00293857', {
 			statusCode: 200,
-			body: {
-				existing: [
-					{
-						id: 'a2k9E000005NnbrQAC',
-						startDate: '2022-03-11',
-						endDate: '2022-03-12',
-						subscriptionName: 'A-S00293857',
-						publicationsImpacted: [
-							{
-								publicationDate: '2022-03-11',
-								estimatedPrice: -8.13,
-								invoiceDate: '2022-03-24',
-								isActioned: false,
-							},
-						],
-						mutabilityFlags: {
-							isFullyMutable: true,
-							isEndDateEditable: true,
-						},
-					},
-				],
-				issueSpecifics: [
-					{
-						firstAvailableDate: '2022-02-05',
-						issueDayOfWeek: 5,
-					},
-				],
-				annualIssueLimit: 6,
-				firstAvailableDate: '2022-02-05',
-			},
+			body: existingHolidays,
 		}).as('fetch_existing_holidays');
 
 		cy.intercept('POST', '/api/holidays', {
@@ -114,14 +81,111 @@ describe('E2E Page rendering', function () {
 		cy.get('[data-cy="create_suspension_cta"] button').click();
 
 		cy.findByText('Choose the dates you will be away');
-		cy.get('[data-cy="date_picker"] div').eq(8).click();
-		cy.get('[data-cy="date_picker"] div').eq(10).click();
+		cy.get('[data-cy="date_picker"] div').eq(9).click();
+		cy.get('[data-cy="date_picker"] div').eq(11).click();
 		cy.wait('@fetch_potential_holidays');
+		cy.get('[data-cy="suspended-issues"')
+			.contains('Suspending')
+			.contains('1 issue');
+
 		cy.findByText('Review details').click();
+
+		cy.get('table').contains('9 February - 11 February 2022');
+		cy.get('table').contains('1 issue');
+		cy.get('table').contains('$8.13 off your 1 February 2023 payment');
 
 		cy.findByText('Confirm').click();
 
 		cy.wait('@create_holiday_stop');
-		cy.findByText('Your schedule has been set');
+		cy.findByText('Your schedule has been set').should('exist');
+	});
+
+	it('can amend a holiday stop', function () {
+		cy.intercept('GET', '/api/me/mma', {
+			statusCode: 200,
+			body: [guardianWeeklyCurrentSubscription],
+		}).as('mma');
+
+		cy.intercept('GET', '/api/me/mma?productType=Weekly', {
+			statusCode: 200,
+			body: [guardianWeeklyCurrentSubscription],
+		}).as('product_detail');
+
+		cy.intercept('GET', '/api/holidays/A-S00293857/potential?*', {
+			statusCode: 200,
+			body: potentialDeliveries,
+		}).as('fetch_potential_holidays');
+
+		cy.intercept('GET', '/api/holidays/A-S00293857', {
+			statusCode: 200,
+			body: existingHolidays,
+		}).as('fetch_existing_holidays');
+
+		cy.intercept('POST', '/api/holidays', {
+			statusCode: 200,
+			body: {
+				message: 'success',
+			},
+		}).as('create_holiday_stop');
+
+		cy.visit('/suspend/guardianweekly');
+		cy.wait('@fetch_existing_holidays');
+		cy.wait('@product_detail');
+		cy.get('[data-cy="create_suspension_cta"] button').click();
+
+		cy.get('[data-cy="date_picker"] div').eq(9).click();
+		cy.get('[data-cy="date_picker"] div').eq(11).click();
+		cy.wait('@fetch_potential_holidays');
+		cy.findByText('Review details').click();
+
+		cy.findByText('Amend').click();
+
+		cy.findByText('Choose the dates you will be away').should('exist');
+		cy.get('[aria-label="day"]').eq(0).should('have.value', '9');
+		cy.get('[aria-label="month"]').eq(0).should('have.value', '2');
+		cy.get('[aria-label="year"]').eq(0).should('have.value', '2022');
+		cy.get('[aria-label="day"]').eq(1).should('have.value', '11');
+		cy.get('[aria-label="month"]').eq(1).should('have.value', '2');
+		cy.get('[aria-label="year"]').eq(1).should('have.value', '2022');
+	});
+
+	it('can not create holiday stop for dates when there is no delivery', function () {
+		cy.intercept('GET', '/api/me/mma', {
+			statusCode: 200,
+			body: [guardianWeeklyCurrentSubscription],
+		}).as('mma');
+
+		cy.intercept('GET', '/api/me/mma?productType=Weekly', {
+			statusCode: 200,
+			body: [guardianWeeklyCurrentSubscription],
+		}).as('product_detail');
+
+		cy.intercept('GET', '/api/holidays/A-S00293857/potential?*', {
+			statusCode: 200,
+			body: noPotentialDeliveries,
+		}).as('fetch_potential_holidays');
+
+		cy.intercept('GET', '/api/holidays/A-S00293857', {
+			statusCode: 200,
+			body: existingHolidays,
+		}).as('fetch_existing_holidays');
+
+		cy.intercept('POST', '/api/holidays', {
+			statusCode: 200,
+			body: {
+				message: 'success',
+			},
+		}).as('create_holiday_stop');
+
+		cy.visit('/suspend/guardianweekly');
+		cy.wait('@fetch_existing_holidays');
+		cy.wait('@product_detail');
+		cy.get('[data-cy="create_suspension_cta"] button').click();
+
+		cy.get('[data-cy="date_picker"] div').eq(8).click();
+		cy.get('[data-cy="date_picker"] div').eq(10).click();
+		cy.wait('@fetch_potential_holidays');
+
+		cy.findByText('No issues occur during selected period').should('exist');
 	});
 });
