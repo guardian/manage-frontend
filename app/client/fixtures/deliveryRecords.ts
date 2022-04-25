493c8ce37c56192de38bf0f4c2381e9f4fd79998
James Mockett <james.mockett@guardian.co.uk>
2022-04-25 15:59:15 +0100

    Extend delivery records spec and remove existing Enzyme tests

--- app/client/fixtures/deliveryRecords.ts
+++ app/client/fixtures/deliveryRecords.ts
@@ -1,4 +1,4 @@
-export const noDeliveryRecords = {
+export const deliveryRecordsWithNoDeliveries = {
 	results: [],
 	deliveryProblemMap: {},
 	contactPhoneNumbers: {
@@ -10,7 +10,7 @@ export const noDeliveryRecords = {
 	},
 };
 
-export const dispatchedDeliveryRecords = {
+export const deliveryRecordsWithDelivery = {
 	results: [
 		{
 			id: 'a339E000000KDOMQA4',
@@ -32,6 +32,49 @@ export const dispatchedDeliveryRecords = {
 		},
 	],
 	deliveryProblemMap: {},
+	contactPhoneNumbers: {
+		Id: '0039E00001Mw6DCQAZ',
+		Phone: null,
+		HomePhone: null,
+		MobilePhone: null,
+		OtherPhone: null,
+	},
+};
+
+export const deliveryRecordsWithReportedProblem = {
+	results: [
+		{
+			id: 'a339E000000KDOMQA4',
+			deliveryDate: '2022-04-15',
+			deliveryInstruction: null,
+			deliveryAddress: 'Kings Place, London, 90 York Way',
+			addressLine1: 'Kings Place',
+			addressLine2: null,
+			addressLine3: null,
+			addressTown: 'London',
+			addressCountry: null,
+			addressPostcode: '90 York Way',
+			hasHolidayStop: false,
+			bulkSuspensionReason: null,
+			problemCaseId: '5009E00000KU9yBQAT',
+			isChangedAddress: false,
+			isChangedDeliveryInstruction: null,
+			credit: {
+				amount: -2.89,
+				invoiceDate: "2023-03-25",
+				isActioned: false
+			},
+		},
+	],
+	deliveryProblemMap: {
+		'5009E00000KU9yBQAT': {
+			id: "5009E00000KU9yBQAT",
+			ref: "01573148",
+			subject: "[Self Service] Delivery Problem : Damaged Paper (Guardian Weekly - A-S00333617)",
+			description: "Pages torn",
+			problemType: "Damaged Paper"
+		}
+	},
 	contactPhoneNumbers: {
 		Id: '0039E00001Mw6DCQAZ',
 		Phone: null,
--- app/cypress/integration/parallel-4/deliveryRecords.spec.ts
+++ app/cypress/integration/parallel-4/deliveryRecords.spec.ts
@@ -1,9 +1,11 @@
 import { guardianWeeklyCurrentSubscription } from '../../../client/fixtures/productDetail';
 import {
-	noDeliveryRecords,
-	dispatchedDeliveryRecords,
+	deliveryRecordsWithNoDeliveries,
+	deliveryRecordsWithDelivery,
+	deliveryRecordsWithReportedProblem
 } from '../../../client/fixtures/deliveryRecords';
 import { signInAndAcceptCookies } from '../../lib/signInAndAcceptCookies';
+import { potentialDeliveries } from '../../../client/fixtures/holidays';
 
 describe('Delivery records', () => {
 	beforeEach(() => {
@@ -16,14 +18,24 @@ describe('Delivery records', () => {
 
 		cy.intercept('GET', '/api/delivery-records/A-S00293857', {
 			statusCode: 200,
-			body: dispatchedDeliveryRecords,
+			body: deliveryRecordsWithDelivery,
 		}).as('delivery_records');
+
+		cy.intercept('GET', '/api/holidays/A-S00293857/potential?*', {
+			statusCode: 200,
+			body: potentialDeliveries,
+		}).as('fetch_potential_holidays');
+
+		cy.intercept('POST', '/api/delivery-records/A-S00293857', {
+			statusCode: 200,
+			body: deliveryRecordsWithReportedProblem,
+		}).as('fetch_potential_holidays');
 	});
 
-	it('does not show delivery history when there are no delivery records', () => {
+	it('does not show delivery history when there have been no deliveries', () => {
 		cy.intercept('GET', '/api/delivery-records/A-S00293857', {
 			statusCode: 200,
-			body: noDeliveryRecords,
+			body: deliveryRecordsWithNoDeliveries,
 		}).as('delivery_records');
 
 		cy.visit('/delivery/guardianweekly/records');
@@ -40,6 +52,61 @@ describe('Delivery records', () => {
 		cy.wait('@product_detail');
 		cy.wait('@delivery_records');
 
-		cy.findByText('Dispatched').should('exist');
+		cy.get('[data-cy="delivery-status"').contains("Dispatched");
+	});
+
+	it('allows you to report a problem with a delivery', () => {
+		cy.visit('/delivery/guardianweekly/records');
+		cy.wait('@product_detail');
+		cy.wait('@delivery_records');
+
+		cy.findByRole('button', {name: 'Report a problem'}).click();
+
+		cy.findByText('Step 1. What type of problem are you experiencing?').should('exist');
+		cy.findByRole('radio', {name: 'Damaged paper'}).click();
+		cy.findByRole('textbox', {name: 'Please specify'}).type('Pages torn');
+		cy.findByRole('button', {name: 'Continue to Step 2 & 3'}).click();
+
+		cy.findByText('Step 2. Select the date you have experienced the problem').should('exist');
+		cy.findByText('Step 3. Check your current delivery address').should('exist');
+		cy.findByRole('checkbox').click();
+		cy.findByRole('button', {name: 'Review your report'}).click();
+
+		cy.wait('@fetch_potential_holidays');
+
+		cy.findByText('Delivery report review').should('exist');
+		cy.findByText('Step 4. Please review your report details').should('exist');
+		cy.findByRole('button', {name: 'Submit your report'}).click();
+
+		cy.findByText('Delivery report confirmation').should('exist');
+		cy.findByText('Your delivery problem report has been successfully submitted.').should('exist');
+		cy.findByText('Problem reported (Damaged paper)').should('exist');
+	});
+
+	it('displays an error message if a delivery problem is not selected', () => {
+		cy.visit('/delivery/guardianweekly/records');
+		cy.wait('@product_detail');
+		cy.wait('@delivery_records');
+
+		cy.findByRole('button', {name: 'Report a problem'}).click();
+
+		cy.findByText('Step 1. What type of problem are you experiencing?').should('exist');
+		cy.findByRole('button', {name: 'Continue to Step 2 & 3'}).click();
+
+		cy.findByText('Please select the type of problem').should('exist');
+	});
+
+	it('returns you to the delivery history page if the cancel button is clicked', () => {
+		cy.visit('/delivery/guardianweekly/records');
+		cy.wait('@product_detail');
+		cy.wait('@delivery_records');
+
+		cy.findByRole('button', {name: 'Report a problem'}).click();
+
+		cy.findByText('Step 1. What type of problem are you experiencing?').should('exist');
+		cy.findByRole('button', {name: 'Cancel'}).click();
+
+		cy.findByText('Step 1. What type of problem are you experiencing?').should('not.exist');
+		cy.findByRole('button', {name: 'Report a problem'}).should('exist');
 	});
 });
