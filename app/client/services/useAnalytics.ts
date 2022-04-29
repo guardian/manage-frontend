23aada43319ba5f4661dd42a1638cbbae14db092
James Mockett <james.mockett@guardian.co.uk>
2022-04-29 16:02:26 +0100

    Revert custom analytics type

--- app/client/services/useAnalytics.ts
+++ app/client/services/useAnalytics.ts
@@ -7,19 +7,9 @@ import {
 	MMA_AB_TEST_DIMENSION_VALUE
 } from './analytics';
 
-type GaType = (
-	command: string,
-	trackingIdOrObject:
-		| string
-		| { trackingId?: string; cookieDomain: string; name: string }
-		| { hitType: string },
-	cookieDomain?: string,
-	name?: string,
-) => void;
-
 declare global {
 	interface Window {
-		ga?: GaType;
+		ga?: any;
 		gaData?: string;
 		dataLayer?: any;
 	}
