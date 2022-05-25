127fab0072392bd753ee7e2be4ae572cfa9a06a1
James Mockett <james.mockett@guardian.co.uk>
2022-05-25 17:53:02 +0100

    Update Emotion Babel plugins in Webpack, Jest and Storybook configs

--- app/jest.config.js
+++ app/jest.config.js
@@ -5,24 +5,27 @@ module.exports = {
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
 	moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'ts', 'tsx'],
 	testResultsProcessor: 'jest-teamcity-reporter',
-	snapshotSerializers: ['jest-emotion'],
+	snapshotSerializers: ['@emotion/jest/serializer'],
 	globals: {
 		'ts-jest': {
-			tsconfig: {
-				jsx: 'react-jsx',
-				module: 'commonjs',
-			},
 			babelConfig: {
 				presets: [
+					['@babel/preset-env', { targets: { node: 'current' } }],
 					'@babel/typescript',
-					['@babel/preset-react', { runtime: 'automatic' }],
+					[
+						'@babel/preset-react',
+						{
+							runtime: 'automatic',
+							importSource: '@emotion/react',
+						},
+					],
 				],
 				plugins: [
 					'@babel/proposal-class-properties',
 					'@babel/proposal-object-rest-spread',
 					'@babel/plugin-proposal-optional-chaining',
 					'lodash',
-					['babel-plugin-emotion', { cssPropOptimization: true }],
+					'@emotion/babel-plugin',
 				],
 			},
 		},
