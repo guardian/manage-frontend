b3d6a3c1bf7d4a944932cf3ed069b788c66cd10f
Richard Bangay <richard.bangay@gmail.com>
2022-07-13 10:17:30 +0100

    [RB] - update email address in ab test example to be the correct one

--- app/client/experiments/tests/example-test.ts
+++ app/client/experiments/tests/example-test.ts
@@ -4,10 +4,10 @@ export const exampleTest: ABTest = {
 	id: 'ExampleTest', // This ID must match the Server Side AB Test
 	start: '2020-05-20',
 	expiry: '2020-12-01', // Remember that the server side test expiry can be different
-	author: 'anemailaddress@theguardian.com',
+	author: 'supporter.revenue.retention.dev@guardian.co.uk',
 	description: 'This Test',
 	audience: 1, // 0.01% (1 is 100%)
-	audienceOffset: 0, // 50% (1 is 100%). Prevent overlapping with other tests.
+	audienceOffset: 0.5, // 50% (1 is 100%). Prevent overlapping with other tests.
 	successMeasure: 'It works',
 	audienceCriteria: 'Everyone',
 	idealOutcome: 'It works',
@@ -22,9 +22,9 @@ export const exampleTest: ABTest = {
 			},
 		},
 		{
-			id: 'variant',
+			id: 'variant', // something like 'showOffer'
 			test: (): string => {
-				return 'variant';
+				return 'variant'; // something like 'showOffer'
 			},
 		},
 	],
