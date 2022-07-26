e2e84bae27d60e71666f9476c76e168cc2380041
Richard Bangay <richard.bangay@gmail.com>
2022-07-26 15:37:54 +0100

    [RB JD] - minimal additions to guCdk ts file that will allow us to generate the diff file with the old cloudformation

--- cdk/lib/manage-frontend.ts
+++ cdk/lib/manage-frontend.ts
@@ -1,11 +1,30 @@
-import { join } from 'path';
+import { AccessScope } from '@guardian/cdk/lib/constants/access';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuNodeApp } from '@guardian/cdk/lib/patterns/ec2-app';
 import type { App } from 'aws-cdk-lib';
-import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
+import { InstanceClass, InstanceSize, InstanceType } from 'aws-cdk-lib/aws-ec2';
 
 export class ManageFrontend extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
+
+		const ec2app = new GuNodeApp(this, {
+			app: 'manage-frontend',
+			access: { scope: AccessScope.PUBLIC },
+			instanceType: InstanceType.of(
+				InstanceClass.T4G,
+				InstanceSize.SMALL,
+			),
+			userData: 'echo todo',
+			monitoringConfiguration: { noMonitoring: true },
+			scaling: { minimumInstances: 1, maximumInstances: 2 },
+			certificateProps: {
+				domainName:
+					'manage.theguardian.com.origin.membership.guardianapis.com',
+			},
+		});
+
+		//sdfgdfg
 	}
 }
