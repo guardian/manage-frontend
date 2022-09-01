e7f2c58dd1dd057659798f84bf91040f16207bd8
James Mockett <james.mockett@guardian.co.uk>
2022-09-01 17:59:53 +0100

    Add cancellation reason selection page

--- client/components/cancel/Cancellation.stories.tsx
+++ client/components/cancel/Cancellation.stories.tsx
@@ -1,10 +1,11 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import fetchMock from 'fetch-mock';
 import { ReactNode } from 'react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import { PRODUCT_TYPES } from '../../../shared/productTypes';
 
-import { guardianWeeklyCard } from '../../fixtures/productDetail';
+import { guardianWeeklyCard, contribution } from '../../fixtures/productDetail';
 import CancellationContainer from './CancellationContainer';
 import CancellationReasonSelection from './CancellationReasonSelection';
 
@@ -43,3 +44,23 @@ export const ContactCustomerService: ComponentStory<
 		component={<CancellationReasonSelection />}
 	/>
 );
+
+export const ReasonSelection: ComponentStory<
+	typeof CancellationContainer
+> = () => {
+	fetchMock.restore().get('glob:/api/cancellation-date/*', {
+		body: { cancellationEffectiveDate: '2022-09-01' },
+	});
+
+	return (
+		<RouterProvider
+			initialState={{ productDetail: contribution }}
+			container={
+				<CancellationContainer
+					productType={PRODUCT_TYPES.contributions}
+				/>
+			}
+			component={<CancellationReasonSelection />}
+		/>
+	);
+};
