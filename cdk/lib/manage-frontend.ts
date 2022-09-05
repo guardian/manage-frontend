11e5b2b03d70ecebc53b7ac8ecbf45d887b5b2df
Richard Bangay <richard.bangay@gmail.com>
2022-09-05 16:27:50 +0100

    [RB JM] - added a new tsconfig file to the root of the project, for the cdk/guardian eslint config

--- cdk/lib/manage-frontend.ts
+++ cdk/lib/manage-frontend.ts
@@ -1,8 +1,8 @@
+import { join } from 'path';
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import type { App } from 'aws-cdk-lib';
 import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
-import { join } from 'path';
 
 export class ManageFrontend extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
