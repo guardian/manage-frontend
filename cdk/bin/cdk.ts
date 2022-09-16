f718afe0c0818efb8a3f91f56da3db07f25b0f78
Richard Bangay <richard.bangay@gmail.com>
2022-09-16 16:43:36 +0100

    enabled local deployment to code (cdk) by enabling the cloudFormationStackName prop and the deploy script

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -6,10 +6,12 @@ const app = new App();
 new ManageFrontend(app, 'ManageFrontend-CODE', {
 	stack: 'support',
 	stage: 'CODE',
-	domain: 'manage.code.dev-theguardian.com.origin.membership.guardianapis.com',
+	domain: 'manage.code.theguardian.com.origin.membership.guardianapis.com',
+	cloudFormationStackName: 'support-CODE-manage-frontend',
 });
 new ManageFrontend(app, 'ManageFrontend-PROD', {
 	stack: 'support',
 	stage: 'PROD',
 	domain: 'manage.theguardian.com.origin.membership.guardianapis.com',
+	cloudFormationStackName: 'support-PROD-manage-frontend',
 });
