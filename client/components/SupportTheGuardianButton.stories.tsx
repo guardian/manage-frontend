f07e23f3091531752d42fc802a91743c88ea6bad
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2022-10-11 17:48:09 +0100

    Update story with examples of new button variants

--- client/components/SupportTheGuardianButton.stories.tsx
+++ client/components/SupportTheGuardianButton.stories.tsx
@@ -1,13 +1,4 @@
-import { ThemeProvider } from '@emotion/react';
-import { palette } from '@guardian/source-foundations';
-import {
-	buttonThemeReaderRevenueBrand,
-	buttonThemeReaderRevenueBrandAlt,
-	Inline,
-	LinkButton,
-	SvgArrowRightStraight,
-} from '@guardian/source-react-components';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { SupportTheGuardianButton } from './supportTheGuardianButton';
 
 export default {
@@ -16,93 +7,29 @@ export default {
 } as ComponentMeta<typeof SupportTheGuardianButton>;
 
 export const Default: ComponentStory<typeof SupportTheGuardianButton> = () => (
-	<>
-		<p>Existing</p>
+	<SupportTheGuardianButton supportReferer="storybook" />
+);
 
-		<Inline space={4}>
-			<SupportTheGuardianButton supportReferer="storybook" />
-			<SupportTheGuardianButton
-				supportReferer="storybook"
-				alternateButtonText="Subscribe"
-				fontWeight="bold"
-			/>
-			<SupportTheGuardianButton
-				supportReferer="storybook"
-				fontWeight="bold"
-				textColour={palette.neutral[100]}
-				colour={palette.brand[400]}
-				notPrimary
-			/>
-			<SupportTheGuardianButton
-				supportReferer="storybook"
-				fontWeight="bold"
-				height="42px"
-				textColour={palette.neutral[100]}
-				colour={palette.brand[400]}
-				notPrimary
-			/>
-			<SupportTheGuardianButton
-				supportReferer="storybook"
-				alternateButtonText="Make a recurring contribution"
-				fontWeight="bold"
-				height="42px"
-				notPrimary
-			/>
-		</Inline>
+export const WithSmallSize: ComponentStory<
+	typeof SupportTheGuardianButton
+> = () => <SupportTheGuardianButton supportReferer="storybook" size="small" />;
 
-		<p>New</p>
+export const WithBrandTheme: ComponentStory<
+	typeof SupportTheGuardianButton
+> = () => <SupportTheGuardianButton supportReferer="storybook" theme="brand" />;
 
-		<Inline space={4}>
-			<ThemeProvider theme={buttonThemeReaderRevenueBrand}>
-				<LinkButton
-					href="/"
-					icon={<SvgArrowRightStraight />}
-					iconSide="right"
-					nudgeIcon={true}
-					size="small"
-				>
-					Support The Guardian
-				</LinkButton>
-				<LinkButton
-					href="/"
-					icon={<SvgArrowRightStraight />}
-					iconSide="right"
-					nudgeIcon={true}
-					size="small"
-				>
-					Subscribe
-				</LinkButton>
-			</ThemeProvider>
+export const WithBrandAltTheme: ComponentStory<
+	typeof SupportTheGuardianButton
+> = () => (
+	<SupportTheGuardianButton supportReferer="storybook" theme="brandAlt" />
+);
 
-			<LinkButton
-				href="/"
-				icon={<SvgArrowRightStraight />}
-				iconSide="right"
-				nudgeIcon={true}
-				size="small"
-			>
-				Support The Guardian
-			</LinkButton>
-
-			<LinkButton
-				href="/"
-				icon={<SvgArrowRightStraight />}
-				iconSide="right"
-				nudgeIcon={true}
-			>
-				Support The Guardian
-			</LinkButton>
-
-			<ThemeProvider theme={buttonThemeReaderRevenueBrandAlt}>
-				<LinkButton
-					href="/"
-					icon={<SvgArrowRightStraight />}
-					iconSide="right"
-					nudgeIcon={true}
-				>
-					Make a recurring contribution
-				</LinkButton>
-			</ThemeProvider>
-		</Inline>
-	</>
+export const WithAlternateButtonText: ComponentStory<
+	typeof SupportTheGuardianButton
+> = () => (
+	<SupportTheGuardianButton
+		supportReferer="storybook"
+		alternateButtonText="Make a recurring contribution"
+		theme="brandAlt"
+	/>
 );
