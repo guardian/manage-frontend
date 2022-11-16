6bb7dadd116d99a0b1344b075214e7e5a0f8e55c
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2022-11-16 15:19:22 +0000

    Only fetch user details once and hide entire component until ready

--- client/components/accountoverview/WelcomeMessage.tsx
+++ client/components/accountoverview/WelcomeMessage.tsx
@@ -1,26 +1,29 @@
 import { css } from '@emotion/react';
 import { headline, space } from '@guardian/source-foundations';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { ProductDetail } from '../../../shared/productResponse';
 import { Users } from '../identity/identity';
+import type { User } from '../identity/models';
 
 interface WelcomeMessageProps {
 	activeProducts: ProductDetail[];
 }
 
 export const WelcomeMessage = ({ activeProducts }: WelcomeMessageProps) => {
-	const [userFirstName, setUserFirstName] = useState<String>();
+	const [userDetails, setUserDetails] = useState<User>();
 
-	Users.getCurrentUser().then((userDetails) =>
-		setUserFirstName(userDetails.firstName),
-	);
+	useEffect(() => {
+		Users.getCurrentUser().then((userDetails) =>
+			setUserDetails(userDetails),
+		);
+	}, []);
 
 	// The product detail array from AccountOverview is sorted by `joinDate` in
 	// descending order so the last entry is the oldest product
 	const oldestProduct = activeProducts[activeProducts.length - 1];
 	const supportStartYear = new Date(oldestProduct.joinDate).getFullYear();
 
-	return (
+	return userDetails ? (
 		<hgroup
 			css={css`
 				margin-top: ${space[12]}px;
@@ -32,7 +35,7 @@ export const WelcomeMessage = ({ activeProducts }: WelcomeMessageProps) => {
 					margin-bottom: 0;
 				`}
 			>
-				Hello {userFirstName}
+				Hello {userDetails.firstName}
 			</h2>
 			<p
 				css={css`
@@ -42,5 +45,5 @@ export const WelcomeMessage = ({ activeProducts }: WelcomeMessageProps) => {
 				Thanks for supporting the Guardian since {supportStartYear}
 			</p>
 		</hgroup>
-	);
+	) : null;
 };
