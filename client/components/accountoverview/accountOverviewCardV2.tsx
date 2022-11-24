5412102f0099143ad066e7436df1966cd7ca7fc0
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2022-11-24 17:37:41 +0000

    Add basic card styles

--- client/components/accountoverview/accountOverviewCardV2.tsx
+++ client/components/accountoverview/accountOverviewCardV2.tsx
@@ -1,6 +1,42 @@
+import { css } from '@emotion/react';
+import { headline, palette, space } from '@guardian/source-foundations';
+import type { ReactNode } from 'react';
 import type { ProductDetail } from '../../../shared/productResponse';
 import { getMainPlan } from '../../../shared/productResponse';
 import { GROUPED_PRODUCT_TYPES } from '../../../shared/productTypes';
+
+interface CardProps {
+	heading: string;
+	children: ReactNode;
+}
+
+const Card = (props: CardProps) => {
+	const containerCss = css`
+		width: 100%;
+		border: 1px solid ${palette.neutral[86]};
+	`;
+
+	const headingContainerCss = css`
+		padding: ${space[3]}px ${space[4]}px;
+		min-height: 128px;
+		color: ${palette.neutral[100]};
+		background-color: ${palette.brand[500]};
+	`;
+
+	const headingCss = css`
+		${headline.small({ fontWeight: 'bold' })};
+		margin: 0;
+	`;
+
+	return (
+		<div css={containerCss}>
+			<div css={headingContainerCss}>
+				<h3 css={headingCss}>{props.heading}</h3>
+			</div>
+			{props.children}
+		</div>
+	);
+};
 
 export const AccountOverviewCardV2 = ({
 	productDetail,
@@ -18,8 +54,8 @@ export const AccountOverviewCardV2 = ({
 		groupedProductType.mapGroupedToSpecific(productDetail);
 
 	return (
-		<div>
-			<h3>{specificProductType.productTitle(mainPlan)}</h3>
-		</div>
+		<Card heading={specificProductType.productTitle(mainPlan)}>
+			<p>Product details go here…</p>
+		</Card>
 	);
 };
