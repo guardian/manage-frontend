3821065f5a2a53451fc827b6bc1d3eb75a48ef7d
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-01-11 12:56:53 +0000

    Added unit tests to cover mdapiResponseReader functionality Added new response object to existing storybook test

---
+++ client/__tests__/components/productResponse.test.tsx
@@ -0,0 +1,35 @@
+/* eslint jest/no-standalone-expect: "off", jest/no-done-callback: "off" */
+import { mdapiResponseReader } from '../../../shared/productResponse';
+import { digitalDD, guardianWeeklyCard } from '../../fixtures/productDetail';
+
+const membersDataApiItems = [guardianWeeklyCard, digitalDD];
+const membersDataApiResponse = {
+	user: {
+		firstName: 'test',
+		lastName: 'name',
+	},
+	products: membersDataApiItems,
+};
+
+describe('testing mdapiResponseReader', () => {
+	it('mdapiResponseReader returns mdaResponse when MembersDataApiItems is passed in', () => {
+		const mdaResponse = membersDataApiItems;
+		expect(mdapiResponseReader(mdaResponse).products).toEqual(
+			membersDataApiItems,
+		);
+	});
+
+	it('mdapiResponseReader returns mdaResponse when MembersDataApiResponse is passed in', () => {
+		const mdaResponse = membersDataApiResponse;
+		expect(mdapiResponseReader(mdaResponse)).toEqual(
+			membersDataApiResponse,
+		);
+	});
+
+	it('throws validation error if MDAPI Response is in incorrect format', () => {
+		const mdaResponse = '';
+		expect(() => mdapiResponseReader(mdaResponse as any)).toThrow(
+			'MDAPI Response is of invalid format',
+		);
+	});
+});
