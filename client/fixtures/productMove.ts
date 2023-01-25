e5749fb6faafbc4545f9c7cbbe5ff282b31f2ed1
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-01-25 16:50:49 +0000

    Add successful product move response to fixtures

---
+++ client/fixtures/productMove.ts
@@ -0,0 +1,9 @@
+export const productMovePreviewResponse = {
+	amountPayableToday: 5.0,
+	contributionRefundAmount: -5.0,
+	supporterPlusPurchaseAmount: 10.0,
+};
+
+export const productMoveResponse = {
+	message: 'Product move completed successfully',
+};
--- client/fixtures/switchPreview.ts
+++
@@ -1,5 +0,0 @@
-export const switchPreview = {
-	amountPayableToday: 5.0,
-	contributionRefundAmount: -5.0,
-	supporterPlusPurchaseAmount: 10.0,
-};
