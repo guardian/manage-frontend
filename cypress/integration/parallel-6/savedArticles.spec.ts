7d083e3ce68ab48ae778b719afdc3877b867fa19
vanessa <32312712+vlbee@users.noreply.github.com>
2023-02-17 12:59:25 +0000

    Add savedArticles feature switch in OFF state

---
+++ cypress/integration/parallel-6/savedArticles.spec.ts
@@ -0,0 +1,24 @@
+import { featureSwitches } from '../../../shared/featureSwitches';
+
+if (!featureSwitches.savedArticles) {
+	describe('Feature Switch OFF: Saved Articles', () => {
+		beforeEach(() => {
+			cy.session('auth', () => {
+				cy.setCookie('gu-cmp-disabled', 'true');
+			});
+		});
+
+		it('redirects to account overview homepage from /saved-articles route ', () => {
+			cy.visit('/saved-articles');
+
+			cy.url().should('not.contain', '/saved-articles');
+
+			// check Navigation/Page Title
+			cy.findAllByText('Account overview').should(
+				'have.length.at.least',
+				1,
+			);
+			cy.findAllByText('Saved articles').should('have.length', 0);
+		});
+	});
+}
