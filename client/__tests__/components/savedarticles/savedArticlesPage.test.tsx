9294192396a3703cd71a7d97dc869561972093ee
Lindsey Dew <lindsey.megan.dew@gmail.com>
2023-02-21 10:58:50 +0000

    Define a test for displaying articles

--- client/__tests__/components/savedarticles/savedArticlesPage.test.tsx
+++ client/__tests__/components/savedarticles/savedArticlesPage.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom';
 import { SavedArticlesPage } from '../../../components/mma/savedArticles/SavedArticles';
 
 describe('SavedArticlesPage', () => {
-	it('should show an error page if the call fails', async () => {
+	it('should show an error page if the S4L API call fails', async () => {
 		render(
 			<SavedArticlesPage
 				saveForLaterAPICall={() => {
@@ -14,6 +14,39 @@ describe('SavedArticlesPage', () => {
 
 		await waitFor(() => {
 			expect(screen.queryByText('Oops!')).toBeInTheDocument();
+		});
+	});
+
+	it('should display a list of articles if the S4L API call succeeds and user has saved articles', async () => {
+		render(
+			<SavedArticlesPage
+				saveForLaterAPICall={() => {
+					return Promise.resolve({
+						version: '1',
+						articles: [
+							{
+								id: 'world/2018/mar/08/donald-trump-north-korea-kim-jong-un-meeting-may-letter-invite-talks-nuclear-weapons',
+								shortUrl: '/p/88btx',
+								date: '2018-03-09T14:08:02Z',
+								read: false,
+							},
+							{
+								id: 'football/2018/mar/12/jamie-carragher-dropped-by-danish-broadcaster-after-spitting-incident',
+								shortUrl: '/p/88qhj',
+								date: '2018-03-12T16:53:32Z',
+								read: false,
+							},
+						],
+					});
+				}}
+			/>,
+		);
+		await waitFor(() => {
+			expect(
+				screen.queryByText(
+					'world/2018/mar/08/donald-trump-north-korea-kim-jong-un-meeting-may-letter-invite-talks-nuclear-weapons',
+				),
+			).toBeInTheDocument();
 		});
 	});
 });
