655826ad8b7f72a5cbefd649519ba011e08fe915
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-02-27 14:15:44 +0000

    Create DataPrivacy.styles.ts

---
+++ client/components/mma/dataPrivacy/DataPrivacy.styles.ts
@@ -0,0 +1,23 @@
+import { css } from '@emotion/react';
+
+export const dataPrivacyHeadingCss = css`
+	margin: 0;
+	margin-bottom: 0.5rem;
+`;
+
+export const dataPrivacyParagraphCss = css`
+	margin: 0;
+`;
+
+export const dataPrivacyUnorderedListCss = css`
+	margin: 0;
+`;
+
+export const dataPrivacyVideoCss = css`
+	width: 100%;
+`;
+
+export const dataPrivacyMarketingToggleCss = css`
+	margin-top: 0;
+	position: relative;
+`;
