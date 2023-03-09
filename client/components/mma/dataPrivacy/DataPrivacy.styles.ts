060f3fa7375ff7e4fe27de1e7504918378ebd3dd
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-03-09 15:25:34 +0000

    Update DataPrivacy.styles.ts

--- client/components/mma/dataPrivacy/DataPrivacy.styles.ts
+++ client/components/mma/dataPrivacy/DataPrivacy.styles.ts
@@ -19,6 +19,43 @@ export const dataPrivacyVideoCss = css`
 	width: 100%;
 `;
 
+export const dataPrivacyVideoOverlayContainer = css`
+	background-color: red;
+	width: 100%;
+	position: relative;
+	/* width: 500px; */
+	/* margin: 0 auto; */
+	/* padding: 20px; */
+
+	video {
+		width: 100%;
+		display: block;
+	}
+`;
+
+export const dataPrivacyVideoOverlay = css`
+	background-color: red;
+	position: absolute;
+	background-color: rgba(0, 0, 0, 0.46);
+	z-index: 2;
+	left: 0;
+	right: 0;
+	top: 0;
+	bottom: 0;
+`;
+
+export const dataPrivacyFooter = css`
+	position: absolute;
+	bottom: 0;
+	color: white;
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	align-items: baseline;
+	padding-left: 1rem;
+	padding-bottom: 0.5rem;
+`;
+
 export const dataPrivacyMarketingToggleCss = css`
 	margin-top: 0;
 	position: relative;
