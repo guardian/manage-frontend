54332ae51ee208e6623f610b6c2037eca80494b8
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-03-09 15:23:49 +0000

    Create VideoPlayer.tsx

---
+++ client/components/mma/dataPrivacy/shared/VideoPlayer.tsx
@@ -0,0 +1,48 @@
+import { brand } from '@guardian/source-foundations';
+import { createRef, useState } from 'react';
+import { Button } from '../../shared/Buttons';
+import {
+	dataPrivacyFooter,
+	dataPrivacyVideoCss,
+	dataPrivacyVideoOverlay,
+	dataPrivacyVideoOverlayContainer,
+} from '../DataPrivacy.styles';
+
+interface VideoPlayerProps {
+	url: string;
+	text: string;
+}
+export const VideoPlayer = (props: VideoPlayerProps) => {
+	const [showOverlay, setShowOverlay] = useState<boolean>(true);
+	const videoRef = createRef<HTMLVideoElement>();
+	const hideOverlay = () => {
+		videoRef.current?.play();
+		setShowOverlay(false);
+	};
+	const overlay = (
+		<div onClick={hideOverlay} css={dataPrivacyVideoOverlay}>
+			<div css={dataPrivacyFooter}>
+				<Button
+					disabled={false}
+					text="PLAY"
+					type="button"
+					colour={brand[400]}
+					onClick={() => hideOverlay()}
+					fontWeight="bold"
+				/>
+				<h1>{props.text}</h1>
+			</div>
+		</div>
+	);
+	return (
+		<div css={dataPrivacyVideoOverlayContainer}>
+			{showOverlay ? overlay : null}
+			<video
+				ref={videoRef}
+				controls
+				css={dataPrivacyVideoCss}
+				src={props.url}
+			/>
+		</div>
+	);
+};
