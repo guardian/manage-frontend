548d30396f9662b0d6095fd8425c14d40e2af05d
James Mockett <1166188+jamesmockett@users.noreply.github.com>
2023-03-09 11:16:48 +0000

    Replace IDs in fixtures with clearly mocked data

--- client/fixtures/cancelledProductDetail.ts
+++ client/fixtures/cancelledProductDetail.ts
@@ -5,12 +5,12 @@ export const cancelledContribution: CancelledProductDetail = {
 	tier: 'Contributor',
 	joinDate: '2022-01-05',
 	subscription: {
-		subscriptionId: 'A-S00303370',
+		subscriptionId: 'A-S00000000',
 		cancellationEffectiveDate: '2023-03-20',
 		start: '2021-12-10',
 		end: '2022-11-29',
 		readerType: 'Direct',
-		accountId: '8ad09f8a7e25bda3017e296317464818',
+		accountId: '00000000',
 	},
 };
 
@@ -19,11 +19,11 @@ export const cancelledGuardianWeekly: CancelledProductDetail = {
 	tier: 'Guardian Weekly - Domestic',
 	joinDate: '2022-01-05',
 	subscription: {
-		subscriptionId: 'A-S00499483',
+		subscriptionId: 'A-S00000000',
 		cancellationEffectiveDate: '2023-02-28',
 		start: '2023-03-10',
 		end: '2023-03-10',
 		readerType: 'Direct',
-		accountId: '8ad09ea08683666d0186991883c55333',
+		accountId: '00000000',
 	},
 };
