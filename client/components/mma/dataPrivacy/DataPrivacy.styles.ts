e522ad0c72e80b14ae14d1a7b9ffe882b5d3c705
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-03-23 17:54:41 +0000

    Update DataPrivacy.styles.ts

--- client/components/mma/dataPrivacy/DataPrivacy.styles.ts
+++ client/components/mma/dataPrivacy/DataPrivacy.styles.ts
@@ -1,10 +1,13 @@
 import { css } from '@emotion/react';
 import { headline, textSans, until } from '@guardian/source-foundations';
+// import Ellipse from './shared/Ellipse.jpg'
+// import Ellipse from './shared/Ellipse.svg'
+// import { PlayButton } from './PlayButton';
 
 export const dataPrivacyHeadingCss = css`
 	margin: 0;
 	margin-bottom: 0.5rem;
-	${headline.xxsmall()}
+	${headline.xxsmall({ fontWeight: 'bold' })}
 `;
 
 export const dataPrivacyParagraphCss = css`
@@ -16,6 +19,16 @@ export const dataPrivacyParagraphCss = css`
 export const dataPrivacyUnorderedListCss = css`
 	margin: 0;
 	${textSans.small()}
+	li::before {
+		display: inline-block;
+		content: '';
+		border-radius: 50%;
+		height: 13px;
+		width: 13px;
+		background-color: #dcdcdc;
+		margin-left: -20px;
+		margin-right: 7px;
+	}
 `;
 
 export const dataPrivacyVideoCss = css`
@@ -45,27 +58,30 @@ export const dataPrivacyVideoOverlay = css`
 `;
 
 export const dataPrivacyVideoTextHeadline = css`
-	${headline.medium()};
-	align-self: center;
+	${headline.medium({ fontWeight: 'bold' })};
+	left: 25%;
+	bottom: 0;
+	position: absolute;
 	${until.tablet} {
-		${headline.xxsmall()};
+		${headline.small({ fontWeight: 'bold' })};
+	}
+	${until.mobileMedium} {
+		${headline.xxsmall({ fontWeight: 'bold' })};
 	}
 `;
 
 export const dataPrivacyFooter = css`
 	color: white;
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-
 	width: 100%;
-	padding-left: 1rem;
-	padding-bottom: 0.5rem;
 	height: 20%;
 `;
 
 export const dataPrivacyPlayButtonWrapper = css`
-	height: 100%;
+	height: 20%;
+	left: 0;
+	bottom: 0;
+	position: absolute;
+	margin-left: 1%;
 `;
 
 export const dataPrivacyMarketingToggleCss = css`
