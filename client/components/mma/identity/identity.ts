3ab0e5b8a84272bf9227598548db95bdde02eef2
David Blatcher <dv.bltchr@gmail.com>
2023-03-27 15:55:33 +0100

    unhide newsletter 6029

--- client/components/mma/identity/identity.ts
+++ client/components/mma/identity/identity.ts
@@ -120,11 +120,6 @@ export const ConsentOptions: ConsentOptionCollection = {
 				// Prevent trial newsletter from displaying.
 				.filter((newsletter: ConsentOption) => newsletter.id !== '6028') // identityId: 'saturday-roundup-trial'
 				// @AB_TEST: Default Onboarding Newsletter Test: END
-
-				// Temporary change - the newsletter with this number needs to be
-				// set up in advance but not rendered on MMA until the journalism
-				// it refers to is published.
-				.filter((newsletter: ConsentOption) => newsletter.id !== '6029')
 		);
 	},
 	consents(options: ConsentOption[]): ConsentOption[] {
