75f97c4ce3451e7c2cd07b0f2ac5f70ba5c260de
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-03-29 10:09:19 +0100

    Update LearnMoreSection.tsx

--- client/components/mma/dataPrivacy/LearnMoreSection.tsx
+++ client/components/mma/dataPrivacy/LearnMoreSection.tsx
@@ -1,10 +1,8 @@
-import { Lines } from '../identity/Lines';
 import { aCss } from '../identity/sharedStyles';
 import {
 	dataPrivacyHeadingCss,
 	dataPrivacyParagraphCss,
 } from './DataPrivacy.styles';
-import { VideoPlayer } from './shared/VideoPlayer';
 
 export const LearnMoreSection = () => {
 	return (
@@ -12,14 +10,6 @@ export const LearnMoreSection = () => {
 			<h3 css={dataPrivacyHeadingCss}>
 				Learn more about our privacy policy
 			</h3>
-			<VideoPlayer
-				overlayText={"The Guardian's privacy policy"}
-				url={
-					'https://uploads.guim.co.uk/2019%2F30%2F26%2FThe+Guardian%27s+privacy+policy+%E2%80%93+video--7d3a7f3f-bc23-4e9d-9566-ea1f8ada5954-1.mp4'
-				}
-			/>
-
-			<Lines n={1} />
 
 			<p css={dataPrivacyParagraphCss}>
 				For more information about how we use your data, visit our&nbsp;
@@ -31,7 +21,6 @@ export const LearnMoreSection = () => {
 				>
 					privacy policy
 				</a>{' '}
-				guide
 			</p>
 		</>
 	);
