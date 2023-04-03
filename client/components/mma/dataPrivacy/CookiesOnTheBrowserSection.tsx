24de13c5cc54154bc0b80bf53952e06eba8f7fce
Akinsola Lawanson <106528085+akinsola-guardian@users.noreply.github.com>
2023-04-03 14:46:34 +0100

    Update CookiesOnTheBrowserSection.tsx

--- client/components/mma/dataPrivacy/CookiesOnTheBrowserSection.tsx
+++ client/components/mma/dataPrivacy/CookiesOnTheBrowserSection.tsx
@@ -18,7 +18,7 @@ export const CookiesOnThisBrowserSection = (
 				{' '}
 				When we make the Guardian available for you online, we use
 				cookies and similar technologies to help us to do this. Some are
-				necessary to help our website work properly and can’s be
+				necessary to help our website work properly and can’t be
 				switched off, and some are optional but support the Guardian and
 				your experience in other ways.
 			</p>
