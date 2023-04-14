58c8182c1755f56cc245490ca9694fcb7eda1c67
andrewHEguardian <Andrew.howe-ely@guardian.co.uk>
2023-04-14 15:29:29 +0100

    Remove jest test

--- client/components/mma/cancel/cancellationSaves/ValueOfSupport.tsx
+++ client/components/mma/cancel/cancellationSaves/ValueOfSupport.tsx
@@ -1,23 +1,26 @@
 import { Button } from '@guardian/source-react-components';
+import { useNavigate } from 'react-router';
 
 export const ValueOfSupport = () => {
+	const navigate = useNavigate();
+
 	return (
 		<>
-			<div data-testid="progress-bar"></div>
-			<h2 data-testid="thanks-heading">
+			<div></div>
+			<h2>
 				Thank you for supporting the Guardian since xxx. Your support
 				has made such a difference.
 			</h2>
-			<p data-testid="thanks-copy">
+			<p>
 				Here comes ... Before you go, please consider adapting your
 				membership to a monthly support or a Monthly + Extra support.
 			</p>
-			<div data-testid="thanks-banner">Image placeholder</div>
+			<div>Image placeholder</div>
 			<div>
-				<Button data-testid="back-button">Back</Button>
-				<Button data-testid="continue-cancellation-button">
-					Continue to cancellation
+				<Button priority="tertiary" onClick={() => navigate('/')}>
+					Back
 				</Button>
+				<Button>Continue to cancellation</Button>
 			</div>
 		</>
 	);
