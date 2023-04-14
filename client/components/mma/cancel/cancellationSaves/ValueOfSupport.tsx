6c7c66c8ceb0f2f99e698a2c1a4654de62d0cd72
andrewHEguardian <Andrew.howe-ely@guardian.co.uk>
2023-04-14 17:05:30 +0100

    Add component to CancellationContainer and add styling

--- client/components/mma/cancel/cancellationSaves/ValueOfSupport.tsx
+++ client/components/mma/cancel/cancellationSaves/ValueOfSupport.tsx
@@ -1,26 +1,57 @@
-import { Button } from '@guardian/source-react-components';
+import { css } from '@emotion/react';
+import { space } from '@guardian/source-foundations';
+import {
+	Button,
+	Stack,
+	SvgArrowRightStraight,
+} from '@guardian/source-react-components';
 import { useNavigate } from 'react-router';
+import { Heading } from '../../shared/Heading';
+import { ProgressIndicator } from '../../shared/ProgressIndicator';
+
+const buttonLayoutCss = css`
+	text-align: right;
+	> * + * {
+		margin-left: ${space[3]}px;
+	}
+`;
 
 export const ValueOfSupport = () => {
 	const navigate = useNavigate();
 
 	return (
 		<>
-			<div></div>
-			<h2>
-				Thank you for supporting the Guardian since xxx. Your support
-				has made such a difference.
-			</h2>
-			<p>
-				Here comes ... Before you go, please consider adapting your
-				membership to a monthly support or a Monthly + Extra support.
-			</p>
+			<ProgressIndicator
+				steps={[
+					{ title: '' },
+					{ title: 'Details', isCurrentStep: true },
+					{ title: '' },
+				]}
+				additionalCSS={css`
+					margin: ${space[5]}px 0 ${space[12]}px;
+				`}
+			/>
+			<Stack space={4}>
+				<Heading>
+					Thank you for supporting the Guardian since xxx.
+					<span css={{ display: 'block' }}>
+						Your support has made such a difference.
+					</span>
+				</Heading>
+				<p>
+					Here comes ... Before you go, please consider adapting your
+					membership to a monthly support or a Monthly + Extra
+					support.
+				</p>
+			</Stack>
 			<div>Image placeholder</div>
-			<div>
+			<div css={buttonLayoutCss}>
 				<Button priority="tertiary" onClick={() => navigate('/')}>
 					Back
 				</Button>
-				<Button>Continue to cancellation</Button>
+				<Button icon={<SvgArrowRightStraight />} iconSide="right">
+					Continue to cancellation
+				</Button>
 			</div>
 		</>
 	);
