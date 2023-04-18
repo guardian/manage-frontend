ce8c8bdade3e40bb431030ef14fe5f5cd0de81ae
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-04-18 17:51:47 +0100

    Refactored to removed all of the multiple section and stack tags which I don't think are needed

--- client/components/mma/cancellationSave/MembershipCancellationLanding.tsx
+++ client/components/mma/cancellationSave/MembershipCancellationLanding.tsx
@@ -7,7 +7,6 @@ import {
 } from '@guardian/source-react-components';
 import { CallCentreEmailAndNumbers } from '../../shared/CallCenterEmailAndNumbers';
 import { Heading } from '../shared/Heading';
-import { sectionSpacing } from '../switch/SwitchStyles';
 
 const buttonLayoutCss = css`
 	text-align: right;
@@ -19,51 +18,34 @@ const buttonLayoutCss = css`
 export const MembershipCancellationLanding = () => {
 	return (
 		<>
-			<section css={sectionSpacing}>
-				<Stack space={3}>
-					<h1>We're sorry to hear you're thinking of cancelling.</h1>
-					Lorem Lipsum. Lorem Lipsum. Lorem Lipsum.
-				</Stack>
-			</section>
-			<section css={sectionSpacing}>
-				<h1>We offer different ways for cancelling your membership</h1>
-			</section>
-			<section css={sectionSpacing}>
-				<Stack space={4}>
-					<Heading sansSerif>Call us</Heading>
-					<CallCentreEmailAndNumbers //TO DO collapse menu
-						hideEmailAddress={true}
-					/>
-				</Stack>
-				<section>
-					<Stack space={4}>
-						<Heading sansSerif>Chat to us</Heading>
-						You can chat with our customer service..
-						<div css={buttonLayoutCss}>
-							<Button
-								icon={<SvgArrowRightStraight />}
-								iconSide="right"
-							>
-								Contact Us
-							</Button>
-						</div>
-					</Stack>
-				</section>
-				<section>
-					<Stack space={4}>
-						<Heading sansSerif>Cancel online</Heading>
-						You can cancel online Lorem Lipsum.
-						<div css={buttonLayoutCss}>
-							<Button
-								icon={<SvgArrowRightStraight />}
-								iconSide="right"
-							>
-								Cancel online
-							</Button>
-						</div>
-					</Stack>
-				</section>
-			</section>
+			{' '}
+			<Stack space={3}>
+				<Heading>
+					We're sorry to hear you're thinking of cancelling.
+				</Heading>
+				<p>Lorem Lipsum Loremm Lipsum</p>
+				<Heading>
+					We offer different ways for cancelling your membership
+				</Heading>
+				<Heading sansSerif>Call us</Heading>
+				<CallCentreEmailAndNumbers //TODO collapse menu
+					hideEmailAddress={true}
+				/>
+				<Heading sansSerif>Chat to us</Heading>
+				You can chat with our customer service..
+				<div css={buttonLayoutCss}>
+					<Button icon={<SvgArrowRightStraight />} iconSide="right">
+						Contact Us
+					</Button>
+				</div>
+				<Heading sansSerif>Cancel online</Heading>
+				You can cancel online Lorem Lipsum.
+				<div css={buttonLayoutCss}>
+					<Button icon={<SvgArrowRightStraight />} iconSide="right">
+						Cancel online
+					</Button>
+				</div>
+			</Stack>
 		</>
 	);
 };
