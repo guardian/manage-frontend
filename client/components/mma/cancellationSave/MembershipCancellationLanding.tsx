c2d1d32a607a638fc27e955f6036e4ddabcda3a8
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-04-19 11:46:34 +0100

    Buttons allinged to be on the left as per design Phone number menu is now collapsed as default

--- client/components/mma/cancellationSave/MembershipCancellationLanding.tsx
+++ client/components/mma/cancellationSave/MembershipCancellationLanding.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-import { space } from '@guardian/source-foundations';
+import { from, headline, palette, space } from '@guardian/source-foundations';
 import {
 	Button,
 	Stack,
@@ -7,46 +7,68 @@ import {
 } from '@guardian/source-react-components';
 import { CallCentreEmailAndNumbers } from '../../shared/CallCenterEmailAndNumbers';
 import { Heading } from '../shared/Heading';
+import { sectionSpacing } from '../switch/SwitchStyles';
 
 const buttonLayoutCss = css`
-	text-align: right;
 	> * + * {
 		margin-left: ${space[3]}px;
+	}
+`;
+
+const headingCss = css`
+	${headline.xsmall({ fontWeight: 'bold' })};
+	margin-top: 0;
+	margin-bottom: 0;
+
+	${from.tablet} {
+		${headline.small({ fontWeight: 'bold' })};
+		span {
+			display: block;
+			color: ${palette.brand['500']};
+		}
 	}
 `;
 
 export const MembershipCancellationLanding = () => {
 	return (
 		<>
-			{' '}
-			<Stack space={3}>
-				<Heading>
-					We're sorry to hear you're thinking of cancelling.
-				</Heading>
-				<p>Lorem Lipsum Loremm Lipsum</p>
-				<Heading>
-					We offer different ways for cancelling your membership
-				</Heading>
-				<Heading sansSerif>Call us</Heading>
-				You can call one of our customer service...
-				<CallCentreEmailAndNumbers //TODO collapse menu
-					hideEmailAddress={true}
-				/>
-				<Heading sansSerif>Chat to us</Heading>
-				You can chat with our customer service..
-				<div css={buttonLayoutCss}>
-					<Button icon={<SvgArrowRightStraight />} iconSide="right">
-						Contact Us
-					</Button>
-				</div>
-				<Heading sansSerif>Cancel online</Heading>
-				You can cancel online Lorem Lipsum.
-				<div css={buttonLayoutCss}>
-					<Button icon={<SvgArrowRightStraight />} iconSide="right">
-						Cancel online
-					</Button>
-				</div>
-			</Stack>
+			<section css={sectionSpacing}>
+				<Stack space={3}>
+					<Heading>
+						We're sorry to hear you're thinking of cancelling.
+					</Heading>
+					<p>Lorem Lipsum Loremm Lipsum</p>
+					<h2 css={headingCss}>
+						We offer different ways for cancelling your membership
+					</h2>
+					<Heading sansSerif>Call us</Heading>
+					You can call one of our customer service...
+					<CallCentreEmailAndNumbers
+						hideEmailAddress={true}
+						collapsed
+					/>
+					<Heading sansSerif>Chat to us</Heading>
+					You can chat with our customer service..
+					<div css={buttonLayoutCss}>
+						<Button
+							icon={<SvgArrowRightStraight />}
+							iconSide="right"
+						>
+							Contact Us
+						</Button>
+					</div>
+					<Heading sansSerif>Cancel online</Heading>
+					You can cancel online Lorem Lipsum.
+					<div css={buttonLayoutCss}>
+						<Button
+							icon={<SvgArrowRightStraight />}
+							iconSide="right"
+						>
+							Cancel online
+						</Button>
+					</div>
+				</Stack>
+			</section>
 		</>
 	);
 };
--- client/components/shared/CallCenterEmailAndNumbers.tsx
+++ client/components/shared/CallCenterEmailAndNumbers.tsx
@@ -79,12 +79,16 @@ export interface CallCentreEmailAndNumbersProps extends CallCentreNumbersProps {
 	hideEmailAddress?: boolean;
 	phoneRegionFilterKeys?: PhoneRegionKey[];
 	compactLayout?: boolean;
+	collapsed?: boolean;
 }
 
 export const CallCentreEmailAndNumbers = (
 	props: CallCentreEmailAndNumbersProps,
 ) => {
-	const [indexOfOpenSection, setIndexOfOpenSection] = useState<number>(0);
+	const intailIndex = props.collapsed ? -1 : 0;
+
+	const [indexOfOpenSection, setIndexOfOpenSection] =
+		useState<number>(intailIndex);
 
 	const filteredPhoneData = PHONE_DATA.filter(
 		(phoneRegion) =>
