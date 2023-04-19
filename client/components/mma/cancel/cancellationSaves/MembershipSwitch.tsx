ba6b9524e311b900b80cea2a45b6359689323043
andrewHEguardian <Andrew.howe-ely@guardian.co.uk>
2023-04-19 11:35:25 +0100

    Add dynamic copy elements to page

--- client/components/mma/cancel/cancellationSaves/MembershipSwitch.tsx
+++ client/components/mma/cancel/cancellationSaves/MembershipSwitch.tsx
@@ -7,9 +7,21 @@ import {
 	SvgClock,
 	SvgCreditCard,
 } from '@guardian/source-react-components';
-import { useNavigate } from 'react-router';
+import { useContext } from 'react';
+import { Navigate, useNavigate } from 'react-router';
+import { dateString } from '../../../../../shared/dates';
+import type {
+	PaidSubscriptionPlan} from '../../../../../shared/productResponse';
+import {
+	getMainPlan
+} from '../../../../../shared/productResponse';
 import { Card } from '../../shared/Card';
 import { Heading } from '../../shared/Heading';
+import type {
+	CancellationContextInterface} from '../CancellationContainer';
+import {
+	CancellationContext
+} from '../CancellationContainer';
 import {
 	buttonCentredCss,
 	buttonLayoutCss,
@@ -21,73 +33,82 @@ import {
 	smallPrintCss,
 } from './SaveStyles';
 
-const YourNewSupport = () => (
-	<section css={sectionSpacing}>
-		<Heading
-			sansSerif
-			cssOverrides={css`
-				margin-bottom: ${space[3]}px;
-			`}
-		>
-			Your new support
-		</Heading>
-		<Card>
-			<Card.Header backgroundColor={palette.brand[500]}>
-				<h3 css={productTitleCss}>Monthly</h3>
-			</Card.Header>
-			<Card.Section>
-				<p
-					css={css`
-						${textSans.medium()};
-						margin: 0;
-					`}
-				>
-					Monthly support with supporter newsletter and fewer asks for
-					support
-				</p>
-				<p css={newAmountCss}>XY/month</p>
-			</Card.Section>
-		</Card>
-	</section>
-);
+const YourNewSupport = (props: { billingPeriod: string }) => {
+	const monthlyOrAnnual = getMonthlyOrAnnual(props.billingPeriod);
+	return (
+		<section css={sectionSpacing}>
+			<Heading
+				sansSerif
+				cssOverrides={css`
+					margin-bottom: ${space[3]}px;
+				`}
+			>
+				Your new support
+			</Heading>
+			<Card>
+				<Card.Header backgroundColor={palette.brand[500]}>
+					<h3 css={productTitleCss}>{monthlyOrAnnual}</h3>
+				</Card.Header>
+				<Card.Section>
+					<p
+						css={css`
+							${textSans.medium()};
+							margin: 0;
+						`}
+					>
+						{monthlyOrAnnual} support with supporter newsletter and
+						fewer asks for support
+					</p>
+					<p css={newAmountCss}>XY/{props.billingPeriod}</p>
+				</Card.Section>
+			</Card>
+		</section>
+	);
+};
 
-const WhatHappensNext = () => (
-	<section css={sectionSpacing}>
-		<Stack space={4}>
-			<Heading sansSerif>What happens next?</Heading>
-			<ul css={[iconListCss, listWithDividersCss]}>
-				<li>
-					<SvgClock size="medium" />
-					<span>
-						<strong>
-							Your new support will start at the end of your
-							billing period
-						</strong>
-						<br />
-						You will be charged XY from the XX month. From that
-						date, you will continue to receive the supporter
-						newsletter and see fewer support asks but you will lose
-						access to premium features in the App and ad-free
-						reading.
-					</span>
-				</li>
-				<li>
-					<SvgCreditCard size="medium" />
-					<span>
-						<strong>Your payment method</strong>
-						<br />
-						The payment will be taken from{' '}
-						{/* <PaymentDetails
+const WhatHappensNext = (props: { nextBillingDate: string }) => {
+	const nextPaymentDate = dateString(
+		new Date(props.nextBillingDate),
+		'd MMMM',
+	);
+	return (
+		<section css={sectionSpacing}>
+			<Stack space={4}>
+				<Heading sansSerif>What happens next?</Heading>
+				<ul css={[iconListCss, listWithDividersCss]}>
+					<li>
+						<SvgClock size="medium" />
+						<span>
+							<strong>
+								Your new support will start at the end of your
+								billing period
+							</strong>
+							<br />
+							You will be charged XY from the {nextPaymentDate}.
+							From that date, you will continue to receive the
+							supporter newsletter and see fewer support asks but
+							you will lose access to premium features in the App
+							and ad-free reading.
+						</span>
+					</li>
+					<li>
+						<SvgCreditCard size="medium" />
+						<span>
+							<strong>Your payment method</strong>
+							<br />
+							The payment will be taken from{' '}
+							{/* <PaymentDetails
             subscription={
                 contributionToSwitch.subscription
             }
         /> */}
-					</span>
-				</li>
-			</ul>
-		</Stack>
-	</section>
-);
+						</span>
+					</li>
+				</ul>
+			</Stack>
+		</section>
+	);
+};
 
 const TsAndCs = () => (
 	<section css={sectionSpacing}>
@@ -116,6 +137,21 @@ const TsAndCs = () => (
 
 export const MembershipSwitch = () => {
 	const navigate = useNavigate();
+	const cancellationContext = useContext(
+		CancellationContext,
+	) as CancellationContextInterface;
+	const membership = cancellationContext.productDetail;
+
+	if (!membership) {
+		return <Navigate to="/" />;
+	}
+	const mainPlan = getMainPlan(
+		membership.subscription,
+	) as PaidSubscriptionPlan;
+
+	const currentPrice = membership.subscription.plan?.price;
+	const billingPeriod = membership.subscription.plan?.billingPeriod ?? '';
+	const monthlyOrAnnual = getMonthlyOrAnnual(billingPeriod);
 
 	return (
 		<>
@@ -127,11 +163,15 @@ export const MembershipSwitch = () => {
 						margin: 0;
 					`}
 				>
-					You are changing your current membership for a XY Monthly.
+					You are changing your current membership for a{' '}
+					{mainPlan.currency}
+					{currentPrice} {monthlyOrAnnual}.
 				</p>
 			</section>
-			<YourNewSupport />
-			<WhatHappensNext />
+			<YourNewSupport billingPeriod={billingPeriod} />
+			<WhatHappensNext
+				nextBillingDate={membership.subscription.plan?.end ?? ''}
+			/>
 			<section css={sectionSpacing}>
 				<p
 					css={css`
@@ -163,3 +203,6 @@ export const MembershipSwitch = () => {
 		</>
 	);
 };
+function getMonthlyOrAnnual(billingPeriod: string | undefined) {
+	return billingPeriod === 'year' ? 'Annual' : 'Monthly';
+}
