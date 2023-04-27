ca109fe741a06e24f4688f453e2b586a4fd0b829
andrewHEguardian <Andrew.howe-ely@guardian.co.uk>
2023-04-27 12:49:24 +0100

    add check for membership tier

--- client/components/mma/cancel/cancellationSaves/MembershipCancellationLanding.tsx
+++ client/components/mma/cancel/cancellationSaves/MembershipCancellationLanding.tsx
@@ -3,6 +3,7 @@ import {
 	Stack,
 	SvgArrowRightStraight,
 } from '@guardian/source-react-components';
+import { useContext } from 'react';
 import { Navigate, useNavigate } from 'react-router';
 import { featureSwitches } from '../../../../../shared/featureSwitches';
 import type {
@@ -20,20 +21,37 @@ import { JsonResponseHandler } from '../../shared/asyncComponents/DefaultApiResp
 import { DefaultLoadingView } from '../../shared/asyncComponents/DefaultLoadingView';
 import { Heading } from '../../shared/Heading';
 import { sectionSpacing } from '../../switch/SwitchStyles';
+import type { CancellationContextInterface } from '../CancellationContainer';
+import { CancellationContext } from '../CancellationContainer';
 import { buttonLayoutCss, headingCss } from './SaveStyles';
 
-function ineligibleForSave(products: ProductDetail[]) {
+function ineligibleForSave(
+	products: ProductDetail[],
+	membershipToCancel: ProductDetail,
+) {
 	const inPaymentFailure = products.find((product) => product.alertText);
 
 	const hasOtherProduct = products.find(
 		(product) => product.mmaCategory != 'membership',
 	);
 
-	return inPaymentFailure || hasOtherProduct;
+	const membershipTierIsNotSupporter =
+		membershipToCancel.tier !== 'Supporter';
+
+	return inPaymentFailure || hasOtherProduct || membershipTierIsNotSupporter;
 }
 
 export const MembershipCancellationLanding = () => {
 	const navigate = useNavigate();
+	const cancellationContext = useContext(
+		CancellationContext,
+	) as CancellationContextInterface;
+	const membership = cancellationContext.productDetail;
+
+	if (!membership) {
+		return <Navigate to="/" />;
+	}
+
 	const {
 		data,
 		loadingState,
@@ -54,7 +72,7 @@ export const MembershipCancellationLanding = () => {
 
 	if (
 		featureSwitches.membershipSave &&
-		ineligibleForSave(data.products as ProductDetail[])
+		ineligibleForSave(data.products as ProductDetail[], membership)
 	) {
 		return (
 			<Navigate
