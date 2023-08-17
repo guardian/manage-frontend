2b100f195aa2d8488c185dca61ae2b2d43b2a253
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-08-17 19:45:51 +0100

    Amount buttons are dynamic pricing which needs to be checked with product/UI on what they would like it to be Choose your amount on click provides a text box to input value. If the number is cleared or a value less than 10 and error shows Submit button is not active yet...

--- client/components/mma/upgrade/UpgradeSupportAmountForm.tsx
+++ client/components/mma/upgrade/UpgradeSupportAmountForm.tsx
@@ -4,18 +4,113 @@ import {
 	Button,
 	ChoiceCard,
 	ChoiceCardGroup,
+	TextInput,
 } from '@guardian/source-react-components';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import type { PaidSubscriptionPlan } from '../../../../shared/productResponse';
+import { calculateMonthlyOrAnnualFromBillingPeriod } from '../../../../shared/productTypes';
+import type { CurrencyIso } from '../../../utilities/currencyIso';
+import {
+	suggestedAmounts,
+	supporterPlusPriceConfigByCountryGroup,
+} from '../../../utilities/supporterPlusPricing';
 import { InfoIconDark } from '../shared/assets/InfoIconDark';
+import { benefitsConfiguration } from '../shared/benefits/BenefitsConfiguration';
+import { BenefitsSection } from '../shared/benefits/BenefitsSection';
 import type { UpgradeSupportInterface } from './UpgradeSupportContainer';
 import { UpgradeSupportContext } from './UpgradeSupportContainer';
+
+function validateChoice(
+	currentAmount: number,
+	chosenAmount: number | null,
+	minAmount: number,
+	maxAmount: number,
+	isOtherAmountSelected: boolean,
+	mainPlan: PaidSubscriptionPlan,
+): string | null {
+	const chosenOptionNum = Number(chosenAmount);
+	if (!chosenAmount && !isOtherAmountSelected) {
+		return 'Please make a selection';
+	} else if (chosenOptionNum === currentAmount) {
+		return 'You have selected the same amount as you currently contribute';
+	} else if (!chosenAmount || isNaN(chosenOptionNum)) {
+		return 'There is a problem with the amount you have selected, please make sure it is a valid amount';
+	} else if (!isNaN(chosenOptionNum) && chosenOptionNum < minAmount) {
+		return `There is a minimum ${
+			mainPlan.billingPeriod
+		}ly contribution amount of ${mainPlan.currency}${minAmount.toFixed(
+			2,
+		)} ${mainPlan.currencyISO}`;
+	} else if (!isNaN(chosenOptionNum) && chosenOptionNum > maxAmount) {
+		return `There is a maximum ${
+			mainPlan.billingPeriod
+		}ly contribution amount of ${mainPlan.currency}${maxAmount.toFixed(
+			2,
+		)} ${mainPlan.currencyISO}`;
+	}
+	return null;
+}
 
 export const UpgradeSupportAmountForm = () => {
 	const upgradeSupportContext = useContext(
 		UpgradeSupportContext,
 	) as UpgradeSupportInterface;
 
+	const priceConfig = (supporterPlusPriceConfigByCountryGroup[
+		upgradeSupportContext.mainPlan.currencyISO as CurrencyIso
+	] || supporterPlusPriceConfigByCountryGroup.international)[
+		calculateMonthlyOrAnnualFromBillingPeriod(
+			upgradeSupportContext.mainPlan.billingPeriod,
+		)
+	];
+
+	const amountLabel = (amount: number) => {
+		return `${upgradeSupportContext.mainPlan.currency}${amount} a ${upgradeSupportContext.mainPlan.billingPeriod}`;
+	};
 	const currentAmount = upgradeSupportContext.mainPlan.price / 100;
+	const mainPlan = upgradeSupportContext.mainPlan;
+
+	const otherAmountLabel = `Choose an amount (${upgradeSupportContext.mainPlan.currency} per ${upgradeSupportContext.mainPlan.billingPeriod})`;
+
+	const [selectedValue, setSelectedValue] = useState<number | null>(null);
+
+	const [isOtherAmountSelected, setIsOtherAmountSelected] =
+		useState<boolean>(false);
+
+	const [hasInteractedWithOtherAmount, setHasInteractedWithOtherAmount] =
+		useState<boolean>(false);
+
+	const [hasSubmitted] = useState<boolean>(false);
+
+	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+	const defaultOtherAmount = priceConfig.minAmount;
+
+	const [otherAmount, setOtherAmount] = useState<number | null>(
+		defaultOtherAmount,
+	);
+	const chosenAmount = isOtherAmountSelected ? otherAmount : selectedValue;
+
+	const shouldShowOtherAmountErrorMessage =
+		hasInteractedWithOtherAmount || hasSubmitted;
+
+	useEffect(() => {
+		if (otherAmount !== defaultOtherAmount) {
+			setHasInteractedWithOtherAmount(true);
+		}
+	}, [otherAmount]);
+
+	useEffect(() => {
+		const newErrorMessage = validateChoice(
+			currentAmount,
+			chosenAmount,
+			priceConfig.minAmount,
+			priceConfig.maxAmount,
+			isOtherAmountSelected,
+			mainPlan,
+		);
+		setErrorMessage(newErrorMessage);
+	}, [otherAmount, selectedValue]);
 
 	return (
 		<>
@@ -27,9 +122,9 @@ export const UpgradeSupportAmountForm = () => {
 					${textSans.medium()};
 				`}
 			>
-				<InfoIconDark />
-				You're currently supporting{' '}
-				{upgradeSupportContext.mainPlan.currency} {currentAmount}
+				<InfoIconDark /> You're currently supporting{' '}
+				{upgradeSupportContext.mainPlan.currency}
+				{currentAmount} per{' '}
 				{upgradeSupportContext.mainPlan.billingPeriod}.
 				<dl />
 				<ChoiceCardGroup
@@ -39,34 +134,69 @@ export const UpgradeSupportAmountForm = () => {
 					columns={2}
 				>
 					<>
-						<ChoiceCard
-							id={`amount-other`}
-							value="£15"
-							label="£15"
-						/>
-						<ChoiceCard
-							id={`amount-other`}
-							value="£12"
-							label="£12"
-						/>
-						<ChoiceCard
-							id={`amount-other`}
-							value="£10"
-							label="£10"
-						/>
+						{suggestedAmounts(currentAmount).map((amount) => (
+							<ChoiceCard
+								id={`amount-${amount}`}
+								key={amount}
+								value={amount.toString()}
+								label={amountLabel(amount)}
+								checked={selectedValue === amount}
+								onChange={() => {
+									setSelectedValue(amount);
+									setIsOtherAmountSelected(false);
+								}}
+							/>
+						))}
+
 						<ChoiceCard
 							id={`amount-other`}
 							value="Choose your amount"
 							label="Choose your amount"
+							checked={isOtherAmountSelected}
+							onChange={() => {
+								setIsOtherAmountSelected(true);
+								setSelectedValue(null);
+							}}
 						/>
 					</>
 				</ChoiceCardGroup>
+				{isOtherAmountSelected && (
+					<div
+						css={css`
+							margin-top: ${space[3]}px;
+						`}
+					>
+						<TextInput
+							label={otherAmountLabel}
+							error={
+								(shouldShowOtherAmountErrorMessage &&
+									errorMessage) ||
+								undefined
+							}
+							type="number"
+							value={otherAmount?.toString() || ''}
+							onChange={(event) =>
+								setOtherAmount(
+									event.target.value
+										? Number(event.target.value)
+										: null,
+								)
+							}
+						/>
+					</div>
+				)}
 				<div
 					css={css`
-						max-width: 500px;
+						margin-bottom: ${space[5]}px;
+						padding: ${space[3]}px ${space[5]}px;
+						${textSans.medium()};
 					`}
-				></div>
-				<Button>Continue with XYZ</Button>
+				>
+					<BenefitsSection
+						benefits={benefitsConfiguration['contributions']}
+					/>
+				</div>
+				<Button>Continue XYZ</Button>
 			</div>
 		</>
 	);
