2693d8cf356c639bad262726fd49c1b3d8ea3b94
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-08-24 16:55:20 +0100

    Created shared styles class for the sign in banner Deleted previously created upgrade support styles files

---
+++ client/components/shared/SignIn.tsx
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+import { from, palette, space, textSans } from '@guardian/source-foundations';
+
+export const signInCss = css`
+	display: grid;
+	overflow: hidden;
+	background-color: ${palette.brand[500]};
+	border-radius: 8px;
+	> * {
+		grid-area: 1 / 1;
+	}
+	> svg {
+		place-self: end;
+		height: 0;
+		min-height: 100%;
+	}
+	${from.tablet} {
+		border-radius: 0;
+	}
+`;
+
+export const signInHeadingCss = css`
+	${textSans.medium({ fontWeight: 'bold' })};
+	margin: 0;
+`;
+
+export const signInParaCss = css`
+	${textSans.medium()};
+	margin: 0;
+	max-width: 64%;
+`;
+
+export const signInContentContainerCss = css`
+	padding: ${space[3]}px;
+	color: ${palette.neutral[100]};
+`;
+
+export const sectionSpacing = css`
+	margin-top: ${space[6]}px;
+	${from.tablet} {
+		margin-top: ${space[9]}px;
+	}
+`;
