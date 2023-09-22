2a702f20b5e930c1a56323746384e470ddeb9e6f
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-09-22 15:54:17 +0100

    Removed unused CSS from styles file Updated brand import (as this is deprecated) to pallette.brand

--- client/components/mma/upgrade/UpgradeSupportStyles.ts
+++ client/components/mma/upgrade/UpgradeSupportStyles.ts
@@ -1,9 +1,9 @@
 import { css } from '@emotion/react';
-import { brand, from, space, textSans } from '@guardian/source-foundations';
+import { from, palette, textSans } from '@guardian/source-foundations';
 
 export const linkCss = css`
 	${textSans.medium()};
-	color: ${brand[400]};
+	color: ${palette.brand[400]};
 	font-weight: 700;
 	text-decoration-line: underline;
 	margin-top: 10px;
@@ -12,18 +12,4 @@ export const linkCss = css`
 	${from.tablet} {
 		margin-left: 20px;
 	}
-`;
-
-export const withMarginParagraphCss = css`
-	${textSans.medium()};
-	margin-left: 36px;
-	margin-bottom: ${space[3]}px;
-`;
-
-export const iconTextCss = css`
-	font-size: 17px;
-	font-style: normal;
-	font-weight: 700;
-	line-height: 130%; /* 22.1px */
-	margin-top: 4px;
 `;
