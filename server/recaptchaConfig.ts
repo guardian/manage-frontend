c82bf3f356bd0b5587948f1c2b373be9ca05bd52
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2023-09-29 16:12:09 +0100

    use DEV for recaptcha keys

--- server/recaptchaConfig.ts
+++ server/recaptchaConfig.ts
@@ -1,9 +1,12 @@
-import { s3ConfigPromise } from './awsIntegration';
+import { s3FilePromise } from './awsIntegration';
+import { conf } from './config';
 
 interface ReCaptchaKeys {
 	publicKey: string;
 	secretKey: string;
 }
 
-export const recaptchaConfigPromise =
-	s3ConfigPromise<ReCaptchaKeys>()('recaptcha');
+export const recaptchaConfigPromise = s3FilePromise<ReCaptchaKeys>(
+	'gu-reader-revenue-private',
+	`manage-frontend/${conf.STAGE}/recaptcha-${conf.STAGE}.json`,
+);
