a16bb5224488317bf10579169b57c4fe9af6efc9
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-10-03 09:59:35 +0100

    Help centre banner re phone lines being unavailable in Canada and the US

--- client/components/helpCentre/HelpCentrePage.tsx
+++ client/components/helpCentre/HelpCentrePage.tsx
@@ -77,7 +77,13 @@ const HelpCentreRouter = () => {
 	]
 	*/
 
-	const knownIssues: KnownIssueObj[] = [];
+	const knownIssues: KnownIssueObj[] = [
+		{
+			date: '3rd Oct 2023 10:00',
+			message:
+				'Due to a technical issue, Customer Service phone lines in the USA & Canada are currently not available. Live chat and email are unaffected.',
+		},
+	];
 
 	return (
 		<Main signInStatus={signInStatus} isHelpCentrePage>
