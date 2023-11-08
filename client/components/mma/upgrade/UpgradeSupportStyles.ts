2381b93ad71ed3ba7573e9c463f7d2e36c00e043
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-11-08 12:25:33 +0000

    added styling onto links as per design. added in new styling for links at the end of non link components as margins are different

--- client/components/mma/upgrade/UpgradeSupportStyles.ts
+++ client/components/mma/upgrade/UpgradeSupportStyles.ts
@@ -16,3 +16,29 @@ export const linkCss = css`
 		margin-left: 20px;
 	}
 `;
+
+export const nonLinkCss = css`
+	${textSans.medium()};
+	color: ${palette.neutral[46]};
+	margin-top: 10px;
+	display: flex;
+	justify-content: center;
+	${from.tablet} {
+		margin-left: 20px;
+	}
+`;
+
+export const linkAdditionCss = css`
+	${textSans.medium()};
+	a {
+		color: ${palette.brand[400]};
+	}
+	color: ${palette.brand[400]};
+	font-weight: 700;
+	text-decoration-line: underline;
+	display: inline;
+	justify-content: center;
+	${from.tablet} {
+		margin-left: 20px;
+	}
+`;
