139fca7abe52e43040929e36dd32e783a0404b7e
Charley_Campbell <charley.campbell@guardian.co.uk>
2023-11-14 15:45:36 +0000

    New file created for V@ of the manage products screen. It was proving to be challenging to make the css changes on top of the existing code in the file. Created a new storybook for V2

---
+++ client/components/mma/accountoverview/ManageProductV2.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ReactRouterDecorator } from '@/.storybook/ReactRouterDecorator';
+import { ManageProductV2 } from '@/client/components/mma/accountoverview/ManageProductV2';
+import { PRODUCT_TYPES } from '@/shared/productTypes';
+import { digitalPackPaidByDirectDebit } from '../../../fixtures/productBuilder/testProducts';
+
+export default {
+	title: 'Pages/ManageProductV2',
+	component: ManageProductV2,
+	decorators: [ReactRouterDecorator],
+	parameters: {
+		layout: 'fullscreen',
+	},
+} as Meta<typeof ManageProductV2>;
+
+export const DigitalSubscription: StoryObj<typeof ManageProductV2> = {
+	render: () => {
+		return <ManageProductV2 productType={PRODUCT_TYPES.digipack} />;
+	},
+
+	parameters: {
+		reactRouter: {
+			state: { productDetail: digitalPackPaidByDirectDebit() },
+		},
+	},
+};
---
+++ client/components/mma/accountoverview/ManageProductV2.tsx
@@ -0,0 +1,374 @@
+import { css } from '@emotion/react';
+import {
+	headline,
+	palette,
+	space,
+	textSans,
+	until,
+} from '@guardian/source-foundations';
+import {
+	LinkButton,
+	Stack,
+	SvgCalendar,
+	SvgClock,
+	SvgCreditCard,
+} from '@guardian/source-react-components';
+import { useState } from 'react';
+import { Link, Navigate, useLocation } from 'react-router-dom';
+import { BasicProductInfoTable } from '@/client/components/mma/shared/BasicProductInfoTable';
+import { getNextPaymentDetails } from '@/client/components/mma/shared/NextPaymentDetails';
+import { PaymentDetails } from '@/client/components/mma/shared/PaymentDetails';
+import {
+	buttonCentredCss,
+	stackedButtonLayoutCss,
+} from '@/client/styles/ButtonStyles';
+import {
+	iconListCss,
+	listWithDividersCss,
+	whatHappensNextIconCss,
+} from '@/client/styles/GenericStyles';
+import { cancellationFormatDate } from '@/shared/dates';
+import type {
+	MembersDataApiResponse,
+	ProductDetail,
+} from '@/shared/productResponse';
+import { getMainPlan, isProduct } from '@/shared/productResponse';
+import type { ProductType, WithProductType } from '@/shared/productTypes';
+import { GROUPED_PRODUCT_TYPES } from '@/shared/productTypes';
+import {
+	LoadingState,
+	useAsyncLoader,
+} from '../../../utilities/hooks/useAsyncLoader';
+import { createProductDetailFetcher } from '../../../utilities/productUtils';
+import { GenericErrorScreen } from '../../shared/GenericErrorScreen';
+import { NAV_LINKS } from '../../shared/nav/NavConfig';
+import { PageContainer } from '../Page';
+import { ErrorIcon } from '../shared/assets/ErrorIcon';
+import { JsonResponseHandler } from '../shared/asyncComponents/DefaultApiResponseHandler';
+import { DefaultLoadingView } from '../shared/asyncComponents/DefaultLoadingView';
+import { PaymentFailureAlertIfApplicable } from '../shared/PaymentFailureAlertIfApplicable';
+
+const subHeadingTitleCss = `
+    ${headline.medium()};
+    font-weight: bold;
+    ${until.tablet} {
+      ${headline.xxsmall()};
+      line-height: 1.6;
+    };
+  `;
+const subHeadingBorderTopCss = `
+    margin: 16px 0 32px;
+  `;
+
+interface InnerContentProps {
+	manageProductV2Props: WithProductType<ProductType>;
+	productDetail: ProductDetail;
+}
+
+const InnerContent = ({
+	manageProductV2Props,
+	productDetail,
+}: InnerContentProps) => {
+	const mainPlan = getMainPlan(productDetail.subscription);
+	if (!mainPlan) {
+		throw new Error('mainPlan does not exist in manageProductV2 page');
+	}
+
+	const specificProductType = manageProductV2Props.productType;
+	const groupedProductType =
+		GROUPED_PRODUCT_TYPES[specificProductType.groupedProductType];
+
+	const hasCancellationPending = productDetail.subscription.cancelledAt;
+
+	const cancelledCopy =
+		specificProductType.cancelledCopy || groupedProductType.cancelledCopy;
+
+	const [overiddenAmount] = useState<number | null>(null);
+
+	const nextPaymentDetails = getNextPaymentDetails(
+		mainPlan,
+		productDetail.subscription,
+		overiddenAmount,
+		!!productDetail.alertText,
+	);
+
+	const maybePatronSuffix =
+		productDetail.subscription.readerType === 'Patron' ? ' - Patron' : '';
+
+	return (
+		<>
+			<PaymentFailureAlertIfApplicable productDetails={[productDetail]} />
+			<div
+				css={css`
+					${subHeadingBorderTopCss};
+					display: flex;
+					align-items: start;
+					justify-content: space-between;
+				`}
+			>
+				<h2
+					css={css`
+						${subHeadingTitleCss};
+						margin: 0;
+					`}
+				>
+					Manage{' '}
+					{specificProductType.productTitle(mainPlan).toLowerCase()}
+					{maybePatronSuffix}
+				</h2>
+			</div>
+
+			{hasCancellationPending && (
+				<p
+					css={css`
+						${textSans.medium()};
+					`}
+				>
+					<ErrorIcon fill={palette.brandAlt[200]} />
+					<span
+						css={css`
+							margin-left: ${space[2]}px;
+						`}
+					>
+						{cancelledCopy}{' '}
+						<strong>
+							{cancellationFormatDate(
+								productDetail.subscription
+									.cancellationEffectiveDate,
+							)}
+						</strong>
+					</span>
+					.
+				</p>
+			)}
+
+			<BasicProductInfoTable
+				groupedProductType={groupedProductType}
+				productDetail={productDetail}
+			/>
+
+			<h2
+				css={css`
+					${textSans.medium({ fontWeight: 'bold' })}
+					font-style: normal;
+					font-size: 20px;
+					font-weight: bold;
+					margin-bottom: 16px;
+				`}
+			>
+				Payment
+			</h2>
+
+			<section
+				css={css`
+					border-bottom: 1px solid ${palette.neutral[86]};
+					padding-bottom: ${space[5]}px;
+				`}
+			>
+				<Stack space={5}>
+					<div
+						css={css`
+							border-top: 1px solid ${palette.neutral[86]};
+							padding-bottom: ${space[1]}px;
+						`}
+					></div>
+					<ul
+						css={[
+							iconListCss,
+							listWithDividersCss,
+							whatHappensNextIconCss,
+						]}
+					>
+						<li>
+							<SvgClock size="medium" />
+							<span>
+								<>
+									<strong
+										css={css`
+											padding-bottom: ${space[1]}px;
+										`}
+									>
+										Next payment
+									</strong>
+									<br />
+									{nextPaymentDetails}
+								</>
+							</span>
+						</li>
+						<li>
+							<SvgCalendar size="medium" />
+							<span>
+								<>
+									<strong
+										css={css`
+											padding-bottom: ${space[1]}px;
+										`}
+									>
+										Next payment date
+									</strong>
+									<br />
+								</>
+							</span>
+						</li>
+						<li>
+							<SvgCreditCard size="medium" />
+							<span data-qm-masking="blocklist">
+								<strong
+									css={css`
+										padding-bottom: ${space[1]}px;
+									`}
+								>
+									Payment Method
+								</strong>
+								<br />
+								<PaymentDetails
+									subscription={productDetail.subscription}
+								/>
+							</span>
+						</li>
+					</ul>
+				</Stack>
+			</section>
+
+			<section
+				css={css`
+					margin-top: ${space[4]}px;
+				`}
+			>
+				<div css={stackedButtonLayoutCss}>
+					{productDetail.isPaidTier &&
+						productDetail.subscription.safeToUpdatePaymentMethod &&
+						!productDetail.subscription.payPalEmail && (
+							<LinkButton
+								href={`/payment/${specificProductType.urlPart}`}
+								cssOverrides={buttonCentredCss}
+							>
+								Update payment method
+							</LinkButton>
+						)}
+
+					{!hasCancellationPending && (
+						<CancellationCTA
+							productDetail={productDetail}
+							friendlyName={groupedProductType.friendlyName()}
+							specificProductType={specificProductType}
+						/>
+					)}
+				</div>
+			</section>
+		</>
+	);
+};
+
+interface CancellationCTAProps {
+	productDetail: ProductDetail;
+	friendlyName: string;
+	specificProductType: ProductType;
+}
+
+const CancellationCTA = (props: CancellationCTAProps) => {
+	const shouldContactUsToCancel =
+		!props.productDetail.selfServiceCancellation.isAllowed ||
+		!props.specificProductType.cancellation;
+	return (
+		<>
+			{shouldContactUsToCancel && (
+				<div
+					css={css`
+						${textSans.medium()};
+						color: ${palette.neutral[46]};
+						margin-top: 16px;
+						justify-content: center;
+					`}
+				>
+					Would you like to cancel your {props.friendlyName}?
+					<Link
+						css={css`
+							${textSans.medium()};
+							color: ${palette.brand[400]};
+							font-weight: 700;
+							text-decoration-line: underline;
+							justify-content: center;
+							margin-left: 5px;
+						`}
+						to={'/cancel/' + props.specificProductType.urlPart}
+						state={{ productDetail: props.productDetail }}
+					>
+						Contact us
+					</Link>
+				</div>
+			)}
+
+			{!shouldContactUsToCancel && (
+				<Link to={'/cancel/' + props.specificProductType.urlPart}>
+					{' '}
+					Cancel {props.friendlyName}{' '}
+				</Link>
+			)}
+		</>
+	);
+};
+
+interface ManageProductV2RouterState {
+	productDetail: ProductDetail;
+}
+
+const AsyncLoadedInnerContent = (props: WithProductType<ProductType>) => {
+	const request = createProductDetailFetcher(
+		props.productType.allProductsProductTypeFilterString,
+	);
+
+	const { data, loadingState } = useAsyncLoader<MembersDataApiResponse>(
+		request,
+		JsonResponseHandler,
+	);
+
+	if (loadingState == LoadingState.HasError) {
+		return <GenericErrorScreen />;
+	}
+	if (loadingState == LoadingState.IsLoading) {
+		return <DefaultLoadingView loadingMessage="Loading your product..." />;
+	}
+
+	if (data == null || data.products.length == 0) {
+		return <Navigate to="/" />;
+	}
+
+	const productDetail = data.products.filter(isProduct)[0];
+
+	return (
+		<InnerContent
+			manageProductV2Props={props}
+			productDetail={productDetail}
+		/>
+	);
+};
+
+export const ManageProductV2 = (props: WithProductType<ProductType>) => {
+	const location = useLocation();
+	const routerState = location.state as ManageProductV2RouterState;
+	const productDetail = routerState?.productDetail;
+
+	return (
+		<PageContainer
+			selectedNavItem={NAV_LINKS.accountOverview}
+			pageTitle={`Your ${
+				GROUPED_PRODUCT_TYPES[props.productType.groupedProductType]
+					.shortFriendlyName ||
+				GROUPED_PRODUCT_TYPES[
+					props.productType.groupedProductType
+				].friendlyName()
+			}`}
+			minimalFooter
+		>
+			{productDetail ? (
+				<InnerContent
+					manageProductV2Props={props}
+					productDetail={productDetail}
+				/>
+			) : (
+				<AsyncLoadedInnerContent {...props} />
+			)}
+		</PageContainer>
+	);
+};
