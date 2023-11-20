fd0a19eacf2dad8fda965a891a4f5c993a885970
Charley Campbell <115992455+charleycampbell@users.noreply.github.com>
2023-11-20 09:43:41 +0000

    Update client/components/mma/cancel/cancellationSaves/digipack/ConfirmCancellation.tsx
    Co-authored-by: Andrew Howe-Ely <114918544+andrewHEguardian@users.noreply.github.com>

--- client/components/mma/cancel/cancellationSaves/digipack/ConfirmCancellation.tsx
+++ client/components/mma/cancel/cancellationSaves/digipack/ConfirmCancellation.tsx
@@ -11,7 +11,7 @@ import { Link } from 'react-router-dom';
 import { benefitsCss } from '@/client/components/mma/shared/benefits/BenefitsStyles';
 import { stackedButtonLayoutCss } from '@/client/styles/ButtonStyles';
 
-function GreyBulletpoints() {
+function GreyBulletpoint() {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
