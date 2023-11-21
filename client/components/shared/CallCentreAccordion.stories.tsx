3c425213a6d32bab3e11b4e80c1c54f337416f5a
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2023-11-21 09:53:36 +0000

    add more explanation in comment

--- client/components/shared/CallCentreAccordion.stories.tsx
+++ client/components/shared/CallCentreAccordion.stories.tsx
@@ -14,7 +14,9 @@ export const Closed: StoryObj = {};
 export const Open: StoryObj = {
 	args: { showEmailAddress: true },
 	play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
-		// There is a useEffect within AccordionRow that sets 'isBrowser' that isn't being run in storybook
+		// There is a useEffect within AccordionRow that sets 'isBrowser' and renders a NoJS version first
+		// Due to a bug the useEffect isn't being run in Storybook/Chromatic before we try to find the button
+		// We use sleep(1) to ensure the useEffect runs, if  this works without the sleep, please remove it
 		await sleep(1);
 		const canvas = within(canvasElement);
 		const button = canvas.getAllByText('Show')[0];
