eaa0a40780336f58204968d9036a725a25bc41e9
andrewHEguardian <114918544+andrewHEguardian@users.noreply.github.com>
2023-12-21 10:50:39 +0000

    refactor: scrollToHashElement

--- client/utilities/hooks/useScrollToHashElement.ts
+++ client/utilities/hooks/useScrollToHashElement.ts
@@ -1,15 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+function removeHashCharacter(str: string) {
+	return str.slice(1);
+}
+
 export const useScrollToHashElement = () => {
 	const location = useLocation();
 	const hashElement = (function () {
 		const hash = location.hash;
-		const removeHashCharacter = (str: string | any[]) => {
-			return str.slice(1);
-		};
+
 		if (hash) {
-			return document.getElementById(<string>removeHashCharacter(hash));
+			return document.getElementById(removeHashCharacter(hash));
 		} else {
 			return null;
 		}
