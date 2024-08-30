18d9530e628cdf68b5669ab246bd36872bebab8d
Tom Wey <tom.wey@theguardian.com>
2024-08-30 12:38:12 +0100

    Add gradient background to hero image on mobile
    Only the newsletter archive component for now, will apply to the others
    in a separate commit.

--- client/components/mma/shared/NewspaperArchiveCta.tsx
+++ client/components/mma/shared/NewspaperArchiveCta.tsx
@@ -10,7 +10,6 @@ interface NewspaperArchiveCtaProps {
 
 const containerCss = css`
 	background-color: #f8f5f7;
-	padding: ${space[3]}px ${space[3]}px ${space[5]}px;
 	h4 {
 		${textSansBold17};
 		margin: 0 ${space[5]}px 0 0;
@@ -35,9 +34,20 @@ const inlineContentsCss = css`
 const copyContainerCss = css`
 	order: 2;
 	margin-top: ${space[4]}px;
+	padding: 0 ${space[3]}px ${space[5]}px;
 	${from.tablet} {
 		order: 1;
 		margin: 0 0 ${space[6]}px;
+		padding: 0;
+	}
+`;
+
+const heroImageContainerCss = css`
+	background: linear-gradient(transparent, rgba(0, 0, 0, 0.1));
+	order: 1;
+	${from.tablet} {
+		background: none;
+		order: 2;
 	}
 `;
 
@@ -45,9 +55,7 @@ const heroImageCss = css`
 	margin: 0 auto;
 	width: 100%;
 	max-width: 450px;
-	order: 1;
 	${from.tablet} {
-		order: 2;
 		max-width: 340px;
 	}
 `;
@@ -80,10 +88,12 @@ export const NewspaperArchiveCta = (props: NewspaperArchiveCtaProps) => {
 						Explore the website
 					</LinkButton>
 				</div>
-				<img
-					css={heroImageCss}
-					src="https://i.guim.co.uk/img/media/a3400ec516a9b46348cc6d898441b6c4e20a52af/0_0_1172_728/500.png?width=400&quality=75&s=79a92c6634c653e25047589a1832a343"
-				/>
+				<div css={heroImageContainerCss}>
+					<img
+						css={heroImageCss}
+						src="https://i.guim.co.uk/img/media/a3400ec516a9b46348cc6d898441b6c4e20a52af/0_0_1172_728/500.png?width=400&quality=75&s=79a92c6634c653e25047589a1832a343"
+					/>
+				</div>
 			</div>
 		</div>
 	);
