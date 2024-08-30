e7a85c3112148216c3c4d4c9f7e8a48fa7d7980c
Tom Wey <tom.wey@theguardian.com>
2024-08-30 13:01:55 +0100

    Add linear gradient to news app CTA

--- client/components/mma/shared/DownloadAppCtaVariation1.tsx
+++ client/components/mma/shared/DownloadAppCtaVariation1.tsx
@@ -11,7 +11,6 @@ interface DownloadAppCtaVariation1Props {
 
 const containerCss = css`
 	background-color: #f0f3f8;
-	padding: ${space[3]}px ${space[3]}px ${space[5]}px;
 	h4 {
 		${textSansBold17};
 		margin: 0 ${space[5]}px 0 0;
@@ -36,6 +35,7 @@ const inlineContentsCss = css`
 const copyContainerCss = css`
 	order: 2;
 	margin-top: ${space[4]}px;
+	padding: 0 ${space[3]}px ${space[5]}px;
 	${from.tablet} {
 		order: 1;
 		margin: 0 0 ${space[6]}px;
@@ -46,13 +46,20 @@ const appStoreBtnsContainerCss = css`
 	margin-top: ${space[4]}px;
 `;
 
+const heroImageContainerCss = css`
+	background: linear-gradient(transparent, rgba(0, 0, 0, 0.1));
+	order: 1;
+	width: 100%;
+	${from.tablet} {
+		background: none;
+		order: 2;
+	}
+`;
+
 const heroImageCss = css`
 	margin: 0 auto;
-	width: 100%;
 	max-width: 450px;
-	order: 1;
 	${from.tablet} {
-		order: 2;
 		max-width: 340px;
 	}
 `;
@@ -83,10 +90,12 @@ export const DownloadAppCtaVariation1 = (
 						/>
 					</div>
 				</div>
-				<img
-					css={heroImageCss}
-					src="https://i.guim.co.uk/img/media/a3400ec516a9b46348cc6d898441b6c4e20a52af/0_0_1172_728/500.png?width=400&quality=75&s=79a92c6634c653e25047589a1832a343"
-				/>
+				<div css={heroImageContainerCss}>
+					<img
+						css={heroImageCss}
+						src="https://i.guim.co.uk/img/media/a3400ec516a9b46348cc6d898441b6c4e20a52af/0_0_1172_728/500.png?width=400&quality=75&s=79a92c6634c653e25047589a1832a343"
+					/>
+				</div>
 			</div>
 		</div>
 	);
