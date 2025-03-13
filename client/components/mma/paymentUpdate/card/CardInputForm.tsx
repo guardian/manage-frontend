71399a6f81914f59e297ec88a748bfe774ec2424
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-03-13 15:53:03 +0000

    add temporary StripeCheckoutSessionButton for development in CardInputForm

--- client/components/mma/paymentUpdate/card/CardInputForm.tsx
+++ client/components/mma/paymentUpdate/card/CardInputForm.tsx
@@ -2,6 +2,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { useStripeSDK } from '../../../../utilities/stripe';
 import type { NewPaymentMethodDetail } from '../NewPaymentMethodDetail';
 import { StripeCardInputForm } from './stripeCardInputForm';
+import { StripeCheckoutSessionButton } from './StripeCheckoutSessionButton';
 
 export interface CardInputFormProps {
 	stripeApiKey: string;
@@ -19,6 +20,10 @@ export const CardInputForm = (props: CardInputFormProps) => {
 
 	return (
 		<>
+			{/* Temporary Implementation Only - For development only */}
+			<StripeCheckoutSessionButton stripeApiKey={props.stripeApiKey} />
+			{/* Temporary Implementation Only - For development only */}
+
 			<Elements
 				stripe={stripePromise}
 				options={{
