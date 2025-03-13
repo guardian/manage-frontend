31d2eafe3ff691d00a553950db1a4234b820dffc
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-03-13 16:12:31 +0000

    Revert "add temporary StripeCheckoutSessionButton for development in CardInputForm"
    This reverts commit 71399a6f81914f59e297ec88a748bfe774ec2424.

--- client/components/mma/paymentUpdate/card/CardInputForm.tsx
+++ client/components/mma/paymentUpdate/card/CardInputForm.tsx
@@ -2,7 +2,6 @@ import { Elements } from '@stripe/react-stripe-js';
 import { useStripeSDK } from '../../../../utilities/stripe';
 import type { NewPaymentMethodDetail } from '../NewPaymentMethodDetail';
 import { StripeCardInputForm } from './stripeCardInputForm';
-import { StripeCheckoutSessionButton } from './StripeCheckoutSessionButton';
 
 export interface CardInputFormProps {
 	stripeApiKey: string;
@@ -20,10 +19,6 @@ export const CardInputForm = (props: CardInputFormProps) => {
 
 	return (
 		<>
-			{/* Temporary Implementation Only - For development only */}
-			<StripeCheckoutSessionButton stripeApiKey={props.stripeApiKey} />
-			{/* Temporary Implementation Only - For development only */}
-
 			<Elements
 				stripe={stripePromise}
 				options={{
