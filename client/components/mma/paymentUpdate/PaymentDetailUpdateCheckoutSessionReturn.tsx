8479e2aadef7e629518d3cedf070d1a5049abaa3
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-03-14 10:43:15 +0000

    Update PaymentDetailUpdateCheckoutSessionReturn.tsx

--- client/components/mma/paymentUpdate/PaymentDetailUpdateCheckoutSessionReturn.tsx
+++ client/components/mma/paymentUpdate/PaymentDetailUpdateCheckoutSessionReturn.tsx
@@ -26,6 +26,16 @@ export const PaymentDetailUpdateCheckoutSessionReturn = () => {
 
 	const navigateToFailedPage = useCallback(() => {
 		const failedPagePath = location.pathname.replace(/[^/]+$/, 'failed');
+
+		/**
+		 * Well, I don't like this approach because the "failed" page is
+		 * loading the current payment methods of the user, but at the same
+		 * time I do not want to create a new page just for this purpose.
+		 * Let's see if we can improve this in the future by avoiding the
+		 * "failed" page to load the payment methods or the requirement
+		 * of having to pass the state to the "failed" page (in that case
+		 * we could just include the component here without having to navigate).
+		 */
 		navigate(failedPagePath, {
 			state: {
 				newPaymentMethodDetailFriendlyName: 'Payment Method',
