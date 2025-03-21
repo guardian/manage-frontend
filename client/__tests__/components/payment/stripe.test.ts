47d9c8e06a6d25103907122349a04750f46d8c3f
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-03-21 10:08:11 +0000

    Uses Tortoise Media Stripe key for Sunday The Observer Unit tests added

--- client/__tests__/components/payment/stripe.test.ts
+++ client/__tests__/components/payment/stripe.test.ts
@@ -1,8 +1,13 @@
 import {
+	sundayTheObserverHomeDeliveryProduct,
+	sundayTheObserverSubscriptionCardProduct,
+} from '@/client/fixtures/productDetail';
+import { PRODUCT_TYPES } from '@/shared/productTypes';
+import {
 	guardianWeeklySubscriptionAustralia,
 	guardianWeeklySubscriptionCard,
 } from '../../../fixtures/subscription';
-import { getStripeKey } from '../../../utilities/stripe';
+import { getStripeKey, getStripeKeyByProduct } from '../../../utilities/stripe';
 
 // @ts-expect-error
 window.guardian = {
@@ -13,6 +18,10 @@ window.guardian = {
 	stripeKeyDefaultCurrencies: {
 		test: 'testKeyDefaultCurrencies',
 		default: 'defaultKeyDefaultCurrencies',
+	},
+	stripeKeyTortoiseMedia: {
+		test: 'testKeyTortoiseMedia',
+		default: 'defaultKeyTortoiseMedia',
 	},
 };
 
@@ -35,5 +44,53 @@ test('Uses Australian Stripe key for Australian delivery address', () => {
 
 	expect(stripePublicKey).toEqual(
 		window.guardian.stripeKeyAustralia?.default,
+	);
+});
+
+test('Uses Tortoise Media Stripe key for Sunday The Observer Home Delivery subscription', () => {
+	const stripePublicKeyDefaultUser = getStripeKeyByProduct(
+		PRODUCT_TYPES.homedelivery,
+		{
+			...sundayTheObserverHomeDeliveryProduct,
+			isTestUser: false,
+		},
+	);
+	expect(stripePublicKeyDefaultUser).toEqual(
+		window.guardian.stripeKeyTortoiseMedia?.default,
+	);
+
+	const stripePublicKeyTestUser = getStripeKeyByProduct(
+		PRODUCT_TYPES.homedelivery,
+		{
+			...sundayTheObserverHomeDeliveryProduct,
+			isTestUser: true,
+		},
+	);
+	expect(stripePublicKeyTestUser).toEqual(
+		window.guardian.stripeKeyTortoiseMedia?.test,
+	);
+});
+
+test('Uses Tortoise Media Stripe key for Sunday The Observer Subscription Card (Test user)', () => {
+	const stripePublicKeyDefaultUser = getStripeKeyByProduct(
+		PRODUCT_TYPES.digitalvoucher,
+		{
+			...sundayTheObserverSubscriptionCardProduct,
+			isTestUser: false,
+		},
+	);
+	expect(stripePublicKeyDefaultUser).toEqual(
+		window.guardian.stripeKeyTortoiseMedia?.default,
+	);
+
+	const stripePublicKeyTestUser = getStripeKeyByProduct(
+		PRODUCT_TYPES.digitalvoucher,
+		{
+			...sundayTheObserverSubscriptionCardProduct,
+			isTestUser: true,
+		},
+	);
+	expect(stripePublicKeyTestUser).toEqual(
+		window.guardian.stripeKeyTortoiseMedia?.test,
 	);
 });
