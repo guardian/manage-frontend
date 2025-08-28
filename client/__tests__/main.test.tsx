10ffe7b2d8db434b2ab8f3d10c3a99a18779fb97
Jon Flynn <jon.flynn@guardian.co.uk>
2025-08-28 09:51:00 +0100

    mock ophan import

--- client/__tests__/main.test.tsx
+++ client/__tests__/main.test.tsx
@@ -1,14 +1,37 @@
 import { create } from 'react-test-renderer';
 import { Main } from '../components/shared/Main';
 
+// Mock ophan to return consistent viewId
+jest.mock('@guardian/ophan-tracker-js/MMA', () => ({
+	record: jest.fn(),
+	sendInitialEvent: jest.fn(),
+	viewId: 'test-view-id-123',
+}));
+
 describe('Main', () => {
 	beforeEach(() => {
 		jest.useFakeTimers();
 		jest.setSystemTime(new Date('2022-01-01'));
+
+		// Set up window.guardian mock
+		Object.defineProperty(window, 'guardian', {
+			writable: true,
+			value: {
+				domain: 'theguardian.com',
+				ophan: {
+					viewId: 'test-view-id-123',
+				},
+			},
+		});
 	});
 
 	afterEach(() => {
 		jest.useRealTimers();
+		// Clean up the guardian mock
+		Object.defineProperty(window, 'guardian', {
+			writable: true,
+			value: undefined,
+		});
 	});
 
 	it('renders something', () => {
