e6236f14ddf7b43b216ffb36d0b96a1e52fe6afd
Jon Flynn <jon.flynn@guardian.co.uk>
2025-08-28 10:09:54 +0100

    import ophan only when window is not undefined

--- client/MMAPage.ts
+++ client/MMAPage.ts
@@ -1,9 +1,13 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import * as Sentry from '@sentry/browser';
-import '@guardian/ophan-tracker-js/MMA';
 import { createRoot } from 'react-dom/client';
 import { MMAPage } from './components/mma/MMAPage';
+
+// Initialize ophan on client side only (should the library handle this??)
+if (typeof window !== 'undefined') {
+	import('@guardian/ophan-tracker-js/MMA');
+}
 
 declare let WEBPACK_BUILD: string;
 
