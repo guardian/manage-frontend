b617395267bcc37e91adb8d0806bfdbc4d5d55a2
Jon Flynn <jon.flynn@guardian.co.uk>
2025-09-08 13:15:13 +0100

    remove duplicate tracker-js initialisation

--- client/HelpCentrePage.ts
+++ client/HelpCentrePage.ts
@@ -4,13 +4,6 @@ import * as Sentry from '@sentry/browser';
 import { createRoot } from 'react-dom/client';
 import { HelpCentrePage } from './components/helpCentre/HelpCentrePage';
 
-// Initialize ophan on client side only
-if (typeof window !== 'undefined') {
-	import('@guardian/ophan-tracker-js/MMA').then(({ init }) => {
-		init();
-	});
-}
-
 declare let WEBPACK_BUILD: string;
 
 if (typeof window !== 'undefined' && window.guardian && window.guardian.dsn) {
