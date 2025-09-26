a8989c7a8bef247bad0d30b825c6296e8e1950ee
Jon Flynn <91546670+jonflynng@users.noreply.github.com>
2025-09-26 09:40:47 +0100

    Upgrade `ophan-tracker-js` (#1397)
    * initial work
    
    * refactor tracker-js entrypoint
    
    * fix imports
    
    * run lint
    
    * update tracker-js version
    
    * run yarn install
    
    * use canaries version of tracker-js for testing in code
    
    * resolve linter errors
    
    * don't use window.guardian.ophan, use imported functions from ophan directly
    
    * fix errors
    
    * fix errors
    
    * change jest.config.json to work with ES modules
    
    * update snapshots
    
    * mock ophan import
    
    * import ophan only when window is not undefined
    
    * update to use named exports from ophan-tracker-js
    
    * use new tracker-js package
    
    * remove comments
    
    * use MMA entrypoint and prevent errors when loading from SSR
    
    * fix test
    
    * upgrade tracker-js to 2.6.0
    
    * remove duplicate tracker-js initialisation
    
    * remove duplicate tracker-js initialisation
    
    * revert
    
    * record bfcache views via component events
    
    ---------
    
    Co-authored-by: Jonathan Flynn <jonl.flynn@guardian.co.uk>

--- client/utilities/analytics.ts
+++ client/utilities/analytics.ts
@@ -1,4 +1,3 @@
-import type { OphanProduct } from '../../shared/ophanTypes';
 import type { ProductDetail } from '../../shared/productResponse';
 import type { ProductType } from '../../shared/productTypes';
 
@@ -22,33 +21,37 @@ export const trackEvent = ({
 	eventLabel,
 	eventValue,
 }: Event) => {
-	if (window.guardian && window.guardian.ophan) {
-		const ophanProduct: OphanProduct | undefined =
-			product &&
-			product.productType.getOphanProductType &&
-			product.productType.getOphanProductType(product.productDetail);
+	if (typeof window === 'undefined') {
+		return;
+	}
 
-		window.guardian.ophan.record({
+	const ophanProduct =
+		product &&
+		product.productType.getOphanProductType &&
+		product.productType.getOphanProductType(product.productDetail);
+
+	const labels = [
+		eventCategory.toUpperCase(),
+		eventAction.toUpperCase(),
+		...(eventLabel ? [eventLabel.toUpperCase()] : []),
+		...(MMA_AB_TEST_DIMENSION_VALUE ? [MMA_AB_TEST_DIMENSION_VALUE] : []),
+	];
+
+	import('@guardian/ophan-tracker-js/MMA').then(({ record }) => {
+		record({
 			componentEvent: {
 				component: {
 					componentType: 'ACQUISITIONS_MANAGE_MY_ACCOUNT',
 					products: ophanProduct ? [ophanProduct] : undefined,
-					campaignCode: window.guardian.INTCMP,
-					labels: [
-						eventCategory.toUpperCase(),
-						eventAction.toUpperCase(),
-						...(eventLabel ? [eventLabel.toUpperCase()] : []),
-						...(MMA_AB_TEST_DIMENSION_VALUE
-							? [MMA_AB_TEST_DIMENSION_VALUE]
-							: []),
-					],
+					campaignCode: window.guardian?.INTCMP,
+					labels,
 				},
 				action: 'VIEW',
 				value: eventValue !== undefined ? `${eventValue}` : undefined,
-				abTest: window.guardian.abTest,
+				abTest: window.guardian?.abTest,
 			},
 		});
-	}
+	});
 };
 
 export const trackEventInOphanOnly = (event: Event) => trackEvent(event);
