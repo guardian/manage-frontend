ccd7efa46af0f811df9d3125ca144238c000820a
(Admin) Jonathan Ruda <admin.jonathan.ruda@J7G4LH7XRF-PT002602.local>
2025-10-21 11:42:33 +0100

    Implements source maps for MMA Client

--- webpack.dev.client.js
+++ webpack.dev.client.js
@@ -1,10 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires -- minimising changes */
 const { merge } = require('webpack-merge');
 const { client } = require('./webpack.common.js');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
+const RELEASE_VERSION = process.env.GITHUB_RUN_NUMBER
+	? process.env.GITHUB_RUN_NUMBER
+	: `DEV_${new Date().getTime()}`;
+
+const sentryPlugins = process.env.SENTRY_AUTH_TOKEN
+	? [
+			SentryWebpackPlugin({
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+				org: 'the-guardian',
+				project: 'manage-frontend-client',
+				release: RELEASE_VERSION,
+			}),
+	  ]
+	: [];
 
 module.exports = merge(client, {
 	devtool: 'inline-source-map',
 	mode: 'development',
+	plugins: sentryPlugins,
 	devServer: {
 		host: 'manage.thegulocal.com',
 		port: 9234,
--- webpack.production.js
+++ webpack.production.js
@@ -3,6 +3,22 @@ const BundleAnalyzerPlugin =
 	require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const { merge } = require('webpack-merge');
 const { client, server } = require('./webpack.common.js');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+
+const RELEASE_VERSION = process.env.GITHUB_RUN_NUMBER
+	? process.env.GITHUB_RUN_NUMBER
+	: `DEV_${new Date().getTime()}`;
+
+const sentryPlugins = process.env.SENTRY_AUTH_TOKEN
+	? [
+			SentryWebpackPlugin({
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+				org: 'the-guardian',
+				project: 'manage-frontend-client',
+				release: RELEASE_VERSION,
+			}),
+	  ]
+	: [];
 
 module.exports = [
 	merge(client, {
@@ -10,10 +26,12 @@ module.exports = [
 		output: {
 			chunkFilename: '[name].[chunkhash].js',
 		},
+		devtool: 'source-map',
 		plugins: [
 			new BundleAnalyzerPlugin({
 				analyzerMode: 'static',
 			}),
+			...sentryPlugins,
 		],
 	}),
 	merge(server, {
