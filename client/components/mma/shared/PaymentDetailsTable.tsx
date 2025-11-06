584d14baee09ca595ab0696a3974844237f6b715
andresilva-guardian <andre.silva.mindera@guardian.co.uk>
2025-11-06 16:53:20 +0000

    Reverted PaymentDetailsTable.tsx to original version

--- client/components/mma/shared/PaymentDetailsTable.tsx
+++ client/components/mma/shared/PaymentDetailsTable.tsx
@@ -1,12 +1,12 @@
 import { css } from '@emotion/react';
-import type { ProductType } from '@/shared/productTypes';
+import { neutral } from '@guardian/source/foundations';
 import type { ProductDetail } from '../../../../shared/productResponse';
 import { PaypalLogo } from './assets/PaypalLogo';
 import { CardDisplay } from './CardDisplay';
 import { DirectDebitDisplay } from './DirectDebitDisplay';
 import type { NextPaymentDetails } from './NextPaymentDetails';
 import { NewPaymentPriceAlert } from './NextPaymentDetails';
-import { ProductDescriptionListTableV2 } from './ProductDescriptionListTableV2';
+import { ProductDescriptionListTable } from './ProductDescriptionListTable';
 import { SepaDisplay } from './SepaDisplay';
 
 interface PaymentDetailsTableProps {
@@ -14,135 +14,93 @@ interface PaymentDetailsTableProps {
 	nextPaymentDetails: NextPaymentDetails | undefined;
 	hasCancellationPending: boolean;
 	tableHeading?: string;
-	specificProductType?: ProductType;
 }
-export const PaymentDetailsTable = (props: PaymentDetailsTableProps) => {
-	const paymentDetailRows =
-		props.nextPaymentDetails &&
-		props.productDetail.subscription.autoRenew &&
-		!props.hasCancellationPending
-			? [
-					{
-						tiles: [
-							{
-								title: props.nextPaymentDetails
-									.nextPaymentDateKey,
-								...(props.productDetail.subscription
-									.nextPaymentDate && {
-									value: props.nextPaymentDetails
-										.nextPaymentDateValue,
-								}),
-							},
-							{
-								title: props.nextPaymentDetails.paymentKey,
-								value: (
-									<span>
-										{props.nextPaymentDetails
-											.isNewPaymentValue && (
-											<NewPaymentPriceAlert />
-										)}
-										{props.nextPaymentDetails.paymentValue}
-									</span>
-								),
-							},
-						],
-					},
-			  ]
-			: [];
-
-	return (
-		<ProductDescriptionListTableV2
-			rows={[
-				...paymentDetailRows,
-				{
-					tiles: [
+export const PaymentDetailsTable = (props: PaymentDetailsTableProps) => (
+	<ProductDescriptionListTable
+		borderColour={neutral[86]}
+		alternateRowBgColors
+		tableHeading={props.tableHeading}
+		content={[
+			...(props.nextPaymentDetails &&
+			props.productDetail.subscription.autoRenew &&
+			!props.hasCancellationPending
+				? [
 						{
-							title: `Payment${
-								props.productDetail.isPaidTier ? ' method' : ''
-							}`,
-							value: props.productDetail.isPaidTier ? (
-								<>
-									{props.productDetail.subscription.card && (
-										<CardDisplay
-											cssOverrides={css`
-												margin: 0;
-											`}
-											inErrorState={
-												!!props.productDetail.alertText
-											}
-											{...props.productDetail.subscription
-												.card}
-										/>
+							title: props.nextPaymentDetails.paymentKey,
+							value: (
+								<span>
+									{props.nextPaymentDetails
+										.isNewPaymentValue && (
+										<NewPaymentPriceAlert />
 									)}
-									{props.productDetail.subscription
-										.payPalEmail && <PaypalLogo />}
-									{props.productDetail.subscription
-										.mandate && (
-										<DirectDebitDisplay
-											inErrorState={
-												!!props.productDetail.alertText
-											}
-											{...props.productDetail.subscription
-												.mandate}
-										/>
-									)}
-									{props.productDetail.subscription
-										.sepaMandate && (
-										<SepaDisplay
-											{...props.productDetail.subscription
-												.sepaMandate}
-										/>
-									)}
-									{props.productDetail.subscription
-										.stripePublicKeyForCardAddition && (
-										<span>No Payment Method</span>
-									)}
-								</>
-							) : (
-								<span>FREE</span>
+									{props.nextPaymentDetails.paymentValue}
+								</span>
 							),
-							spanTwoCols: props.productDetail.subscription
-								.payPalEmail
-								? true
-								: undefined,
 						},
 						{
-							title: 'Expiry date',
-							...(props.productDetail.subscription.card
-								?.expiry && {
-								value: `${
-									props.productDetail.subscription.card.expiry
-										.month < 10
-										? '0'
-										: ''
-								}${
-									props.productDetail.subscription.card.expiry
-										.month
-								}
-								${' / '}
-								${props.productDetail.subscription.card.expiry.year}`,
+							title: props.nextPaymentDetails.nextPaymentDateKey,
+							...(props.productDetail.subscription
+								.nextPaymentDate && {
+								value: props.nextPaymentDetails
+									.nextPaymentDateValue,
 							}),
 						},
-					],
-					actions:
-						props.productDetail.isPaidTier &&
-						props.productDetail.subscription
-							.safeToUpdatePaymentMethod &&
-						!props.productDetail.subscription.payPalEmail
-							? [
-									{
-										text: 'Update method',
-										linkTo: `/payment/${props.specificProductType?.urlPart}`,
-										alert: !!props.productDetail.alertText,
-										state: {
-											productDetail: props.productDetail,
-										},
-									},
-							  ]
-							: [],
-				},
-			]}
-			separateEachRow
-		/>
-	);
-};
+				  ]
+				: []),
+			{
+				title: `Payment${
+					props.productDetail.isPaidTier ? ' method' : ''
+				}`,
+				value: props.productDetail.isPaidTier ? (
+					<>
+						{props.productDetail.subscription.card && (
+							<CardDisplay
+								cssOverrides={css`
+									margin: 0;
+								`}
+								inErrorState={!!props.productDetail.alertText}
+								{...props.productDetail.subscription.card}
+							/>
+						)}
+						{props.productDetail.subscription.payPalEmail && (
+							<PaypalLogo />
+						)}
+						{props.productDetail.subscription.mandate && (
+							<DirectDebitDisplay
+								inErrorState={!!props.productDetail.alertText}
+								{...props.productDetail.subscription.mandate}
+							/>
+						)}
+						{props.productDetail.subscription.sepaMandate && (
+							<SepaDisplay
+								{...props.productDetail.subscription
+									.sepaMandate}
+							/>
+						)}
+						{props.productDetail.subscription
+							.stripePublicKeyForCardAddition && (
+							<span>No Payment Method</span>
+						)}
+					</>
+				) : (
+					<span>FREE</span>
+				),
+				spanTwoCols: props.productDetail.subscription.payPalEmail
+					? true
+					: undefined,
+			},
+			{
+				title: 'Expiry date',
+				...(props.productDetail.subscription.card?.expiry && {
+					value: `${
+						props.productDetail.subscription.card.expiry.month < 10
+							? '0'
+							: ''
+					}${props.productDetail.subscription.card.expiry.month}
+                    ${' / '}
+                    ${props.productDetail.subscription.card.expiry.year}`,
+				}),
+			},
+		]}
+	/>
+);
