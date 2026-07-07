306eb737de161d7bb6e228aa38e68d5f2eceddbf
enesihsa <enes.aydogan@guardian.co.uk>
2026-07-07 17:10:02 +0100

    Refactoring

--- client/components/mma/cancel/digitalShared/SharedSubscriptionLeaveContainer.tsx
+++ client/components/mma/cancel/digitalShared/SharedSubscriptionLeaveContainer.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import { NAV_LINKS } from '../../shared/nav/NavConfig';
-import { PageContainer } from '../Page';
+import { NAV_LINKS } from '@/client/components/shared/nav/NavConfig';
+import { PageContainer } from '../../Page';
 
 export const LeaveSharedSubscriptionContainer = () => (
 	<PageContainer
