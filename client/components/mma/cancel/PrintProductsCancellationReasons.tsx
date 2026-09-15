5a3440ab8a77ca092458b5a1ba63846b958a86e4
enesihsa <enes.aydogan@guardian.co.uk>
2026-09-15 14:28:10 +0100

    Moved all reason configurations to cancellationReason.ts

--- client/components/mma/cancel/PrintProductsCancellationReasons.tsx
+++ client/components/mma/cancel/PrintProductsCancellationReasons.tsx
@@ -1,62 +1,29 @@
 import { shuffleArray } from '@/client/utilities/utils';
-import { standardAlternateFeedbackIntro } from './cancellationConstants';
 import {
 	type CancellationReason,
-	sharedSubscriptionCancellationReason,
+	otherCancellationReason4,
+	reasonBenefits5,
+	reasonDontReadEnough2,
+	reasonDuplicateSubscription,
+	reasonEditorial1,
+	reasonFinancialCircumstances5,
+	reasonIssue1,
+	reasonSharedSubscriptionCancellation,
+	reasonTime2,
 } from './cancellationReason';
 
 const printProductsCancellationReasons: CancellationReason[] = [
-	{
-		reasonId: 'mma_dont_read_enough',
-		linkLabel: "I don't use the subscription enough to justify the cost",
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_benefits',
-		linkLabel:
-			"The subscription's benefits and features don't match my expectations",
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_time',
-		linkLabel: 'I no longer engage with the content as I used to',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_issue',
-		linkLabel:
-			'I have encountered usability/technical issues or delivery issues',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_financial_circumstances',
-		linkLabel:
-			'My personal circumstances have changed, or I have decided to take a break from the news',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_duplicate_subscription',
-		linkLabel:
-			'I realised I have a duplicate subscription or am already covered by a multiple account plan',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	{
-		reasonId: 'mma_editorial',
-		linkLabel: 'I am unhappy with some editorial decisions',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
-	sharedSubscriptionCancellationReason,
-];
-
-const otherCancellationReason: CancellationReason[] = [
-	{
-		reasonId: 'mma_other',
-		linkLabel: 'Other (please specify)',
-		alternateFeedbackIntro: standardAlternateFeedbackIntro,
-	},
+	reasonEditorial1,
+	reasonTime2,
+	reasonDontReadEnough2,
+	reasonIssue1,
+	reasonFinancialCircumstances5,
+	reasonBenefits5,
+	reasonDuplicateSubscription,
+	reasonSharedSubscriptionCancellation,
 ];
 
 export const shuffledPrintProductsCancellationReasons: CancellationReason[] = [
 	...(shuffleArray(printProductsCancellationReasons) as CancellationReason[]),
-	...otherCancellationReason,
+	otherCancellationReason4,
 ];
