0f2073d8247ad3f493bae5be5095a9e1878e4913
Graham Hopgood <graham.hopgood@guardian.co.uk>
2026-09-17 09:41:31 +0100

    Update minimum Supporter Plus prices

--- client/utilities/pricingConfig/supporterPlusPricing.ts
+++ client/utilities/pricingConfig/supporterPlusPricing.ts
@@ -10,32 +10,32 @@ export const supporterPlusPriceConfigByCountryGroup: Record<
 	Record<string, PriceConfig>
 > = {
 	GBP: {
-		month: { minAmount: 12, maxAmount: 166 },
-		year: { minAmount: 120, maxAmount: 2000 },
+		month: { minAmount: 14, maxAmount: 166 },
+		year: { minAmount: 140, maxAmount: 2000 },
 	},
 	USD: {
-		month: { minAmount: 15, maxAmount: 800 },
-		year: { minAmount: 150, maxAmount: 10000 },
+		month: { minAmount: 18, maxAmount: 800 },
+		year: { minAmount: 180, maxAmount: 10000 },
 	},
 	EUR: {
-		month: { minAmount: 12, maxAmount: 166 },
-		year: { minAmount: 120, maxAmount: 2000 },
+		month: { minAmount: 14, maxAmount: 166 },
+		year: { minAmount: 140, maxAmount: 2000 },
 	},
 	AUD: {
-		month: { minAmount: 20, maxAmount: 200 },
-		year: { minAmount: 200, maxAmount: 2000 },
+		month: { minAmount: 25, maxAmount: 200 },
+		year: { minAmount: 250, maxAmount: 2000 },
 	},
 	NZD: {
-		month: { minAmount: 20, maxAmount: 200 },
-		year: { minAmount: 200, maxAmount: 2000 },
+		month: { minAmount: 25, maxAmount: 200 },
+		year: { minAmount: 250, maxAmount: 2000 },
 	},
 	CAD: {
-		month: { minAmount: 15, maxAmount: 166 },
-		year: { minAmount: 150, maxAmount: 2000 },
+		month: { minAmount: 18, maxAmount: 166 },
+		year: { minAmount: 180, maxAmount: 2000 },
 	},
 	international: {
-		month: { minAmount: 15, maxAmount: 166 },
-		year: { minAmount: 150, maxAmount: 2000 },
+		month: { minAmount: 18, maxAmount: 166 },
+		year: { minAmount: 180, maxAmount: 2000 },
 	},
 };
 
